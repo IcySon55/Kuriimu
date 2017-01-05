6fcf700576372da454b299fc14d879152223db55
IcySon55 <icyson55@gmail.com>
2017-01-05 01:25:26 -0500

    ~Updated JMSG plugin assembly information.

--- file_jmsg/Properties/AssemblyInfo.cs
+++ file_jmsg/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("file_jmsg")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("JMSG File plugin for Kuriimu")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("file_jmsg")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © IcySon55 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
