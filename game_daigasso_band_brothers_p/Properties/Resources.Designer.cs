48963cb6c820c7da3bc9cbb98edc19c957f985dd
adibsurani <adib.surani@gmail.com>
2017-02-14 05:18:56 +1100

    Improvements to daigasso handler

--- game_daigasso_band_brothers_p/Properties/Resources.Designer.cs
+++ game_daigasso_band_brothers_p/Properties/Resources.Designer.cs
@@ -71,6 +71,26 @@ namespace game_daigasso_band_brothers_p.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] BasicRim_bcfnt {
+            get {
+                object obj = ResourceManager.GetObject("BasicRim_bcfnt", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] cbf_std_bcfnt {
+            get {
+                object obj = ResourceManager.GetObject("cbf_std_bcfnt", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap daigasso_box {
