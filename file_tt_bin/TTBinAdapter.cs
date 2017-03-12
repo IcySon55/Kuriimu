9200b568a4d34fbc28cb843ed84946c5dc4d695f
Kirito <Kirito@LIZBETH>
2017-03-12 18:44:13 +0100

    Fixed several bugs with Cetera

--- file_tt_bin/TTBinAdapter.cs
+++ file_tt_bin/TTBinAdapter.cs
@@ -12,215 +12,220 @@ using KuriimuContract;
 
 namespace file_ttbin
 {
-	public sealed class TTBinAdapter : IFileAdapter
-	{
-		private FileInfo _fileInfo = null;
-		private TTBIN _ttbin = null;
-		private TTBIN _ttbinBackup = null;
-		private List<Entry> _entries = null;
+    public sealed class TTBinAdapter : IFileAdapter
+    {
+        private FileInfo _fileInfo = null;
+        private TTBIN _ttbin = null;
+        private TTBIN _ttbinBackup = null;
+        private List<Entry> _entries = null;
 
-		#region Properties
+        #region Properties
 
-		// Information
-		public string Name => "TTBIN";
+        // Information
+        public string Name => "TTBIN";
 
-		public string Description => "Time Travelers Binary Text";
+        public string Description => "Time Travelers Binary Text";
 
-		public string Extension => "*.cfg.bin;*.pck";
+        public string Extension => "*.cfg.bin;*.pck";
 
-		public string About => "This is the TTBin file adapter for Kuriimu.";
+        public string About => "This is the TTBin file adapter for Kuriimu.";
 
-		// Feature Support
-		public bool FileHasExtendedProperties => false;
+        // Feature Support
+        public bool FileHasExtendedProperties => false;
 
-		public bool CanSave => true;
+        public bool CanSave => true;
 
-		public bool CanAddEntries => false;
+        public bool CanAddEntries => false;
 
-		public bool CanRenameEntries => false;
+        public bool CanRenameEntries => false;
 
-		public bool CanDeleteEntries => false;
+        public bool CanDeleteEntries => false;
 
-		public bool CanSortEntries => true;
+        public bool CanSortEntries => true;
 
-		public bool EntriesHaveSubEntries => false;
+        public bool EntriesHaveSubEntries => false;
 
-		public bool EntriesHaveUniqueNames => true;
+        public bool EntriesHaveUniqueNames => true;
 
-		public bool EntriesHaveExtendedProperties => false;
+        public bool EntriesHaveExtendedProperties => false;
 
-		public FileInfo FileInfo
-		{
-			get
-			{
-				return _fileInfo;
-			}
-			set
-			{
-				_fileInfo = value;
-			}
-		}
+        public FileInfo FileInfo
+        {
+            get
+            {
+                return _fileInfo;
+            }
+            set
+            {
+                _fileInfo = value;
+            }
+        }
 
-		public string LineEndings => "\n";
+        public string LineEndings => "\n";
 
-		#endregion
+        #endregion
 
-		public bool Identify(string filename)
-		{
-			using (var br = new BinaryReaderX(File.OpenRead(filename)))
-			{
-				//possible identifications: PCK, cfg.bin, XPCK-Archive
-				//if cfg.bin
-				br.BaseStream.Position = 0x18;
-				uint t1 = br.ReadUInt32();
-				br.BaseStream.Position = 0x24;
-				uint t2 = br.ReadUInt32();
-				if (t1 == 0x0 && t2 == 0x14)
-				{
-					return true;
-				}
-				br.BaseStream.Position = 0;
+        public bool Identify(string filename)
+        {
+            using (var br = new BinaryReaderX(File.OpenRead(filename)))
+            {
+                byte[] decomp = LZ11.Decompress(br.BaseStream);
+                File.OpenWrite("test.decomp").Write(decomp, 0, decomp.Length);
+                byte[] comp = LZ10.Compress(new MemoryStream(decomp));
+                File.OpenWrite("test.comp").Write(comp, 0, comp.Length);
 
-				//if PCK
-				int entryCount = br.ReadInt32();
-				br.BaseStream.Position = 0x8;
-				if (entryCount * 3 * 4 + 4 == br.ReadInt32())
-				{
-					return true;
-				}
-				br.BaseStream.Position = 0;
+                //possible identifications: PCK, cfg.bin, XPCK-Archive
+                //if cfg.bin
+                br.BaseStream.Position = 0x18;
+                uint t1 = br.ReadUInt32();
+                br.BaseStream.Position = 0x24;
+                uint t2 = br.ReadUInt32();
+                if (t1 == 0x0 && t2 == 0x14)
+                {
+                    return true;
+                }
+                br.BaseStream.Position = 0;
 
-				//if XPCK
-				if (br.ReadString(4) == "XPCK")
-				{
-					return true;
-				}
-				else
-				{
-					br.BaseStream.Position = 0;
-					byte[] result = CriWare.GetDecompressedBytes(new MemoryStream(br.ReadBytes((int)br.BaseStream.Length)));
-					using (BinaryReaderX br2 = new BinaryReaderX(new MemoryStream(result)))
-					{
-						if (br2.ReadString(4) == "XPCK")
-						{
-							br2.BaseStream.Position = 0;
-							return true;
-						}
-					}
-				}
+                //if PCK
+                int entryCount = br.ReadInt32();
+                br.BaseStream.Position = 0x8;
+                if (entryCount * 3 * 4 + 4 == br.ReadInt32())
+                {
+                    return true;
+                }
+                br.BaseStream.Position = 0;
 
-				return false;
-			}
-		}
+                //if XPCK
+                if (br.ReadString(4) == "XPCK")
+                {
+                    return true;
+                }
+                else
+                {
+                    br.BaseStream.Position = 0;
+                    byte[] result = CriWare.GetDecompressedBytes(new MemoryStream(br.ReadBytes((int)br.BaseStream.Length)));
+                    using (BinaryReaderX br2 = new BinaryReaderX(new MemoryStream(result)))
+                    {
+                        if (br2.ReadString(4) == "XPCK")
+                        {
+                            br2.BaseStream.Position = 0;
+                            return true;
+                        }
+                    }
+                }
 
-		//Load file, make backup if needed
-		public LoadResult Load(string filename, bool autoBackup = false)
-		{
-			LoadResult result = LoadResult.Success;
+                return false;
+            }
+        }
 
-			_fileInfo = new FileInfo(filename);
-			_entries = null;
+        //Load file, make backup if needed
+        public LoadResult Load(string filename, bool autoBackup = false)
+        {
+            LoadResult result = LoadResult.Success;
 
-			if (_fileInfo.Exists)
-			{
-				_ttbin = new TTBIN(_fileInfo.FullName);
+            _fileInfo = new FileInfo(filename);
+            _entries = null;
 
-				string backupFilePath = _fileInfo.FullName + ".bak";
-				if (File.Exists(backupFilePath))
-				{
-					_ttbinBackup = new TTBIN(backupFilePath);
-				}
-				else if (autoBackup || MessageBox.Show("Would you like to create a backup of " + _fileInfo.Name + "?\r\nA backup allows the Original text box to display the source text before edits were made.", "Create Backup", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
-				{
-					File.Copy(_fileInfo.FullName, backupFilePath);
-					_ttbinBackup = new TTBIN(backupFilePath);
-				}
-				else
-				{
-					_ttbinBackup = null;
-				}
-			}
-			else
-				result = LoadResult.FileNotFound;
+            if (_fileInfo.Exists)
+            {
+                _ttbin = new TTBIN(_fileInfo.FullName);
 
-			return result;
-		}
+                string backupFilePath = _fileInfo.FullName + ".bak";
+                if (File.Exists(backupFilePath))
+                {
+                    _ttbinBackup = new TTBIN(backupFilePath);
+                }
+                else if (autoBackup || MessageBox.Show("Would you like to create a backup of " + _fileInfo.Name + "?\r\nA backup allows the Original text box to display the source text before edits were made.", "Create Backup", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    File.Copy(_fileInfo.FullName, backupFilePath);
+                    _ttbinBackup = new TTBIN(backupFilePath);
+                }
+                else
+                {
+                    _ttbinBackup = null;
+                }
+            }
+            else
+                result = LoadResult.FileNotFound;
 
-		public SaveResult Save(string filename = "")
-		{
-			SaveResult result = SaveResult.Success;
+            return result;
+        }
 
-			if (filename.Trim() != string.Empty)
-				_fileInfo = new FileInfo(filename);
+        public SaveResult Save(string filename = "")
+        {
+            SaveResult result = SaveResult.Success;
 
-			try
-			{
-				_ttbin.Save(_fileInfo.FullName);
-			}
-			catch (Exception)
-			{
-				result = SaveResult.Failure;
-			}
+            if (filename.Trim() != string.Empty)
+                _fileInfo = new FileInfo(filename);
 
-			return result;
-		}
+            try
+            {
+                _ttbin.Save(_fileInfo.FullName);
+            }
+            catch (Exception)
+            {
+                result = SaveResult.Failure;
+            }
 
-		// Entries
-		public IEnumerable<IEntry> Entries
-		{
-			get
-			{
-				if (_entries == null)
-				{
-					_entries = new List<Entry>();
+            return result;
+        }
 
-					foreach (Label label in _ttbin.Labels)
-					{
-						if (_ttbinBackup == null)
-						{
-							Entry entry = new Entry(label);
-							_entries.Add(entry);
-						}
-						else
-						{
-							Entry entry = new Entry(label, _ttbinBackup.Labels.FirstOrDefault(o => o.TextID == label.TextID));
-							_entries.Add(entry);
-						}
-					}
-				}
+        // Entries
+        public IEnumerable<IEntry> Entries
+        {
+            get
+            {
+                if (_entries == null)
+                {
+                    _entries = new List<Entry>();
 
-				return _entries;
-			}
-		}
+                    foreach (Label label in _ttbin.Labels)
+                    {
+                        if (_ttbinBackup == null)
+                        {
+                            Entry entry = new Entry(label);
+                            _entries.Add(entry);
+                        }
+                        else
+                        {
+                            Entry entry = new Entry(label, _ttbinBackup.Labels.FirstOrDefault(o => o.TextID == label.TextID));
+                            _entries.Add(entry);
+                        }
+                    }
+                }
 
-		public IEnumerable<string> NameList => Entries?.Select(o => o.Name);
+                return _entries;
+            }
+        }
 
-		public string NameFilter => @".*";
+        public IEnumerable<string> NameList => Entries?.Select(o => o.Name);
 
-		public int NameMaxLength => 0;
+        public string NameFilter => @".*";
 
-		// Features
-		public bool ShowProperties(Icon icon) => false;
+        public int NameMaxLength => 0;
 
-		public IEntry NewEntry() => new Entry();
+        // Features
+        public bool ShowProperties(Icon icon) => false;
 
-		public bool AddEntry(IEntry entry) => false;
+        public IEntry NewEntry() => new Entry();
 
-		public bool RenameEntry(IEntry entry, string newName) => false;
+        public bool AddEntry(IEntry entry) => false;
 
-		public bool DeleteEntry(IEntry entry) => false;
+        public bool RenameEntry(IEntry entry, string newName) => false;
 
-		public bool ShowEntryProperties(IEntry entry, Icon icon) => false;
+        public bool DeleteEntry(IEntry entry) => false;
 
-		// Settings
-		public bool SortEntries
-		{
-			get { return Settings.Default.SortEntries; }
-			set
-			{
-				Settings.Default.SortEntries = value;
-				Settings.Default.Save();
-			}
-		}
-	}
+        public bool ShowEntryProperties(IEntry entry, Icon icon) => false;
+
+        // Settings
+        public bool SortEntries
+        {
+            get { return Settings.Default.SortEntries; }
+            set
+            {
+                Settings.Default.SortEntries = value;
+                Settings.Default.Save();
+            }
+        }
+    }
 }
--- image_xi/XI.cs
+++ image_xi/XI.cs
@@ -9,140 +9,140 @@ using Cetera.Compression;
 
 namespace image_xi
 {
-	public class XI
-	{
-		public enum Format : byte
-		{
-			RGBA8888, RGBA4444,
-			RGBA5551, RGB888, RGB565,
-			LA88 = 11, LA44, L8, HL88, A8,
-			L4 = 26, A4, ETC1, ETC1A4
-		}
+    public class XI
+    {
+        public enum Format : byte
+        {
+            RGBA8888, RGBA4444,
+            RGBA5551, RGB888, RGB565,
+            LA88 = 11, LA44, L8, HL88, A8,
+            L4 = 26, A4, ETC1, ETC1A4
+        }
 
-		public enum Compression : byte
-		{
-			Uncompressed, LZSS,
-			Huffman4, Huffman8,
-			RLE
-		}
+        public enum Compression : byte
+        {
+            Uncompressed, LZSS,
+            Huffman4, Huffman8,
+            RLE
+        }
 
-		public static Compression tableComp = 0;
-		public static Compression picComp = 0;
+        public static Compression tableComp = 0;
+        public static Compression picComp = 0;
 
-		[StructLayout(LayoutKind.Sequential, Pack = 1)]
-		struct Header
-		{
-			public Magic magic; // IMGC
-			public int const1; // 30 30 00 00
-			public short const2; // 30 00
-			public Format imageFormat;
-			public byte const3; // 01
-			public byte combineFormat;
-			public byte bitDepth;
-			public short bytesPerTile;
-			public short width;
-			public short height;
-			public int const4; // 30 00 00 00
-			public int const5; // 30 00 01 00
-			public int tableDataOffset; // always 0x48
-			public int const6; // 03 00 00 00
-			public int const7; // 00 00 00 00
-			public int const8; // 00 00 00 00
-			public int const9; // 00 00 00 00
-			public int const10; // 00 00 00 00
-			public int tableSize1;
-			public int tableSize2;
-			public int imgDataSize;
-			public int const11; // 00 00 00 00
-			public int const12; // 00 00 00 00
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        struct Header
+        {
+            public Magic magic; // IMGC
+            public int const1; // 30 30 00 00
+            public short const2; // 30 00
+            public Format imageFormat;
+            public byte const3; // 01
+            public byte combineFormat;
+            public byte bitDepth;
+            public short bytesPerTile;
+            public short width;
+            public short height;
+            public int const4; // 30 00 00 00
+            public int const5; // 30 00 01 00
+            public int tableDataOffset; // always 0x48
+            public int const6; // 03 00 00 00
+            public int const7; // 00 00 00 00
+            public int const8; // 00 00 00 00
+            public int const9; // 00 00 00 00
+            public int const10; // 00 00 00 00
+            public int tableSize1;
+            public int tableSize2;
+            public int imgDataSize;
+            public int const11; // 00 00 00 00
+            public int const12; // 00 00 00 00
 
-			public void checkConst()
-			{
-				var ex = new Exception("Unknown xi file format!");
+            public void checkConst()
+            {
+                var ex = new Exception("Unknown xi file format!");
 
-				if (const1 != 0x3030) throw ex;
-				if (const2 != 0x30) throw ex;
-				if (const3 != 1) throw ex;
-				if (const4 != 0x30) throw ex;
-				if (const5 != 0x10030) throw ex;
-				if (tableDataOffset != 0x48) throw ex;
-				if (const6 != 3) throw ex;
-				if (const7 != 0) throw ex;
-				if (const8 != 0) throw ex;
-				if (const9 != 0) throw ex;
-				if (const10 != 0) throw ex;
-				if (const11 != 0) throw ex;
-				if (const12 != 0) throw ex;
-				if (((tableSize1 + 3) & ~3) != tableSize2) throw ex;
-				if (bytesPerTile != 8 * bitDepth) throw ex;
-			}
-		}
+                if (const1 != 0x3030) throw ex;
+                if (const2 != 0x30) throw ex;
+                if (const3 != 1) throw ex;
+                if (const4 != 0x30) throw ex;
+                if (const5 != 0x10030) throw ex;
+                if (tableDataOffset != 0x48) throw ex;
+                if (const6 != 3) throw ex;
+                if (const7 != 0) throw ex;
+                if (const8 != 0) throw ex;
+                if (const9 != 0) throw ex;
+                if (const10 != 0) throw ex;
+                if (const11 != 0) throw ex;
+                if (const12 != 0) throw ex;
+                if (((tableSize1 + 3) & ~3) != tableSize2) throw ex;
+                if (bytesPerTile != 8 * bitDepth) throw ex;
+            }
+        }
 
-		public static byte[] Decomp(BinaryReaderX br)
-		{
-			// above to be restored eventually with some changes to Cetera
-			return CriWare.GetDecompressedBytes(br.BaseStream);
-		}
+        public static byte[] Decomp(BinaryReaderX br)
+        {
+            // above to be restored eventually with some changes to Cetera
+            return CriWare.GetDecompressedBytes(br.BaseStream);
+        }
 
-		public static Bitmap Load(Stream input)
-		{
-			using (var br = new BinaryReaderX(input))
-			{
-				//check header
-				var header = br.ReadStruct<Header>();
-				header.checkConst();
+        public static Bitmap Load(Stream input)
+        {
+            using (var br = new BinaryReaderX(input))
+            {
+                //check header
+                var header = br.ReadStruct<Header>();
+                header.checkConst();
 
-				//decompress table
-				br.BaseStream.Position = header.tableDataOffset;
-				tableComp = (Compression)(br.ReadUInt32() % 8);
-				br.BaseStream.Position = header.tableDataOffset;
-				byte[] table = CriWare.GetDecompressedBytes(new MemoryStream(br.ReadBytes(header.tableSize1)));
+                //decompress table
+                br.BaseStream.Position = header.tableDataOffset;
+                tableComp = (Compression)(br.ReadUInt32() % 8);
+                br.BaseStream.Position = header.tableDataOffset;
+                byte[] table = CriWare.GetDecompressedBytes(new MemoryStream(br.ReadBytes(header.tableSize1)));
 
-				//get decompressed picture data
-				br.BaseStream.Position = header.tableDataOffset + header.tableSize2;
-				picComp = (Compression)(br.ReadUInt32() % 8);
-				br.BaseStream.Position = header.tableDataOffset + header.tableSize2;
-				byte[] tex = CriWare.GetDecompressedBytes(new MemoryStream(br.ReadBytes(header.imgDataSize)));
+                //get decompressed picture data
+                br.BaseStream.Position = header.tableDataOffset + header.tableSize2;
+                picComp = (Compression)(br.ReadUInt32() % 8);
+                br.BaseStream.Position = header.tableDataOffset + header.tableSize2;
+                byte[] tex = CriWare.GetDecompressedBytes(new MemoryStream(br.ReadBytes(header.imgDataSize)));
 
-				//order pic blocks by table
-				byte[] pic = Order(new BinaryReaderX(new MemoryStream(table)), table.Length, new BinaryReaderX(new MemoryStream(tex)), header.width, header.height, header.bitDepth);
+                //order pic blocks by table
+                byte[] pic = Order(new BinaryReaderX(new MemoryStream(table)), table.Length, new BinaryReaderX(new MemoryStream(tex)), header.width, header.height, header.bitDepth);
 
-				//return decompressed picture data
-				var settings = new Settings
-				{
-					Width = header.width,
-					Height = header.height,
-					Orientation = Orientation.TransposeTile,
-					Format = Settings.ConvertFormat(header.imageFormat),
-					PadToPowerOf2 = false
-				};
-				return Common.Load(pic, settings);
-			}
-		}
+                //return decompressed picture data
+                var settings = new ImageSettings
+                {
+                    Width = header.width,
+                    Height = header.height,
+                    Orientation = Orientation.TransposeTile,
+                    Format = ImageSettings.ConvertFormat(header.imageFormat),
+                    PadToPowerOf2 = false
+                };
+                return Common.Load(pic, settings);
+            }
+        }
 
-		public static byte[] Order(BinaryReaderX table, int tableLength, BinaryReaderX tex, int w, int h, byte bitDepth)
-		{
-			var ms = new MemoryStream();
-			for (int i = 0; i < tableLength; i += 2)
-			{
-				int entry = table.ReadUInt16();
-				if (entry == 0xFFFF)
-				{
-					for (int j = 0; j < (64 * bitDepth) / 8; j++)
-					{
-						ms.WriteByte(0);
-					}
-				}
-				else
-				{
-					tex.BaseStream.Position = entry * (64 * bitDepth / 8);
-					for (int j = 0; j < (64 * bitDepth) / 8; j++)
-					{
-						ms.WriteByte(tex.ReadByte());
-					}
-				}
-			}
-			return ms.ToArray();
-		}
-	}
+        public static byte[] Order(BinaryReaderX table, int tableLength, BinaryReaderX tex, int w, int h, byte bitDepth)
+        {
+            var ms = new MemoryStream();
+            for (int i = 0; i < tableLength; i += 2)
+            {
+                int entry = table.ReadUInt16();
+                if (entry == 0xFFFF)
+                {
+                    for (int j = 0; j < (64 * bitDepth) / 8; j++)
+                    {
+                        ms.WriteByte(0);
+                    }
+                }
+                else
+                {
+                    tex.BaseStream.Position = entry * (64 * bitDepth / 8);
+                    for (int j = 0; j < (64 * bitDepth) / 8; j++)
+                    {
+                        ms.WriteByte(tex.ReadByte());
+                    }
+                }
+            }
+            return ms.ToArray();
+        }
+    }
 }
