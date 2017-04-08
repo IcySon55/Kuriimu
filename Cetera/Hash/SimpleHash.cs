b30951eea8d2fe973174e7217ce07abde3ac23b0
IcySon55 <icyson55@gmail.com>
2017-04-08 00:16:14 -0400

    =SimpleHash style updates. -Removed the old LabelChecksum from MSBT and centralized the magic hash value.

--- Cetera/Hash/SimpleHash.cs
+++ Cetera/Hash/SimpleHash.cs
@@ -1,21 +1,17 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Cetera.Hash
 {
     public class SimpleHash
     {
-        public static uint Create(string label, uint magic, uint hashCount)
+        public static uint Create(string input, uint magic, uint hashCount)
         {
             uint hash = 0;
 
-            for (int i = 0; i < label.Count(); i++)
+            for (int i = 0; i < input.Count(); i++)
             {
                 hash *= magic;
-                hash += label[i];
+                hash += input[i];
             }
 
             return hash % hashCount;
