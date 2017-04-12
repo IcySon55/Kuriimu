d73d0b2b8f830f7cc8abb93164afd037ae8f890c
Kirito <alberteinstein1980@gmx.de>
2017-04-12 18:09:35 +0200

    Added Save function for XI;

--- image_xi/XI.cs
+++ image_xi/XI.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
+using System.Linq;
+using System.IO.Compression;
 using System.Runtime.InteropServices;
+using System.Threading;
 using Cetera.Compression;
 using Cetera.Image;
 using Kuriimu.Contract;
@@ -78,18 +82,14 @@ namespace image_xi
             }
         }
 
-        public static byte[] Decomp(BinaryReaderX br)
-        {
-            // above to be restored eventually with some changes to Cetera
-            return CriWare.GetDecompressedBytes(br.BaseStream);
-        }
+        private static Header header;
 
         public static Bitmap Load(Stream input)
         {
             using (var br = new BinaryReaderX(input))
             {
                 //check header
-                var header = br.ReadStruct<Header>();
+                header = br.ReadStruct<Header>();
                 header.checkConst();
 
                 //decompress table
@@ -105,13 +105,17 @@ namespace image_xi
                 byte[] tex = CriWare.GetDecompressedBytes(new MemoryStream(br.ReadBytes(header.imgDataSize)));
 
                 //order pic blocks by table
-                byte[] pic = Order(new BinaryReaderX(new MemoryStream(table)), table.Length, new BinaryReaderX(new MemoryStream(tex)), header.width, header.height, header.bitDepth);
+                int width = header.width;
+                int height = header.height;
+                while (width % 8 != 0) width++;
+                while (height % 8 != 0) height++;
+                byte[] pic = Order(new BinaryReaderX(new MemoryStream(table)), table.Length, new BinaryReaderX(new MemoryStream(tex)), width, height, header.bitDepth);
 
                 //return decompressed picture data
                 var settings = new ImageSettings
                 {
-                    Width = header.width,
-                    Height = header.height,
+                    Width = width,
+                    Height = height,
                     Orientation = Orientation.TransposeTile,
                     Format = ImageSettings.ConvertFormat(header.imageFormat),
                     PadToPowerOf2 = false
@@ -128,21 +132,90 @@ namespace image_xi
                 int entry = table.ReadUInt16();
                 if (entry == 0xFFFF)
                 {
-                    for (int j = 0; j < (64 * bitDepth) / 8; j++)
+                    for (int j = 0; j < 64 * (bitDepth / 8); j++)
                     {
                         ms.WriteByte(0);
                     }
                 }
                 else
                 {
-                    tex.BaseStream.Position = entry * (64 * bitDepth / 8);
-                    for (int j = 0; j < (64 * bitDepth) / 8; j++)
+                    tex.BaseStream.Position = entry * (64 * (bitDepth / 8));
+                    for (int j = 0; j < 64 * (bitDepth / 8); j++)
                     {
                         ms.WriteByte(tex.ReadByte());
                     }
                 }
             }
             return ms.ToArray();
+        }
+
+        public static void Save(String filename, Bitmap bitmap)
+        {
+            byte[] pic = Common.Save(bitmap, new ImageSettings
+            {
+                Width = bitmap.Width,
+                Height = bitmap.Height,
+                Orientation = Orientation.TransposeTile,
+                Format = ImageSettings.ConvertFormat(Format.RGBA8888),
+                PadToPowerOf2 =false
+            });
+
+            using (BinaryWriterX bw = new BinaryWriterX(File.OpenWrite(filename)))
+            {
+                List<short> table;
+                byte[] importPic = Deflate(pic, out table, 32, bitmap.Width, bitmap.Height);
+
+                header.imageFormat = Format.RGBA8888;
+                header.bitDepth = 32;
+                header.bytesPerTile = 32 * 8;
+                header.tableSize1 = table.Count * 2 + 4;
+                header.tableSize2 = (header.tableSize1 + 3) & ~3;
+                header.imgDataSize = importPic.Length + 4;
+                bw.WriteStruct(header);
+
+                bw.Write(header.tableSize1<<3);
+                foreach (var tableEntry in table) bw.Write(tableEntry);
+
+                bw.BaseStream.Position = 0x48 + header.tableSize2;
+                bw.Write(header.imgDataSize<<3);
+                bw.Write(importPic);
+            }
+        }
+
+        public static byte[] Deflate(byte[] pic, out List<short> table, int bitDepth, int width, int height)
+        {
+            table = new List<short>();
+            List<byte[]> parts = new List<byte[]>();
+            byte[] result = new byte[width * height * (bitDepth / 8)];
+
+            using (BinaryReaderX br = new BinaryReaderX(new MemoryStream(pic)))
+            {
+                for (int i = 0; i < width * height / 64; i++)
+                {
+                    byte[] tmp = br.ReadBytes(64 * (bitDepth / 8));
+                    bool found = false;
+                    int count = 0;
+                    while (found == false && count < parts.Count)
+                    {
+                        if (tmp.SequenceEqual(parts[count]))
+                        {
+                            found = true;
+                            table.Add((short)count);
+                        }
+                        count++;
+                    }
+                    if (!found)
+                    {
+                        table.Add((short)parts.Count);
+                        parts.Add(tmp);
+                    }
+                }
+
+                int resOffset = 0;
+                foreach (var part in parts) part.CopyTo(result,(resOffset+=part.Length)-part.Length);
+            }
+
+            return result;
         }
     }
 }
