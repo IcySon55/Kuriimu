e494791e93b5bb8c00e82f32a4348351424a6fd6
Kirito <alberteinstein1980@gmx.de>
2017-04-13 21:21:26 +0200

    Added MBM support; Added EOV game plugin;

---
+++ file_mbm/MBM.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using Kuriimu.IO;
+using Kuriimu.Contract;
+
+namespace file_mbm
+{
+    public sealed class MBM
+    {
+        public List<Label> Labels = new List<Label>();
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public struct Header
+        {
+            public int zero0;
+            public Magic magic;
+            public int const0;
+            public int fileSize; //Not always correct?
+            public int entryCount;
+            public int entryOffset;
+            public int zero1;
+            public int zero2;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public struct MBMEntry
+        {
+            public int ID;
+            public int stringSize;
+            public int stringOffset;
+            public int padding;
+        }
+
+        public Header header;
+        public List<MBMEntry> entries;
+
+        public MBM(String filename)
+        {
+            using (BinaryReaderX br = new BinaryReaderX(File.OpenRead(filename), true))
+            {
+                header = br.ReadStruct<Header>();
+
+                entries=new List<MBMEntry>();
+                entries.AddRange(br.ReadMultiple<MBMEntry>(header.entryCount).OrderBy(e => e.ID));
+
+                Encoding sjis = Encoding.GetEncoding("SJIS");
+
+                foreach (var entry in entries)
+                {
+                    br.BaseStream.Position = entry.stringOffset;
+                    if (entry.stringOffset!=0)
+                        Labels.Add(new Label
+                        {
+                            Name = "Text " + (entry.ID + 1),
+                            Text = sjis.GetString(br.ReadBytes(entry.stringSize)),
+                            TextID = entry.ID
+                        });
+                }
+            }
+        }
+
+        /*public void Save(String filename)
+        {
+            using (BinaryWriterX bw = new BinaryWriterX(File.Create(filename)))
+            {
+                bw.WriteStruct(header);
+
+                for (int i = 0; i < header.entryCount; i++)
+                {
+                    long bk = bw.BaseStream.Length;
+                    bw.BaseStream.Position += 0xc;
+
+                    byte[] name = writeText(Labels[i].Name, "ascii");
+                    bw.Write(name);
+                    byte[] text = writeText(Labels[i].Text, "sjis");
+                    bw.Write(text);
+
+                    long bk2 = bw.BaseStream.Length;
+                    bw.BaseStream.Position = bk;
+                    bw.Write((int)(bw.BaseStream.Length - bk));
+                    bw.Write(0xc);
+                    bw.Write(0xc + name.Length);
+
+                    bw.BaseStream.Position = bk2;
+                }
+            }
+        }*/
+    }
+}
---
+++ game_etrian_odyssey_v/Handler.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using Cetera.Font;
+using game_etrian_odyssey_v.Properties;
+using Kuriimu.Contract;
+
+namespace game_etrian_odyssey_v
+{
+    public class Handler : IGameHandler
+    {
+        #region Properties
+
+        // Information
+        public string Name => "Etrian Odyssey V";
+        public Image Icon => Resources.icon;
+
+        // Feature Support
+        public bool HandlerHasSettings => true;
+        public bool HandlerCanGeneratePreviews => true;
+
+        #endregion
+
+        Dictionary<string, string> _pairs = new Dictionary<string, string>
+        {
+
+        };
+
+        BCFNT font;
+
+        public Handler()
+        {
+            var ms = new MemoryStream();
+            new MemoryStream(Resources.MainFont).CopyTo(ms);
+            ms.Position = 0;
+            font = new BCFNT(ms);
+        }
+
+        public string GetKuriimuString(string rawString)
+        {
+            return _pairs.Aggregate(rawString, (str, pair) => str.Replace(pair.Key, pair.Value));
+        }
+
+        public string GetRawString(string kuriimuString)
+        {
+            return _pairs.Aggregate(kuriimuString, (str, pair) => str.Replace(pair.Value, pair.Key));
+        }
+
+        Bitmap textBox = new Bitmap(Resources.blank_top);
+
+        // Previewer
+        public IList<Bitmap> GeneratePreviews(IEntry entry)
+        {
+            var pages = new List<Bitmap>();
+            if (entry == null) return pages;
+
+            string kuriimuString = GetKuriimuString(entry.EditedText);
+            int boxes = kuriimuString.Count(c => c == (char)0x17) + 1;
+
+            Bitmap img = new Bitmap(textBox.Width, textBox.Height * boxes);
+
+            using (Graphics gfx = Graphics.FromImage(img))
+            {
+                gfx.SmoothingMode = SmoothingMode.HighQuality;
+                gfx.InterpolationMode = InterpolationMode.Bicubic;
+                gfx.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                for (int i = 0; i < boxes; i++)
+                    gfx.DrawImage(textBox, 0, i * textBox.Height);
+
+                RectangleF rectText = new RectangleF(10, 10, 370, 100);
+
+                Color colorDefault = Color.FromArgb(255, 255, 255, 255);
+
+                float scaleDefault = 1.0f;
+                //float scaleName = 0.86f;
+                float scaleCurrent = scaleDefault;
+                float x = rectText.X, pX = x;
+                float y = rectText.Y, pY = y;
+                //float yAdjust = 4;
+                //int box = 0;
+
+                //font.SetTextColor(colorDefault);
+
+                for (int i = 0; i < kuriimuString.Length; i++)
+                {
+                    /*if (kuriimuString[i] == ' ')
+                    {
+                        x += 5;
+                    }
+                    else if (kuriimuString[i] == '\xa')
+                    {
+                        x = rectText.X;
+                        y += 15;
+                    }
+                    else
+                    {*/
+                        var info = font.GetWidthInfo(kuriimuString[i]);
+                        x += info.left;
+                        font.Draw(kuriimuString[i], gfx, x, y, scaleDefault, scaleDefault);
+                        x += info.glyph_width - info.left;
+                    //}
+                }
+            }
+
+            pages.Add(img);
+
+            return pages;
+        }
+
+        public bool ShowSettings(Icon icon) => false;
+    }
+}
