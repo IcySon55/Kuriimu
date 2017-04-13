de559e20ab2da58fb238b6de407b11beeea991b6
Kirito <alberteinstein1980@gmx.de>
2017-04-14 01:09:57 +0200

    Add CTPK unfinished image plugin

---
+++ image_ctpk/CtpkAdapter.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using Kuriimu.Contract;
+using Kuriimu.IO;
+
+namespace image_ctpk
+{
+    public sealed class CtpkAdapter : IImageAdapter
+    {
+        private FileInfo _fileInfo = null;
+        private CTPK _stex = null;
+
+        public string Name => "CTPK";
+        public string Description => "CTR Texture PaCkage";
+        public string Extension => "*.ctpk";
+        public string About => "This is the CTPK file adapter for Kukkii.";
+
+        // Feature Support
+        public bool FileHasExtendedProperties => false;
+        public bool CanSave => false;
+        public FileInfo FileInfo
+        {
+            get
+            {
+                return _fileInfo;
+            }
+            set
+            {
+                _fileInfo = value;
+            }
+        }
+
+        public bool Identify(string filename)
+        {
+            using (var br = new BinaryReaderX(File.OpenRead(filename)))
+            {
+                if (br.BaseStream.Length < 4) return false;
+                return br.ReadString(4) == "CTPK";
+            }
+        }
+
+        public LoadResult Load(string filename)
+        {
+            LoadResult result = LoadResult.Success;
+
+            _fileInfo = new FileInfo(filename);
+
+            if (_fileInfo.Exists)
+                _stex = new CTPK(_fileInfo.FullName);
+            else
+                result = LoadResult.FileNotFound;
+
+            return result;
+        }
+
+        public SaveResult Save(string filename = "")
+        {
+            SaveResult result = SaveResult.Success;
+
+            if (filename.Trim() != string.Empty)
+                _fileInfo = new FileInfo(filename);
+
+            try
+            {
+                //_stex.Save(_fileInfo.FullName, _stex.bmp);
+            }
+            catch (Exception)
+            {
+                result = SaveResult.Failure;
+            }
+
+            return result;
+        }
+
+        // Bitmaps
+        public Bitmap Bitmap
+        {
+            get
+            {
+                return _stex.bmp;
+            }
+            set
+            {
+                _stex.bmp = value;
+            }
+        }
+    }
+}
---
+++ image_ctpk/CtpkSupport.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kuriimu.Contract;
+using Cetera.Image;
+
+namespace image_ctpk
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public class Header
+    {
+        Magic magic = "CTPK";
+        short version = 0x1;
+        public short texCount = 0;
+        public int texSecOffset = 0;
+        public int texSecSize = 0;
+        public int crc32SecOffset = 0;
+        public int texInfoOffset = 0;
+        int zero0 = 0;
+        int zero1 = 0;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct Entry
+    {
+        public int nameOffset;
+        public int texDataSize;
+        public int texOffset;
+        public int imageFormat;
+        public short width;
+        public short height;
+        public byte mipLvl;
+        public byte type;
+        public short zero0;
+        public int bitmapSizeOffset;
+        public uint timeStamp;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public class HashEntry
+    {
+        public int crc32;
+        public int entryNr;
+    }
+}
