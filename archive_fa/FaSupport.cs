a764cd102b90ceb0919d694b11415d1ab16fb55b
Kirito <alberteinstein1980@gmx.de>
2017-04-15 15:48:34 +0200

    Added fa archive support; Minnor adjustment to HPIHPB;

---
+++ archive_fa/FaSupport.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kuriimu.Contract;
+using Cetera.Image;
+
+namespace archive_fa
+{
+    public class FAFileInfo : ArchiveFileInfo
+    {
+        public Entry Entry;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct Header
+    {
+        public Magic magic;
+        public int offset0;
+        public int offset1;
+        public int entryOffset;
+        public uint nameOffset;
+        public uint dataOffset;
+        public short unk0;
+		public short folderCount;
+        public int entryCount;
+        public uint infoSecSize; //without header 0x48
+        public int zero0;
+
+        public uint unk1;
+        public uint unk2;
+        public uint unk3;
+        public uint unk4;
+
+        public uint unk5;
+        public int entryCount2;
+        public uint unk6;
+        public int zero1;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct Entry
+    {
+        public uint crc32;
+        public uint nameOffset;
+        public uint fileOffset;
+        public uint fileSize;
+    }
+}
