4f52bbf304a9dfec0db49e4eb7c9dc2af3580055
Kirito <alberteinstein1980@gmx.de>
2017-04-16 15:06:10 +0200

    Added CTXB Load function;

--- image_ctpk/CTPK.cs
+++ image_ctpk/CTPK.cs
@@ -94,70 +94,13 @@ namespace image_ctpk
             }
         }
 
+        /*Save not supported until Bitmap[] is introduced*/
+
         /*public void Save(String filename)
         {
             using (BinaryWriterX bw = new BinaryWriterX(File.OpenWrite(filename)))
             {
-                //get nameList Length
-                int nameListLength = 0;
-                for (int i = 0; i < Textures.Count; i++) nameListLength += Textures[i].FileName.Length + 1;
-                while (nameListLength % 4 != 0) nameListLength += 1;
-
-                //Offsets
-                int nameOffset = (Textures.Count + 1) * 0x20 + Textures.Count * 0x4;
-                int dataOffset = nameOffset + nameListLength + Textures.Count * 0x4 * 2;
-
-                //Header
-                bw.WriteStruct(new Header
-                {
-                    texCount = (short)Textures.Count,
-                    texSecOffset = dataOffset,
-                    crc32SecOffset = nameOffset + nameListLength,
-                    texInfoOffset = nameOffset + nameListLength + Textures.Count * 0x4
-                });
-
-                //entryList
-                for (int i = 0; i < Textures.Count; i++)
-                {
-                    bw.WriteStruct(new Entry
-                    {
-                        nameOffset = nameOffset,
-                        texDataSize = (int)Textures[i].FileData.Length,
-                        texOffset = dataOffset,
-                        format = Textures[i].Entry.format,
-                        width = Textures[i].Entry.width,
-                        height = Textures[i].Entry.height,
-                        mipLvl = Textures[i].Entry.mipLvl,
-                        type = Textures[i].Entry.type,
-                        bitmapSizeOffset = Textures[i].Entry.bitmapSizeOffset,
-                        timeStamp = Textures[i].Entry.timeStamp,
-                    });
-                    nameOffset += Textures[i].FileName.Length + 1;
-                    dataOffset += (int)Textures[i].FileData.Length;
-                }
-
-                //texInfo 1 List
-                for (int i = 0; i < Textures.Count; i++) bw.Write((int)Textures[i].FileData.Length);
-
-                //nameList
-                for (int i = 0; i < Textures.Count; i++) { bw.WriteASCII(Textures[i].FileName); bw.Write((byte)0); }
-                while (bw.BaseStream.Position % 4 != 0) bw.BaseStream.Position++;
-
-                //crc32List
-                for (int i = 0; i < Textures.Count; i++) { bw.Write(Textures[i].hashEntry.crc32); bw.Write(Textures[i].hashEntry.entryNr); }
-
-                //texInfo 2 List
-                for (int i = 0; i < Textures.Count; i++) bw.Write(Textures[i].texInfo);
-
-                //Write data
-                int texSecSize = 0;
-                for (int i = 0; i < Textures.Count; i++)
-                {
-                    bw.Write(new BinaryReaderX(Textures[i].FileData, true).ReadBytes((int)Textures[i].FileData.Length));
-                    texSecSize += (int)Textures[i].FileData.Length;
-                }
-                bw.BaseStream.Position = 0xc;
-                bw.Write(texSecSize);
+                
             }
         }*/
     }
---
+++ image_ctxb/CTXB.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Drawing;
+using Kuriimu.IO;
+using Kuriimu.Contract;
+using Cetera.Image;
+
+/*Original image and data types by xdaniel and his tool Tharsis
+ * https://github.com/xdanieldzd/Tharsis */
+
+namespace image_ctxb
+{
+    public class CTXB
+    {
+        public enum DataTypes : uint
+        {
+            Byte = 0x1400,
+            UByte = 0x1401,
+            Short = 0x1402,
+            UShort = 0x1403,
+            Int = 0x1404,
+            UInt = 0x1405,
+            Float = 0x1406,
+            UnsignedByte44DMP = 0x6760,
+            Unsigned4BitsDMP = 0x6761,
+            UnsignedShort4444 = 0x8033,
+            UnsignedShort5551 = 0x8034,
+            UnsignedShort565 = 0x8363
+        };
+
+        public enum Format : uint
+        {
+            RGBA8888 = 0x6752,
+            RGB888 = 0x6754,
+            A8 = 0x6756,
+            L8 = 0x6757,
+            LA44 = 0x6758,
+            ETC1 = 0x675A,
+            ETC1A4 = 0x675B
+        };
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public struct Header
+        {
+            public Magic magic; //ctxb
+            public int fileSize;
+            public int chunkCount;
+            public int zero0;
+            public int chunkOffset;
+            public int texDataOffset;
+        }
+
+        public class Chunk
+        {
+            public Chunk(Stream input)
+            {
+                using (var br = new BinaryReaderX(input,true))
+                {
+                    magic = br.ReadStruct<Magic>();
+                    chunkSize = br.ReadInt32();
+                    texCount = br.ReadInt32();
+
+                    textures = br.ReadMultiple<TexEntry>(texCount);
+                }
+            }
+            public Magic magic; //tex
+            public int chunkSize;
+            public int texCount;
+            public List<TexEntry> textures;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public struct TexEntry
+        {
+            public int dataLength;
+            public short unk0;
+            public short unk1;
+            public ushort width;
+            public ushort height;
+            public Format imageFormat;
+        }
+
+        public Header header;
+        List<Chunk> chunks=new List<Chunk>();
+
+        public Bitmap bmp;
+        public ImageSettings settings;
+
+        public CTXB(Stream input)
+        {
+            using (var br = new BinaryReaderX(input))
+            {
+                //Header
+                header = br.ReadStruct<Header>();
+
+                //Chunks
+                for(int i=0;i<header.chunkCount;i++)
+                    chunks.Add(new Chunk(br.BaseStream));
+
+                settings = new ImageSettings
+                {
+                    Width = chunks[0].textures[0].width,
+                    Height = chunks[0].textures[0].height,
+                    Format=ImageSettings.ConvertFormat(chunks[0].textures[0].imageFormat)
+                };
+                br.BaseStream.Position = header.texDataOffset;
+                bmp = Common.Load(br.ReadBytes(chunks[0].textures[0].dataLength), settings);
+            }
+        }
+
+        /*Save not supported until Bitmap[] is introduced*/
+
+        /*public void Save(String filename, Bitmap bitmap)
+        {
+            using (BinaryWriterX bw = new BinaryWriterX(File.Create(filename)))
+            {
+
+            }
+        }*/
+    }
+}
