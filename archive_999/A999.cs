fc33635365713a6aa75ec207bc8566059ef3d05d
IcySon55 <icyson55@gmail.com>
2017-04-29 01:22:28 -0400

    More using cleanups for A999.

--- archive_999/A999.cs
+++ archive_999/A999.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Kuriimu.Contract;
 using Kuriimu.IO;
 
