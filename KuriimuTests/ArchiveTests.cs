70e8820995eda69157d2b75a261991a84da8e330
Kirito <alberteinstein1980@gmx.de>
2017-05-15 23:15:16 +0200

    Added archiveTest for plainMSBT archive;

--- KuriimuTests/ArchiveTests.cs
+++ KuriimuTests/ArchiveTests.cs
@@ -51,6 +51,10 @@ namespace KuriimuTests
         [TestMethod]
         public void HpiHpbTest3() => HpiHpbTest("mori5.hpi");
 
+        //PlainMSBT Archive
+        [TestMethod]
+        public void PlainMSBTTest() => Test<archive_umsbt.UmsbtManager>("EU_English.LZ.decomp");
+
         //GAR
         [TestMethod]
         public void GarTest() => Test<archive_gar.GarManager>("Joker.Main.gar");
