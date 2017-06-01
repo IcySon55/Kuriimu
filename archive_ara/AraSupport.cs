e86c85e139399ca94c47ae4a3a6fc0590a5cac83
IcySon55 <icyson55@gmail.com>
2017-06-01 02:39:13 -0400

    Implemented an archive plugin for Angelique Retour PSP. (Lacking Save)

---
+++ archive_ara/AraSupport.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.InteropServices;
+using Kuriimu.Contract;
+
+namespace archive_ara
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public sealed class Header
+    {
+        public Magic Magic;
+        public uint Padding1;
+        public uint FileCount;
+        public uint Unknown1;
+        public uint FilenamesOffset;
+        public uint Unknown2;
+        public ulong Padding2;
+    }
+
+    public class AraFileEntry
+    {
+        public uint FilenameOffset = 0;
+        public string Filename = "";
+        public uint Offset = 0;
+        public uint Filesize = 0;
+        public uint Unk1 = 0;
+        public uint Unk2 = 0;
+    }
+
+    public class AraFileInfo : ArchiveFileInfo
+    {
+        public AraFileEntry Entry;
+    }
+}
