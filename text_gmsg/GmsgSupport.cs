f25f1f5eed6b694a95c8d074b3411be3010b35dd
Kirito <alberteinstein1980@gmx.de>
2017-06-18 23:14:51 +0200

    Added GMSG Load and Save; (Contains one bug)

---
+++ text_gmsg/GmsgSupport.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Kuriimu.Contract;
+
+namespace text_gmsg
+{
+    #region Entry_Definition
+    public sealed class Entry : TextEntry
+    {
+        // Interface
+        public string Name
+        {
+            get { return EditedLabel.Name; }
+            set { }
+        }
+
+        public string OriginalText => OriginalLabel.Text;
+
+        public string EditedText
+        {
+            get { return EditedLabel.Text; }
+            set { EditedLabel.Text = value; }
+        }
+
+        public int MaxLength { get; set; }
+
+        public TextEntry ParentEntry { get; set; }
+
+        public bool IsSubEntry => ParentEntry != null;
+
+        public bool HasText { get; }
+
+        public List<TextEntry> SubEntries { get; set; }
+
+        // Adapter
+        public Label OriginalLabel { get; }
+        public Label EditedLabel { get; set; }
+
+        public Entry()
+        {
+            OriginalLabel = new Label();
+            EditedLabel = new Label();
+
+            Name = string.Empty;
+            MaxLength = 0;
+            ParentEntry = null;
+            HasText = true;
+            SubEntries = new List<TextEntry>();
+        }
+
+        public Entry(Label editedLabel) : this()
+        {
+            if (editedLabel != null)
+                EditedLabel = editedLabel;
+        }
+
+        public Entry(Label editedLabel, Label originalLabel) : this(editedLabel)
+        {
+            if (originalLabel != null)
+                OriginalLabel = originalLabel;
+        }
+
+        public override string ToString()
+        {
+            return Name == string.Empty ? "" : Name;
+        }
+    }
+    #endregion
+
+    #region Label_Definition
+    public sealed class Label
+    {
+        public string Name;
+        public string Text;
+        public int TextID;
+
+        public Label()
+        {
+            Name = string.Empty;
+            Text = string.Empty;
+            TextID = 0;
+        }
+    }
+    #endregion
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public class Header
+    {
+        public Magic magic;
+        public uint version;
+        public int entryCount;
+        public uint unk1;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public class GMSGEntry
+    {
+        public short id;
+        public short padding;
+        public ulong unk1;
+        public uint stringOffset;
+        public int stringLength;
+    }
+}
