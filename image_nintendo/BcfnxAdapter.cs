e46b3891d718af1f48ff0e565b4550cf9e846df3
IcySon55 <icyson55@gmail.com>
2017-06-20 09:45:59 -0400

    Updated the BCFNX adapter to the new standards.

--- image_nintendo/BcfnxAdapter.cs
+++ image_nintendo/BcfnxAdapter.cs
@@ -10,7 +10,8 @@ namespace image_nintendo.BCFNX
 {
     public class BcfnxAdapter : IImageAdapter
     {
-        private BCFNT _bcfnx = null;
+        private BCFNT _bcfnx;
+        private List<BitmapInfo> _bitmaps;
 
         #region Properties
 
@@ -43,7 +44,10 @@ namespace image_nintendo.BCFNX
             FileInfo = new FileInfo(filename);
 
             if (FileInfo.Exists)
+            {
                 _bcfnx = new BCFNT(FileInfo.OpenRead());
+                _bitmaps = _bcfnx.bmps.Select(o => new BitmapInfo {Bitmap = o}).ToList();
+            }
         }
 
         public void Save(string filename = "")
@@ -59,7 +63,7 @@ namespace image_nintendo.BCFNX
         }
 
         // Bitmaps
-        public IList<BitmapInfo> Bitmaps => _bcfnx.bmps.Select(o => new BitmapInfo { Bitmap = o }).ToList();
+        public IList<BitmapInfo> Bitmaps => _bitmaps;
 
         public bool ShowProperties(Icon icon) => false;
     }
