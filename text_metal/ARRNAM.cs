b7f72c73f5e96677138cf892d88a49dc775c2f98
IcySon55 <icyson55@gmail.com>
2017-06-23 02:02:19 -0400

    Finished ARR/NAM saving, now with duplicate optimization.

--- text_metal/ARRNAM.cs
+++ text_metal/ARRNAM.cs
@@ -11,22 +11,25 @@ namespace text_metal
     {
         public List<Entry> Entries = new List<Entry>();
 
-        private List<ArrEntry> ArrEntries = new List<ArrEntry>();
-
         public ARRNAM(Stream namInput, Stream arrInput)
         {
+            List<ArrEntry> arrEntries;
+
             using (var br = new BinaryReaderX(arrInput, false))
             {
-                ArrEntries = br.ReadMultiple<ArrEntry>((int)br.BaseStream.Length / 12);
+                arrEntries = br.ReadMultiple<ArrEntry>((int)br.BaseStream.Length / 12);
             }
 
             using (var br = new BinaryReaderX(namInput, false))
             {
-                for (int i = 0; i < ArrEntries.Count; i++)
+                var lastOffset = -1;
+                
+                for (int i = 0; i < arrEntries.Count; i++)
                 {
-                    var arrEntry = ArrEntries[i];
+                    var arrEntry = arrEntries[i];
                     br.BaseStream.Position = arrEntry.Offset;
 
+                    var bad = false;
                     var chars = br.ReadBytes(2);
                     var text = Encoding.Unicode.GetString(chars);
                     while (!chars.SequenceEqual(new byte[] { 0, 0 }))
@@ -38,10 +41,15 @@ namespace text_metal
                             break;
                     }
 
+                    if (arrEntry.Offset > lastOffset)
+                        lastOffset = arrEntry.Offset;
+                    else
+                        bad = true;
+
                     var entry = new Entry
                     {
                         ArrEntry = arrEntry,
-                        Index = i + 1,
+                        Index = i + 1 + (bad ? 1000 : 0),
                         Text = text.TrimEnd('\0')
                     };
 
@@ -52,14 +60,22 @@ namespace text_metal
 
         public void Save(Stream namOutput, Stream arrOutput)
         {
+            var added = new Dictionary<string, short>();
+
             using (var bw = new BinaryWriterX(namOutput))
                 foreach (var entry in Entries)
                 {
                     var arrEntry = entry.ArrEntry;
-                    arrEntry.Offset = (short)bw.BaseStream.Position;
 
-                    bw.Write(Encoding.Unicode.GetBytes(entry.Text));
-                    bw.Write(new byte[] { 0, 0 });
+                    if (!added.ContainsKey(entry.Text))
+                    {
+                        arrEntry.Offset = (short)bw.BaseStream.Position;
+                        added.Add(entry.Text, arrEntry.Offset);
+                        bw.Write(Encoding.Unicode.GetBytes(entry.Text));
+                        bw.Write(new byte[] { 0, 0 });
+                    }
+                    else
+                        arrEntry.Offset = added[entry.Text];
                 }
 
             using (var bw = new BinaryWriterX(arrOutput))
