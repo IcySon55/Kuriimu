1cb3a4937bc67984bf2147209221f289a81999b1
IcySon55 <icyson55@gmail.com>
2017-06-29 11:16:00 -0400

    Added a new text plugin for DBZ Heroes.

---
+++ text_heroes/CSV.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Kuriimu.IO;
+
+namespace text_heroes
+{
+    public sealed class CSV
+    {
+        public List<Entry> Entries = new List<Entry>();
+
+        public CSV(Stream input)
+        {
+            using (var br = new BinaryReaderX(input, false))
+            {
+               
+            }
+        }
+
+        public void Save(Stream output)
+        {
+            using (var bw = new BinaryWriterX(output, false))
+            {
+
+            }
+        }
+    }
+}
