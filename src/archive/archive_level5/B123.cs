2786630a519dac973c76a3f3c56074e90972211d
Kirito <alberteinstein1980@gmx.de>
2017-08-05 23:05:02 +0200

    Revised B123 Load/Save;

--- src/archive/archive_level5/B123.cs
+++ src/archive/archive_level5/B123.cs
@@ -14,12 +14,13 @@ namespace archive_level5.B123
         public List<B123FileInfo> Files = new List<B123FileInfo>();
         Stream _stream = null;
 
-        private Header header;
-        private byte[] unk1;
-        private byte[] unk2;
-
-        private List<Entry> entries;
+        public Header header;
+        public byte[] table1;
+        public byte[] table2;
+        public byte[] nameC;
+        private List<FileEntry> entries;
         private List<string> fileNames;
+
         private List<string> dirStruct = new List<string>();
         private List<int> folderCounts = new List<int>();
 
@@ -31,172 +32,126 @@ namespace archive_level5.B123
                 //Header
                 header = br.ReadStruct<Header>();
 
-                //unknown lists
-                unk1 = br.ReadBytes(header.offset1 - header.offset0);
-                unk2 = br.ReadBytes(header.entryOffset - header.offset1);
+                //Table 1
+                br.BaseStream.Position = header.offset1;
+                table1 = br.ReadBytes((int)(header.offset2 - header.offset1));
 
-                //Entries
-                entries = br.ReadMultiple<Entry>(header.fileCount);
+                //Table 2
+                br.BaseStream.Position = header.offset2;
+                table2 = br.ReadBytes((int)(header.fileEntriesOffset - header.offset2));
 
-                //Names
+                //File Entry Table
+                br.BaseStream.Position = header.fileEntriesOffset;
+                entries = new BinaryReaderX(new MemoryStream(br.ReadBytes((int)(header.nameOffset - header.fileEntriesOffset))))
+                  .ReadMultiple<FileEntry>(header.fileEntriesCount);
+
+                //NameTable
                 br.BaseStream.Position = header.nameOffset;
+                nameC = br.ReadBytes((int)(header.dataOffset - header.nameOffset));
+                fileNames = GetFileNames(nameC);
 
-                string currentFolder = "";
-                br.BaseStream.Position++;
-                string tmp = ReadString(br.BaseStream);
-                br.BaseStream.Position--;
-                fileNames = new List<string>();
-                folderCounts.Add(0);
-
-                while (tmp != "" && br.BaseStream.Position < header.dataOffset)
+                //Add Files
+                List<uint> offsets = new List<uint>();
+                foreach (var name in fileNames)
                 {
-                    if (tmp.Last() == '/')
+                    var crc32 = Crc32.Create(name.Split('/').Last().ToLower());
+                    var entry = entries.Find(c => c.crc32 == crc32 && !offsets.Contains(c.fileOffset));
+                    offsets.Add(entry.fileOffset);
+                    Files.Add(new B123FileInfo
                     {
-                        folderCounts.Add(0);
-                        dirStruct.Add(tmp);
-                        currentFolder = tmp;
+                        State = ArchiveFileState.Archived,
+                        FileName = name,
+                        FileData = new SubStream(br.BaseStream, header.dataOffset + entry.fileOffset, entry.fileSize),
+                        entry = entry
+                    });
+                }
+            }
+        }
+
+        public List<string> GetFileNames(byte[] namePart)
+        {
+            List<string> names = new List<string>();
+
+            using (var br = new BinaryReaderX(new MemoryStream(namePart)))
+            {
+                string currentDir = "";
+                br.BaseStream.Position = 1;
+
+                while (br.BaseStream.Position < br.BaseStream.Length)
+                {
+                    string tmpString = br.ReadCStringSJIS();
+                    if (tmpString[tmpString.Length - 1] == '/')
+                    {
+                        currentDir = tmpString;
                     }
                     else
                     {
-                        dirStruct.Add(currentFolder + tmp);
-                        fileNames.Add(currentFolder + tmp);
-                        folderCounts[folderCounts.Count - 1] += 1;
-                    }
-
-                    br.BaseStream.Position++;
-                    if (br.BaseStream.Position < header.dataOffset)
-                    {
-                        tmp = ReadString(br.BaseStream);
-                        br.BaseStream.Position--;
-                    }
-                }
-
-                //FileData
-                int pos = 0;
-                foreach (var folderCount in folderCounts)
-                {
-                    var tmpFiles = new List<NameEntry>();
-                    for (int i = 0; i < folderCount; i++)
-                        tmpFiles.Add(new NameEntry
-                        {
-                            name = fileNames[pos + i],
-                            crc32 = Crc32.Create(Encoding.GetEncoding("SJIS").GetBytes(fileNames[pos + i].Split('/').Last().ToLower()))
-                        });
-                    tmpFiles = tmpFiles.OrderBy(x => x.crc32).ToList();
-
-                    foreach (var nameEntry in tmpFiles)
-                    {
-                        if (nameEntry.crc32 == entries[pos].crc32)
-                            Files.Add(new B123FileInfo
-                            {
-                                State = ArchiveFileState.Archived,
-                                FileName = nameEntry.name,
-                                FileData = new SubStream(br.BaseStream, entries[pos].fileOffset + header.dataOffset, entries[pos].fileSize),
-                                crc32 = entries[pos++].crc32
-                            });
+                        names.Add(currentDir + tmpString);
                     }
                 }
             }
+
+            return names;
         }
 
         public void Save(Stream output)
         {
-            using (BinaryWriterX bw = new BinaryWriterX(output))
+            using (var bw = new BinaryWriterX(output))
             {
+                int dataOffset = ((0x48 + table1.Length + table2.Length + nameC.Length + entries.Count * 0x10 + 4) + 0x4) & ~0x4;
+
+                //Table 1 & 2
                 bw.BaseStream.Position = 0x48;
+                bw.Write(table1);
+                bw.Write(table2);
 
-                //first unknown half of info section
-                bw.Write(unk1);
-                bw.Write(unk2);
+                //FileEntries Table
+                bw.Write((entries.Count * 0x10) << 3);
 
-                //entryList and Data
-                uint dataOffset = 0;
-                uint movDataOffset = (uint)(0x48 + unk1.Length + unk2.Length + Files.Count * 0x10);
-                foreach (var name in dirStruct) movDataOffset += 1 + (uint)Encoding.GetEncoding("SJIS").GetBytes((name.Last() != '/') ? name.Split('/').Last() : name).Length;
-                while (movDataOffset % 4 != 0) movDataOffset++;
-
-                header.dataOffset = movDataOffset;
-                header.nameOffset = (uint)(0x48 + unk1.Length + unk2.Length + Files.Count * 0x10);
-                header.folderCount = (short)folderCounts.Count;
-                header.fileCount = Files.Count;
-                header.fileCount2 = Files.Count;
-
-                int pos = 0;
-                foreach (var folderCount in folderCounts)
+                uint offset = 0;
+                List<B123FileInfo> files = new List<B123FileInfo>();
+                foreach (var entry in entries)
                 {
-                    var nameSorted = new List<NameEntry>();
-                    for (int i = 0; i < folderCount; i++) nameSorted.Add(new NameEntry { name = Files[pos + i].FileName, crc32 = Files[pos + i].crc32, size = (uint)Files[pos + i].FileSize });
-                    nameSorted = nameSorted.OrderBy(x => x.name, StringComparer.OrdinalIgnoreCase).ToList();
+                    var file = Files.Find(c => c.entry.fileOffset == entry.fileOffset);
+                    files.Add(file);
 
-                    var entriesTmp = new List<Entry>();
-                    uint nameOffset = 0;
-                    for (int i = 0; i < folderCount; i++)
+                    //catch file limits
+                    if (file.FileData.Length > 0xffffffff)
                     {
-                        entriesTmp.Add(new Entry { crc32 = Files[pos + i].crc32 });
+                        throw new Exception("File " + file.FileName + " is too big to pack into this archive type!");
                     }
-                    for (int i = 0; i < folderCount; i++)
+                    else if (offset + dataOffset > 0xffffffff)
                     {
-                        var foundEntry = entriesTmp.Find(x => x.crc32 == nameSorted[i].crc32);
-                        foundEntry.nameOffsetInFolder = nameOffset;
-                        foundEntry.fileOffset = dataOffset;
-                        foundEntry.fileSize = nameSorted[i].size;
-
-                        var t = "";
-                        if (bw.BaseStream.Position == 0x74fc)
-                            t = nameSorted[i].name;
-                        t = "";
-
-                        nameOffset += 1 + (uint)nameSorted[i].name.Split('/').Last().Length;
-
-                        long bk = bw.BaseStream.Position;
-                        bw.BaseStream.Position = movDataOffset;
-                        Files.Find(x => x.FileName == nameSorted[i].name).FileData.CopyTo(bw.BaseStream);
-                        bw.BaseStream.Position++;
-                        while (bw.BaseStream.Position % 4 != 0) bw.BaseStream.Position++;
-                        dataOffset += (uint)bw.BaseStream.Position - movDataOffset;
-                        movDataOffset = (uint)bw.BaseStream.Position;
-                        bw.BaseStream.Position = bk;
-                    }
-                    for (int i = 0; i < folderCount; i++)
-                    {
-                        bw.WriteStruct(entriesTmp[i]);
+                        throw new Exception("The archive can't be bigger than 0xffffffff Bytes.");
                     }
 
-                    pos += folderCount;
+                    //edit entry
+                    entry.fileOffset = offset;
+                    entry.fileSize = (uint)file.FileData.Length;
+
+                    //write entry
+                    bw.WriteStruct(entry);
+
+                    //edit values
+                    offset = (uint)(((offset + file.FileData.Length) + 0x4) & ~0x4);
                 }
 
+                //Nametable
+                bw.Write(nameC);
 
-                //nameList
-                foreach (var name in dirStruct)
+                //Files
+                bw.BaseStream.Position = dataOffset;
+                foreach (var file in files)
                 {
-                    bw.Write((byte)0);
-                    if (name.Last() != '/')
-                        bw.Write(Encoding.GetEncoding("SJIS").GetBytes(name.Split('/').Last()));
-                    else
-                        bw.Write(Encoding.GetEncoding("SJIS").GetBytes(name));
+                    file.FileData.CopyTo(bw.BaseStream);
+                    bw.BaseStream.Position = (bw.BaseStream.Position + 0x4) & ~0x4;
                 }
-                bw.BaseStream.Position++;
-                while (bw.BaseStream.Position % 4 != 0) bw.BaseStream.Position++;
 
-                //Write Header
+                //Header
+                header.nameOffset = (uint)(0x48 + table1.Length + table2.Length + entries.Count * 0x10 + 4);
+                header.dataOffset = (uint)dataOffset;
                 bw.BaseStream.Position = 0;
                 bw.WriteStruct(header);
-            }
-        }
-
-        public string ReadString(Stream input)
-        {
-            using (var br = new BinaryReaderX(input, true))
-            {
-                var result = new List<byte>();
-                var tmp = br.ReadByte();
-                while (tmp != 0x00)
-                {
-                    result.Add(tmp);
-                    tmp = br.ReadByte();
-                }
-
-                return Encoding.GetEncoding("SJIS").GetString(result.ToArray());
             }
         }
 
