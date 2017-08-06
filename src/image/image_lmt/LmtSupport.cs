93f02c7a875096e49636d77e333fde189e58f7f1
Kirito <alberteinstein1980@gmx.de>
2017-08-06 23:35:24 +0200

    Added LMT Load; (only loads PNG, doesn't correct it)

---
+++ src/image/image_lmt/LmtSupport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace image_lmt
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public class Entry
+    {
+        public uint unk1;       //gradually incrementing, no static increment value
+        public uint unk2;
+        public uint unk3;
+        public uint unk4;
+        public uint unk5;
+    }
+}
