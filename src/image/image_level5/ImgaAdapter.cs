0e91980e4f1352d12f468caefe74829ca04b12da
Kirito <alberteinstein1980@gmx.de>
2017-08-08 21:10:34 +0200

    Added IMGA Load/Save;

---
+++ src/image/image_level5/ImgaAdapter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using Kuriimu.Kontract;
+using Kuriimu.IO;
+
+namespace image_level5.imga
+{
+    public sealed class ImgaAdapter : IImageAdapter
+    {
+        private Bitmap _imga = null;
+        private List<BitmapInfo> _bitmaps;
+
+        #region Properties
+
+        public string Name => "IMGA";
+        public string Description => "Level 5 Compressed Image";
+        public string Extension => "*.xi";
+        public string About => "This is the IMGA image adapter for Kukkii.";
+
+        // Feature Support
+        public bool FileHasExtendedProperties => false;
+        public bool CanSave => true;
+
+        public FileInfo FileInfo { get; set; }
+
+        #endregion
+
+        public bool Identify(string filename)
+        {
+            using (var br = new BinaryReaderX(File.OpenRead(filename)))
+            {
+                if (br.BaseStream.Length < 4) return false;
+                return br.ReadString(4) == "IMGA";
+            }
+        }
+
+        public void Load(string filename)
+        {
+            FileInfo = new FileInfo(filename);
+
+            if (FileInfo.Exists)
+            {
+                _imga = IMGA.Load(FileInfo.OpenRead());
+
+                _bitmaps = new List<BitmapInfo> { new BitmapInfo { Bitmap = _imga } };
+            }
+        }
+
+        public void Save(string filename = "")
+        {
+            if (filename.Trim() != string.Empty)
+                FileInfo = new FileInfo(filename);
+
+            _imga = _bitmaps[0].Bitmap;
+            IMGA.Save(FileInfo.FullName, _imga);
+        }
+
+        // Bitmaps
+        public IList<BitmapInfo> Bitmaps => _bitmaps;
+
+        public bool ShowProperties(Icon icon) => false;
+    }
+}
