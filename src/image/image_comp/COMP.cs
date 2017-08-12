f25fceda085b48d18a8552f388a979bfd246415f
Kirito <alberteinstein1980@gmx.de>
2017-08-12 16:04:21 +0200

    Added Load/Save for comp images;

---
+++ src/image/image_comp/COMP.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using Cetera.Image;
+using Kuriimu.IO;
+
+namespace image_comp
+{
+    public class COMP
+    {
+        public List<Bitmap> bmps = new List<Bitmap>();
+
+        public Header header;
+
+        public COMP(Stream input)
+        {
+            using (var br = new BinaryReaderX(input))
+            {
+                //Header
+                header = br.ReadStruct<Header>();
+
+                //add image
+                br.BaseStream.Position = 0x10;
+                var settings = new ImageSettings
+                {
+                    Width = header.width,
+                    Height = header.height,
+                    Format = header.format,
+                    PadToPowerOf2 = false
+                };
+                bmps.Add(Common.Load(br.ReadBytes((int)header.dataSize), settings));
+            }
+        }
+
+        public void Save(string filename)
+        {
+            using (var bw = new BinaryWriterX(File.Create(filename)))
+            {
+                //get texture
+                var settings = new ImageSettings
+                {
+                    Width = bmps[0].Width,
+                    Height = bmps[0].Height,
+                    Format = header.format,
+                    PadToPowerOf2 = false
+                };
+                byte[] tex = Common.Save(bmps[0], settings);
+
+                //Write header
+                header.width = (short)bmps[0].Width;
+                header.height = (short)bmps[0].Height;
+                bw.WriteStruct(header);
+                bw.WriteAlignment();
+
+                //Write image data
+                bw.Write(tex);
+            }
+        }
+    }
+}
---
+++ src/image/image_comp/CompAdapter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using Kuriimu.IO;
+using Kuriimu.Kontract;
+
+namespace image_comp
+{
+    public sealed class CompAdapter : IImageAdapter
+    {
+        private COMP _comp = null;
+        private List<BitmapInfo> _bitmaps;
+
+        #region Properties
+
+        public string Name => "COMP";
+        public string Description => "Generic image format";
+        public string Extension => "*.comp";
+        public string About => "This is the COMP image adapter for Kukkii.";
+
+        // Feature Support
+        public bool FileHasExtendedProperties => false;
+        public bool CanSave => true;
+
+        public FileInfo FileInfo { get; set; }
+
+        #endregion
+
+        public bool Identify(string filename)
+        {
+            using (var br = new BinaryReaderX(File.OpenRead(filename)))
+            {
+                return br.ReadUInt32() + 0x10 == br.BaseStream.Length;
+            }
+        }
+
+        public void Load(string filename)
+        {
+            FileInfo = new FileInfo(filename);
+
+            if (FileInfo.Exists)
+            {
+                _comp = new COMP(FileInfo.OpenRead());
+
+                _bitmaps = _comp.bmps.Select(o => new BitmapInfo { Bitmap = o }).ToList();
+            }
+        }
+
+        public void Save(string filename = "")
+        {
+            if (filename.Trim() != string.Empty)
+                FileInfo = new FileInfo(filename);
+
+            _comp.bmps = _bitmaps.Select(o => o.Bitmap).ToList();
+            _comp.Save(FileInfo.FullName);
+        }
+
+        // Bitmaps
+        public IList<BitmapInfo> Bitmaps => _bitmaps;
+
+        public bool ShowProperties(Icon icon) => false;
+    }
+}
