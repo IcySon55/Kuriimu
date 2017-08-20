281609e69a06136ed84e06d36e677d601077df73
Kirito <alberteinstein1980@gmx.de>
2017-08-20 23:07:02 +0200

    Added Load/Save for Touch Detective archives;

---
+++ src/archive/archive_touch_detective/TD.cs
@@ -0,0 +1,67 @@
+using System.Collections.Generic;
+using System.IO;
+using Kuriimu.Kontract;
+using Kuriimu.IO;
+
+namespace archive_td
+{
+    public class TD
+    {
+        public List<TDFileInfo> Files = new List<TDFileInfo>();
+        private Stream _stream = null;
+
+        public TD(Stream input)
+        {
+            _stream = input;
+            using (var br = new BinaryReaderX(input, true))
+            {
+                //Entries
+                var entryCount = br.ReadInt32();
+                var entries = br.ReadMultiple<Entry>(entryCount);
+
+                //Files
+                var count = 0;
+                foreach (var entry in entries)
+                    Files.Add(new TDFileInfo
+                    {
+                        State = ArchiveFileState.Archived,
+                        FileName = $"{count++:00000000}.bin",
+                        FileData = new SubStream(br.BaseStream, entry.offset * 4, entry.size),
+                        entry = entry
+                    });
+            }
+        }
+
+        public void Save(Stream output)
+        {
+            using (var bw = new BinaryWriterX(output))
+            {
+                //Count
+                bw.Write(Files.Count);
+
+                //Entries
+                uint dataOffset = 4 + (uint)Files.Count * 8;
+                foreach (var file in Files)
+                {
+                    bw.Write(dataOffset / 4);
+                    bw.Write((uint)file.FileSize);
+
+                    dataOffset = (uint)((dataOffset + file.FileSize + 0x3) & ~0x3);
+                }
+
+                //Files
+                foreach (var file in Files)
+                {
+                    file.FileData.CopyTo(bw.BaseStream);
+                    bw.WriteAlignment(4);
+                }
+            }
+        }
+
+        public void Close()
+        {
+            _stream?.Dispose();
+            _stream = null;
+        }
+    }
+}
