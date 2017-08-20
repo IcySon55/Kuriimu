2e20e2d2f760bbb881ed0f858d992f5ffce9534d
DarkSynopsis <gunnut14@gmail.com>
2017-08-20 16:35:15 +1200

    Yokai Watch Game Handler Update
    - Added Furigana Support
    - Add Settings Handler
    - Settings Handler allows custom name
    - Settings Handler switches background between Dialog / Cutscene Bottom
    
    Cutscene Bottom could use some work though, I believe text is always centered in the middle of the screen in the actual game, but for now would allow for seeing more text when needed.

--- src/game/game_yokai_watch/Handler.cs
+++ src/game/game_yokai_watch/Handler.cs
@@ -9,6 +9,12 @@ using Kuriimu.Kontract;
 
 namespace game_yokai_watch
 {
+    public enum Scenes
+    {
+        Dialog,
+        CutsceneBottom
+    }
+
     public class Handler : IGameHandler
     {
         #region Properties
@@ -18,7 +24,7 @@ namespace game_yokai_watch
         public Image Icon => Resources.icon;
 
         // Feature Support
-        public bool HandlerHasSettings => false;
+        public bool HandlerHasSettings => true;
         public bool HandlerCanGeneratePreviews => true;
 
         #endregion
@@ -30,10 +36,11 @@ namespace game_yokai_watch
             ["\x5c\x6e"] = "\r\n",
         };
 
-        Dictionary<string, string> _previewPairs = new Dictionary<string, string>
-        {
-            ["<PNAME>"] = "Nate"
-        };
+        // Old Preview Dictionary
+        //Dictionary<string, string> _previewPairs = new Dictionary<string, string>
+        //{
+        //["<PNAME>"] = Properties.Settings.Default.PlayerName
+        //};
 
         static Lazy<XF> fontInitializer = new Lazy<XF>(() => new XF(new MemoryStream(Resources.ft_nrm)));
         static XF font => fontInitializer.Value;
@@ -49,6 +56,7 @@ namespace game_yokai_watch
         }
 
         Bitmap textBox = new Bitmap(Resources.blank_top);
+        Bitmap csBTM = new Bitmap(Resources.cutscene_bottom);
 
         // Previewer
         public IList<Bitmap> GeneratePreviews(TextEntry entry)
@@ -56,26 +64,64 @@ namespace game_yokai_watch
             var pages = new List<Bitmap>();
             if (entry?.EditedText == null) return pages;
 
-            string kuriimuString = _previewPairs.Aggregate(GetKuriimuString(entry.EditedText), (s, pair) => s.Replace(pair.Key, pair.Value));
+            //string kuriimuString = _previewPairs.Aggregate(GetKuriimuString(entry.EditedText), (s, pair) => s.Replace(pair.Key, pair.Value));
 
-            Bitmap img = new Bitmap(textBox.Width, textBox.Height);
+            // Replaces the need for Preview Dictionary for the time being.
+            string kuriimuString = GetKuriimuString(entry.EditedText).Replace("<PNAME>", Properties.Settings.Default.PlayerName == string.Empty ? "Nate" : Properties.Settings.Default.PlayerName);
+
+            //Bitmap img = new Bitmap(textBox.Width, textBox.Height);
+            Bitmap img = new Bitmap(1, 1);
+
+            switch (Enum.Parse(typeof(Scenes), Properties.Settings.Default.Scene))
+            {
+                case Scenes.Dialog:
+                    img = new Bitmap(textBox.Width, textBox.Height);
+                    break;
+
+                case Scenes.CutsceneBottom:
+                    img = new Bitmap(csBTM.Width, csBTM.Height);
+                    break;
+            }
 
             using (Graphics gfx = Graphics.FromImage(img))
             {
-                gfx.DrawImage(textBox, 0, 0);
-
                 gfx.SmoothingMode = SmoothingMode.HighQuality;
                 gfx.InterpolationMode = InterpolationMode.Bicubic;
                 gfx.PixelOffsetMode = PixelOffsetMode.HighQuality;
 
-                RectangleF rectText = new RectangleF(40, 43, 370, 100);
-
+                //RectangleF rectText = new RectangleF(40, 43, 370, 100);
+                RectangleF rectText = new RectangleF(0, 0, 0, 0);
                 Color colorDefault = Color.FromArgb(255, 0, 0, 0);
+
+                switch (Enum.Parse(typeof(Scenes), Properties.Settings.Default.Scene))
+                {
+                    case Scenes.Dialog:
+                        gfx.DrawImage(textBox, 0, 0);
+                        rectText = new RectangleF(40, 43, 370, 100);
+                        colorDefault = Color.FromArgb(255, 0, 0, 0); // Black
+                        break;
+
+                    case Scenes.CutsceneBottom:
+                        gfx.DrawImage(csBTM, 0, 0);
+                        rectText = new RectangleF(25, 85, 370, 100);
+                        colorDefault = Color.FromArgb(255, 255, 255, 255); // White
+                        break;
+                }
 
                 float scaleDefault = 1.0f;
                 float scaleCurrent = scaleDefault;
                 float x = rectText.X, pX = x;
                 float y = rectText.Y, pY = y;
+                float fontSize = 0.85f;
+
+                bool furigana = false;
+                bool furiganaUpper = false;
+                float furiganaLowerWidth = 0;
+                float furiganaUpperWidth = 0;
+                float lowerOffset = 0;
+                float upperOffset = 0;
+                float bkX = 0;
+
                 for (int i = 0; i < kuriimuString.Length; i++)
                 {
                     if (kuriimuString[i] == '<')
@@ -97,13 +143,90 @@ namespace game_yokai_watch
                     {
                         continue;
                     }
-                    else
+                    else if (kuriimuString[i].ToString() == "[")
                     {
-                        XF.CharacterMap charMap = font.GetCharacterMap(kuriimuString[i], false);
+                        bkX = x;
+                        furigana = true;
 
-                        font.DrawCharacter(kuriimuString[i], colorDefault, gfx, x, y, false);
-                        x += charMap.CharWidth;
+                        //getting char length from lower and upper furigana string in px
+                        furiganaLowerWidth = 0;
+                        furiganaUpperWidth = 0;
+                        int tmp = i + 1;
+                        bool fin = false;
+                        bool furiganaTmp = false;
+                        bool draw = false;
+                        while (fin == false)
+                        {
+                            if (tmp >= kuriimuString.Length)
+                            {
+                                if (fin == false)
+                                {
+                                    draw = true;
+                                }
+                                break;
+                            }
+
+                            if (kuriimuString[tmp].ToString() == "/")
+                            {
+                                furiganaTmp = true;
+                                tmp++;
+                            }
+                            else if (kuriimuString[tmp].ToString() == "]")
+                            {
+                                fin = true;
+                                if (furiganaTmp == false)
+                                {
+                                    draw = true;
+                                }
+                            }
+                            else
+                            {
+                                XF.CharacterMap charMaptmp = font.GetCharacterMap(kuriimuString[tmp++], furiganaTmp);
+                                var t1 = (furiganaTmp) ? furiganaUpperWidth += charMaptmp.CharWidth : furiganaLowerWidth += charMaptmp.CharWidth;
+                            }
+                        }
+
+                        //setting furigana offset
+                        var t2 = (furiganaLowerWidth > furiganaUpperWidth) ? upperOffset = (furiganaLowerWidth - furiganaUpperWidth) / 2 : lowerOffset = (furiganaUpperWidth - furiganaLowerWidth) / 2;
+                        if (furiganaLowerWidth < furiganaUpperWidth)
+                        {
+                            x += lowerOffset;
+                        }
+
+                        if (draw == false)
+                        {
+                            continue;
+                        }
                     }
+                    else if (kuriimuString[i].ToString() == "/")
+                    {
+                        if (furigana)
+                        {
+                            furiganaUpper = true;
+                            y -= 7;
+                            var t3 = (furiganaUpperWidth < furiganaLowerWidth) ? x = bkX + upperOffset : x = bkX;
+                            continue;
+                        }
+                    }
+                    else if (kuriimuString[i].ToString() == "]")
+                    {
+                        if (furiganaUpper)
+                        {
+                            var t4 = (furiganaUpperWidth > furiganaLowerWidth) ? x = bkX + furiganaUpperWidth : x = bkX + furiganaLowerWidth;
+                            y += 7;
+                            furiganaUpper = false;
+                            furigana = false;
+                            upperOffset = 0;
+                            lowerOffset = 0;
+                            continue;
+                        }
+                    }
+
+                    XF.CharacterMap charMap = font.GetCharacterMap(kuriimuString[i], furiganaUpper);
+
+                    //draw regular character
+                    font.DrawCharacter(kuriimuString[i], colorDefault, gfx, x, y, furiganaUpper);
+                    x += charMap.CharWidth-fontSize;
                 }
             }
 
@@ -112,6 +235,11 @@ namespace game_yokai_watch
             return pages;
         }
 
-        public bool ShowSettings(Icon icon) => false;
+        public bool ShowSettings(Icon icon)
+        {
+            var settings = new Settings(icon);
+            settings.ShowDialog();
+            return settings.HasChanges;
+        }
     }
 }
