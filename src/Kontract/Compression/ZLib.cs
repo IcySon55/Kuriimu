22cd7e387938d9cc110336fbf1504feae95ecb96
IcySon55 <icyson55@gmail.com>
2017-08-21 03:43:15 -0400

    Expanded the ZLib methods to allow for customizing the header bytes.

--- src/Kontract/Compression/ZLib.cs
+++ src/Kontract/Compression/ZLib.cs
@@ -8,18 +8,21 @@ namespace Kuriimu.Compression
 {
     public class ZLib
     {
-        public static byte[] Compress(Stream instream)
+        public static byte[] Compress(Stream instream, byte[] header, CompressionLevel compressionLevel = CompressionLevel.Optimal, bool leaveOpen = false)
         {
-            var inData = new BinaryReaderX(instream).ReadBytes((int)instream.Length);
+            var inData = new BinaryReaderX(instream, leaveOpen).ReadBytes((int)instream.Length);
             var ms = new MemoryStream();
-            ms.Write(new byte[] { 0x78, 0xDA }, 0, 2);
-            using (var ds = new DeflateStream(ms, CompressionLevel.Optimal, true))
-            {
+            ms.Write(header, 0, 2);
+            using (var ds = new DeflateStream(ms, compressionLevel, true))
                 ds.Write(inData, 0, inData.Length);
-            }
             var adler = inData.Aggregate(Tuple.Create(1, 0), (x, n) => Tuple.Create((x.Item1 + n) % 65521, (x.Item1 + x.Item2 + n) % 65521));
             ms.Write(new[] { (byte)(adler.Item2 >> 8), (byte)adler.Item2, (byte)(adler.Item1 >> 8), (byte)adler.Item1 }, 0, 4);
             return ms.ToArray();
+        }
+
+        public static byte[] Compress(Stream instream, CompressionLevel compressionLevel = CompressionLevel.Optimal, bool leaveOpen = false)
+        {
+            return Compress(instream, new byte[] { 0x78, 0xDA }, compressionLevel, leaveOpen);
         }
 
         public static byte[] Decompress(Stream inData)
@@ -29,9 +32,7 @@ namespace Kuriimu.Compression
                 var ms = new MemoryStream();
                 br.BaseStream.Position = 2;
                 using (var ds = new DeflateStream(new MemoryStream(br.ReadBytes((int)br.BaseStream.Length - 6)), CompressionMode.Decompress))
-                {
                     ds.CopyTo(ms);
-                }
                 return ms.ToArray();
             }
         }
