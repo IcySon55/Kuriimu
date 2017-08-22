99ca974e23c6c6f335c0865222a79ba0763cd457
Kirito <alberteinstein1980@gmx.de>
2017-08-22 08:03:16 +0200

    Added basic kbd plugin;

---
+++ src/text/text_kbd/KBD.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Kuriimu.IO;
+
+namespace text_kbd
+{
+    public sealed class KBD
+    {
+        public List<Label> Labels = new List<Label>();
+
+        public KBD(string filename)
+        {
+            using (var br = new BinaryReaderX(File.OpenRead(filename)))
+            {
+
+            }
+        }
+
+        public void Save(string filename)
+        {
+            using (var bw = new BinaryWriterX(File.OpenWrite(filename)))
+            {
+
+            }
+        }
+    }
+}
