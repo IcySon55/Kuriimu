42bda925a63ef6d3ff64743cbfa0015b6d938013
Rhainer Zufall <alberteinstein1980@gmx.de>
2017-08-23 11:18:59 +0200

    Add Load/Save ZDP archive;

---
+++ src/archive/archive_zdp/ZdpManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using Kuriimu.Kontract;
+using Kuriimu.IO;
+using System.Text;
+
+namespace archive_zdp
+{
+    public class ZdpManager : IArchiveManager
+    {
+        private ZDP _zdp = null;
+
+        #region Properties
+
+        // Information
+        public string Name => "ZDP";
+        public string Description => "Whatever ZDP should mean";
+        public string Extension => "*.zdp";
+        public string About => "This is the ZDP archive manager for Karameru.";
+
+        // Feature Support
+        public bool ArchiveHasExtendedProperties => false;
+        public bool CanAddFiles => false;
+        public bool CanRenameFiles => false;
+        public bool CanReplaceFiles => true;
+        public bool CanDeleteFiles => false;
+        public bool CanSave => true;
+
+        public FileInfo FileInfo { get; set; }
+
+        #endregion
+
+        public bool Identify(string filename)
+        {
+            using (var br = new BinaryReaderX(File.OpenRead(filename)))
+            {
+                return br.ReadString(8) == "datapack";
+            }
+        }
+
+        public void Load(string filename)
+        {
+            FileInfo = new FileInfo(filename);
+
+            if (FileInfo.Exists)
+                _zdp = new ZDP(FileInfo.OpenRead());
+        }
+
+        public void Save(string filename = "")
+        {
+            if (!string.IsNullOrEmpty(filename))
+                FileInfo = new FileInfo(filename);
+
+            // Save As...
+            if (!string.IsNullOrEmpty(filename))
+            {
+                _zdp.Save(FileInfo.Create());
+                _zdp.Close();
+            }
+            else
+            {
+                // Create the temp file
+                _zdp.Save(File.Create(FileInfo.FullName + ".tmp"));
+                _zdp.Close();
+                // Delete the original
+                FileInfo.Delete();
+                // Rename the temporary file
+                File.Move(FileInfo.FullName + ".tmp", FileInfo.FullName);
+            }
+
+            // Reload the new file to make sure everything is in order
+            Load(FileInfo.FullName);
+        }
+
+        public void Unload()
+        {
+            _zdp?.Close();
+        }
+
+        // Files
+        public IEnumerable<ArchiveFileInfo> Files => _zdp.Files;
+
+        public bool AddFile(ArchiveFileInfo afi) => false;
+
+        public bool DeleteFile(ArchiveFileInfo afi) => false;
+
+        // Features
+        public bool ShowProperties(Icon icon) => false;
+    }
+}
