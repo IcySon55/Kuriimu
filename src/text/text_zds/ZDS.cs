a2183b246d44e55c1783be5281fd12074d3fa518
Rhainer Zufall <alberteinstein1980@gmx.de>
2017-08-23 17:27:24 +0200

    Added Zsd text plugin (no Load/Save);

---
+++ src/text/text_zds/ZDS.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Kuriimu.IO;
+
+namespace text_zds
+{
+    public sealed class ZDS
+    {
+        public List<Label> Labels = new List<Label>();
+
+        public PartitionHeader partHeader;
+        public Header header;
+
+        public ZDS(Stream input)
+        {
+            using (var br = new BinaryReaderX(input))
+            {
+                //Header
+                partHeader = br.ReadStruct<PartitionHeader>();
+                header = br.ReadStruct<Header>();
+
+                //Entries
+                /*var entries = new List<TextClass>();
+                for (int i = 0; i < header.entryCount; i++)
+                {
+                    entries.Add(new TextClass
+                    {
+                        labelOffset = br.ReadUInt32(),
+                        textOffset = br.ReadUInt32()
+                    });
+                    br.BaseStream.Position += 8;
+                }
+
+                //Text
+                for (int i = 0; i < entries.Count; i++)
+                {
+                    int labelSize = (int)((entries[i].textOffset - 1) - entries[i].labelOffset);
+                    var textSize = (i + 1 == entries.Count) ?
+                        (int)((br.BaseStream.Length - 1) - entries[i].textOffset) :
+                        (int)((entries[i + 1].labelOffset - 1) - entries[i].textOffset);
+
+                    br.BaseStream.Position = entries[i].labelOffset;
+                    var label = br.ReadString(labelSize, Encoding.UTF8);
+                    br.BaseStream.Position = entries[i].textOffset;
+                    var text = br.ReadString(textSize, Encoding.UTF8);
+
+                    Labels.Add(new Label
+                    {
+                        Text = text,
+                        Name = label,
+                        TextID = i,
+                        textEntry = entries[i]
+                    });
+                }*/
+            }
+        }
+
+        public void Save(string filename)
+        {
+            using (var bw = new BinaryWriterX(File.OpenWrite(filename)))
+            {
+
+            }
+        }
+    }
+}
---
+++ src/text/text_zds/ZdsSupport.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Kuriimu.Kontract;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace text_zds
+{
+    #region Entry_Definition
+    public sealed class Entry : TextEntry
+    {
+        // Interface
+        public string Name
+        {
+            get { return EditedLabel.Name; }
+            set { }
+        }
+
+        public string OriginalText => OriginalLabel.Text;
+
+        public string EditedText
+        {
+            get { return EditedLabel.Text; }
+            set { EditedLabel.Text = value; }
+        }
+
+        public int MaxLength { get; set; }
+
+        public TextEntry ParentEntry { get; set; }
+
+        public bool IsSubEntry => ParentEntry != null;
+
+        public bool HasText { get; }
+
+        public List<TextEntry> SubEntries { get; set; }
+
+        // Adapter
+        public Label OriginalLabel { get; }
+        public Label EditedLabel { get; set; }
+
+        public Entry()
+        {
+            OriginalLabel = new Label();
+            EditedLabel = new Label();
+
+            Name = string.Empty;
+            MaxLength = 0;
+            ParentEntry = null;
+            HasText = true;
+            SubEntries = new List<TextEntry>();
+        }
+
+        public Entry(Label editedLabel) : this()
+        {
+            if (editedLabel != null)
+                EditedLabel = editedLabel;
+        }
+
+        public Entry(Label editedLabel, Label originalLabel) : this(editedLabel)
+        {
+            if (originalLabel != null)
+                OriginalLabel = originalLabel;
+        }
+
+        public override string ToString()
+        {
+            return Name == string.Empty ? "!NoName!" : Name;
+        }
+    }
+    #endregion
+
+    #region Label_Definition
+    public sealed class Label
+    {
+        public string Name;
+        public string Text;
+        public int TextID;
+        public TextClass textEntry;
+
+        public Label()
+        {
+            Name = string.Empty;
+            Text = string.Empty;
+            TextID = 0;
+            textEntry = null;
+        }
+    }
+    #endregion
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct PartitionHeader
+    {
+        public Magic8 magic;
+        public uint zero1;
+        public uint unk1;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct Header
+    {
+        public uint entryCount;
+        public uint unk1;
+        public uint headerSize;
+        public uint entryOffset;
+        public uint unk2;
+        public uint unk3;
+        public uint unk4;
+        public uint unk5;
+    }
+
+    public class TextClass
+    {
+        public uint offset1;
+        public uint offset2;
+        public uint unk1;
+        public uint unk2;
+    }
+}
