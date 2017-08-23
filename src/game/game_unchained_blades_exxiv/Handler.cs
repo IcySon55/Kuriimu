a690844103c9e48e7f52b7a41573a7843ccbf0f4
DarkSynopsis <gunnut14@gmail.com>
2017-08-23 18:00:56 +1200

    Unchained Blades EXXiV Game Handler
    Quick Game Handler for Unchained Blades EXXiV.

---
+++ src/game/game_unchained_blades_exxiv/Handler.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using Cetera.Font;
+using game_unchained_blades_exxiv.Properties;
+using Kuriimu.Kontract;
+
+namespace game_unchained_blades_exxiv
+{
+    public enum BubbleType
+    {
+        Type1,
+        Type2
+    }
+
+    public class Handler : IGameHandler
+    {
+        #region Properties
+
+        // Information
+        public string Name => "Unchained Blades EXXiV";
+        public Image Icon => Resources.icon;
+
+        // Feature Support
+        public bool HandlerHasSettings => true;
+        public bool HandlerCanGeneratePreviews => true;
+
+        #endregion
+
+        Dictionary<string, string> _pairs = new Dictionary<string, string>
+        {
+
+        };
+
+        Dictionary<string, string> _previewPairs = new Dictionary<string, string>
+        {
+            ["<1>"] = "Player",
+        };
+
+        BCFNT font;
+
+        public Handler()
+        {
+            var ms = new MemoryStream();
+            new MemoryStream(Resources.MainFont).CopyTo(ms);
+            ms.Position = 0;
+            font = new BCFNT(ms);
+        }
+
+        public string GetKuriimuString(string rawString)
+        {
+            return _pairs.Aggregate(rawString, (str, pair) => str.Replace(pair.Key, pair.Value));
+        }
+
+        public string GetRawString(string kuriimuString)
+        {
+            return _pairs.Aggregate(kuriimuString, (str, pair) => str.Replace(pair.Value, pair.Key));
+        }
+
+        Bitmap bubble001 = new Bitmap(Resources.Bubble001);
+        Bitmap bubble002 = new Bitmap(Resources.Bubble002);
+
+        // Previewer
+        public IList<Bitmap> GeneratePreviews(TextEntry entry)
+        {
+            var pages = new List<Bitmap>();
+            if (entry?.EditedText == null) return pages;
+
+            //string kuriimuString = _previewPairs.Aggregate(GetKuriimuString(entry.EditedText), (s, pair) => s.Replace(pair.Key, pair.Value));
+            string kuriimuString = _previewPairs.Aggregate(GetKuriimuString(entry.EditedText), (s, pair) => s.Replace(pair.Key, pair.Value));
+
+            Bitmap img = new Bitmap(1, 1);
+
+            switch (Enum.Parse(typeof(BubbleType), Properties.Settings.Default.BubbleType))
+            {
+                case BubbleType.Type1:
+                    img = new Bitmap(bubble001.Width, bubble001.Height);
+                    break;
+
+                case BubbleType.Type2:
+                    img = new Bitmap(bubble002.Width, bubble002.Height);
+                    break;
+            }
+
+            using (Graphics gfx = Graphics.FromImage(img))
+            {
+                gfx.SmoothingMode = SmoothingMode.HighQuality;
+                gfx.InterpolationMode = InterpolationMode.Bicubic;
+                gfx.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                RectangleF rectText = new RectangleF(0, 0, 0, 0);
+                Color colorDefault = Color.FromArgb(255, 0, 0, 0);
+
+                switch (Enum.Parse(typeof(BubbleType), Properties.Settings.Default.BubbleType))
+                {
+                    case BubbleType.Type1:
+                        gfx.DrawImage(bubble001, 0, 0);
+                        rectText = new RectangleF(12, 9, 370, 100);
+                        break;
+
+                    case BubbleType.Type2:
+                        gfx.DrawImage(bubble002, 0, 0);
+                        rectText = new RectangleF(20, 11, 370, 100);
+                        break;
+                }
+
+                float scaleDefault = 1.0f;
+                float scaleCurrent = scaleDefault;
+                float x = rectText.X, pX = x;
+                float y = rectText.Y, pY = y;
+                //float fontSize = 3.2f;
+                float fontSize = 1.5f;
+
+                for (int i = 0; i < kuriimuString.Length; i++)
+                {
+                    BCFNT.CharWidthInfo widthInfo = font.GetWidthInfo(kuriimuString[i]);
+                    font.SetColor(colorDefault);
+
+                    if (kuriimuString[i] == '\n')
+                    {
+                        switch (Enum.Parse(typeof(BubbleType), Properties.Settings.Default.BubbleType))
+                        {
+                            case BubbleType.Type1:
+                                y += 15;
+                                x = 12;
+                                continue;
+
+                            case BubbleType.Type2:
+                                y += 13;
+                                x = 18;
+                                continue;
+                        }
+                    }
+
+                    font.Draw(kuriimuString[i], gfx, x, y, scaleCurrent, scaleCurrent);
+                    x += widthInfo.char_width - fontSize;
+                }
+            }
+
+            pages.Add(img);
+
+            return pages;
+        }
+
+        public bool ShowSettings(Icon icon)
+        {
+            var settings = new Settings(icon);
+            settings.ShowDialog();
+            return settings.HasChanges;
+        }
+    }
+}
---
+++ src/game/game_unchained_blades_exxiv/Settings.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace game_unchained_blades_exxiv
+{
+    partial class Settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.grpProperties = new System.Windows.Forms.GroupBox();
+            this.cmbScene = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.grpProperties.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOK.Location = new System.Drawing.Point(113, 104);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 3;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(194, 104);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // grpProperties
+            // 
+            this.grpProperties.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grpProperties.Controls.Add(this.cmbScene);
+            this.grpProperties.Controls.Add(this.label2);
+            this.grpProperties.Location = new System.Drawing.Point(12, 12);
+            this.grpProperties.Name = "grpProperties";
+            this.grpProperties.Padding = new System.Windows.Forms.Padding(6);
+            this.grpProperties.Size = new System.Drawing.Size(257, 86);
+            this.grpProperties.TabIndex = 2;
+            this.grpProperties.TabStop = false;
+            this.grpProperties.Text = "Settings";
+            // 
+            // cmbScene
+            // 
+            this.cmbScene.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbScene.FormattingEnabled = true;
+            this.cmbScene.Location = new System.Drawing.Point(94, 22);
+            this.cmbScene.Name = "cmbScene";
+            this.cmbScene.Size = new System.Drawing.Size(154, 21);
+            this.cmbScene.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(16, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Bubble:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Settings
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(281, 139);
+            this.Controls.Add(this.grpProperties);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Settings";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Handler Settings";
+            this.Load += new System.EventHandler(this.Settings_Load);
+            this.grpProperties.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.GroupBox grpProperties;
+        private System.Windows.Forms.ComboBox cmbScene;
+        private System.Windows.Forms.Label label2;
+    }
+}
