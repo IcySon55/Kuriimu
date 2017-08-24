e844b591d6bd1e80cc828fdac420e7b03bc6a8eb
Kirito <alberteinstein1980@gmx.de>
2017-08-24 22:15:50 +0200

    Added LMD Load/Save;

---
+++ src/text/text_lmd/LMD.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Kuriimu.IO;
+
+namespace text_lmd
+{
+    public sealed class LMD
+    {
+        public List<Label> Labels = new List<Label>();
+
+        public Header header;
+        public List<Sec1Entry> sec1entries = new List<Sec1Entry>();
+        public List<byte[]> sec2entries = new List<byte[]>();
+        public string internFilename;
+
+        public LMD(Stream input)
+        {
+            using (var br = new BinaryReaderX(input))
+            {
+                //Header
+                header = br.ReadStruct<Header>();
+
+                //Sec1Entries
+                br.BaseStream.Position = header.entryOffset;
+                sec1entries = br.ReadMultiple<Sec1Entry>((int)header.entryCount);
+
+                //Sec2Entries
+                br.BaseStream.Position = header.sec2Offset;
+                for (int i = 0; i < sec1entries.Count; i++)
+                {
+                    br.BaseStream.Position = header.sec2Offset + sec1entries[i].offset;
+                    var size = (i + 1 == sec1entries.Count) ? (ushort)header.sec2Size - sec1entries[i].offset : sec1entries[i + 1].offset - sec1entries[i].offset;
+                    sec2entries.Add(br.ReadBytes(size));
+                }
+
+                //textEntries
+                br.BaseStream.Position = header.textOffset;
+                var textEntries = br.ReadMultiple<TextClass>((int)header.textCount);
+
+                //Texts
+                for (int i = 0; i < header.textCount; i++)
+                {
+                    br.BaseStream.Position = textEntries[i].offset;
+                    Labels.Add(new Label
+                    {
+                        Text = br.ReadString((int)(textEntries[i].size1 * 2), Encoding.GetEncoding("UTF-16")),
+                        Name = $"text{i:00000000}",
+                        TextID = i,
+                        offset = textEntries[i].offset
+                    });
+                }
+
+                //Filename
+                br.BaseStream.Position = header.fileNameOffset;
+                internFilename = br.ReadCStringA();
+            }
+        }
+
+        public void Save(string filename)
+        {
+            using (var bw = new BinaryWriterX(File.Create(filename)))
+            {
+                //Sec1
+                bw.BaseStream.Position = 0x24;
+                foreach (var sec1entry in sec1entries) bw.WriteStruct(sec1entry);
+
+                //Sec2
+                foreach (var sec2entry in sec2entries) bw.Write(sec2entry);
+                var textEntryOffset = bw.BaseStream.Position;
+
+                //texts
+                bw.BaseStream.Position += Labels.Count * 0xc;
+                foreach (var text in Labels)
+                {
+                    text.offset = (uint)bw.BaseStream.Position;
+                    bw.Write(Encoding.GetEncoding("UTF-16").GetBytes(text.Text));
+                    bw.Write((ushort)0);
+                    bw.WriteAlignment(4);
+                }
+                header.fileNameOffset = (uint)bw.BaseStream.Position;
+
+                //TextEntries
+                bw.BaseStream.Position = textEntryOffset;
+                foreach (var text in Labels)
+                {
+                    bw.WriteStruct(text.offset);
+                    var size = Encoding.GetEncoding("UTF-16").GetByteCount(text.Text) / 2;
+                    bw.Write(size);
+                    bw.Write(size);
+                }
+
+                //Filename
+                bw.BaseStream.Position = header.fileNameOffset;
+                bw.Write(Encoding.ASCII.GetBytes(internFilename));
+                bw.Write((byte)0);
+
+                //Header
+                bw.BaseStream.Position = 0;
+                bw.WriteStruct(header);
+            }
+        }
+    }
+}
---
+++ src/text/text_lmd/LmdSupport.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Kuriimu.Kontract;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace text_lmd
+{
+    #region Entry_Definition
+    public sealed class Entry : TextEntry
+    {
+        // Interface
+        public string Name
+        {
+            get { return EditedLabel.Name; }
+            set { }
+        }
+
+        public string OriginalText => OriginalLabel.Text;
+
+        public string EditedText
+        {
+            get { return EditedLabel.Text; }
+            set { EditedLabel.Text = value; }
+        }
+
+        public int MaxLength { get; set; }
+
+        public TextEntry ParentEntry { get; set; }
+
+        public bool IsSubEntry => ParentEntry != null;
+
+        public bool HasText { get; }
+
+        public List<TextEntry> SubEntries { get; set; }
+
+        // Adapter
+        public Label OriginalLabel { get; }
+        public Label EditedLabel { get; set; }
+
+        public Entry()
+        {
+            OriginalLabel = new Label();
+            EditedLabel = new Label();
+
+            Name = string.Empty;
+            MaxLength = 0;
+            ParentEntry = null;
+            HasText = true;
+            SubEntries = new List<TextEntry>();
+        }
+
+        public Entry(Label editedLabel) : this()
+        {
+            if (editedLabel != null)
+                EditedLabel = editedLabel;
+        }
+
+        public Entry(Label editedLabel, Label originalLabel) : this(editedLabel)
+        {
+            if (originalLabel != null)
+                OriginalLabel = originalLabel;
+        }
+
+        public override string ToString()
+        {
+            return Name == string.Empty ? "!NoName!" : Name;
+        }
+    }
+    #endregion
+
+    #region Label_Definition
+    public sealed class Label
+    {
+        public string Name;
+        public string Text;
+        public int TextID;
+        public uint offset;
+
+        public Label()
+        {
+            Name = string.Empty;
+            Text = string.Empty;
+            TextID = 0;
+            offset = 0;
+        }
+    }
+    #endregion
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct Header
+    {
+        public Magic magic;
+        public uint const1;
+        public uint entryCount;
+        public uint sec2Size;
+        public uint textCount;
+        public uint entryOffset;
+        public uint sec2Offset;
+        public uint textOffset;
+        public uint fileNameOffset;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct Sec1Entry
+    {
+        public uint hash;
+        public ushort offset;
+        public ushort unk1;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct TextClass
+    {
+        public uint offset;
+        public uint size1;
+        public uint size2;
+    }
+}
