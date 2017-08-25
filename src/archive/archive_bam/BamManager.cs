76de5d7592ecc3c593d0593bfc73a281e9b52037
Kirito <alberteinstein1980@gmx.de>
2017-08-25 19:27:55 +0200

    Added BAM Load/Save;

---
+++ src/archive/archive_bam/BamManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using Kuriimu.Kontract;
+using Kuriimu.IO;
+
+namespace archive_bam
+{
+    public class BamManager : IArchiveManager
+    {
+        private BAM _bam = null;
+
+        #region Properties
+
+        // Information
+        public string Name => "BAM";
+        public string Description => "BAM";
+        public string Extension => "*.bam";
+        public string About => "This is the BAM archive manager for Karameru.";
+
+        // Feature Support
+        public bool ArchiveHasExtendedProperties => false;
+        public bool CanAddFiles => false;
+        public bool CanRenameFiles => false;
+        public bool CanReplaceFiles => true;
+        public bool CanDeleteFiles => false;
+        public bool CanSave => true;
+
+        public FileInfo FileInfo { get; set; }
+
+        #endregion
+
+        public bool Identify(string filename)
+        {
+            using (var br = new BinaryReaderX(File.OpenRead(filename)))
+            {
+                return br.ReadString(4) == "ATBC";
+            }
+        }
+
+        public void Load(string filename)
+        {
+            FileInfo = new FileInfo(filename);
+
+            if (FileInfo.Exists)
+                _bam = new BAM(FileInfo.OpenRead());
+        }
+
+        public void Save(string filename = "")
+        {
+            if (!string.IsNullOrEmpty(filename))
+                FileInfo = new FileInfo(filename);
+
+            // Save As...
+            if (!string.IsNullOrEmpty(filename))
+            {
+                _bam.Save(FileInfo.Create());
+                _bam.Close();
+            }
+            else
+            {
+                // Create the temp file
+                _bam.Save(File.Create(FileInfo.FullName + ".tmp"));
+                _bam.Close();
+                // Delete the original
+                FileInfo.Delete();
+                // Rename the temporary file
+                File.Move(FileInfo.FullName + ".tmp", FileInfo.FullName);
+            }
+
+            // Reload the new file to make sure everything is in order
+            Load(FileInfo.FullName);
+        }
+
+        public void Unload()
+        {
+            _bam?.Close();
+        }
+
+        // Files
+        public IEnumerable<ArchiveFileInfo> Files => _bam.Files;
+
+        public bool AddFile(ArchiveFileInfo afi) => false;
+
+        public bool DeleteFile(ArchiveFileInfo afi) => false;
+
+        // Features
+        public bool ShowProperties(Icon icon) => false;
+    }
+}
---
+++ src/archive/archive_bam/BamSupport.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+using Kuriimu.Kontract;
+
+namespace archive_bam
+{
+    public class BAMFileInfo : ArchiveFileInfo
+    {
+
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public class Header
+    {
+        public Magic magic;
+        public uint fileSize;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public class FileHeader
+    {
+        public Magic8 magic;
+        public uint size;
+    }
+}
