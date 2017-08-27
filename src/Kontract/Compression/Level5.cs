a15543ac38026c0e764191024f5b4b9e6d13d9b7
Kirito <alberteinstein1980@gmx.de>
2017-08-27 03:26:25 +0200

    Added Level5 RLE and LZSS compression;

--- src/Kontract/Compression/Level5.cs
+++ src/Kontract/Compression/Level5.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Text;
 using Kuriimu.IO;
+using System.Linq;
 
 namespace Kuriimu.Compression
 {
@@ -44,15 +45,41 @@ namespace Kuriimu.Compression
 
         public static byte[] Compress(Stream stream, Method method)
         {
+            uint methodSize = 0;
             switch (method)
             {
                 case Method.NoCompression:
-                    var methodSize = (uint)stream.Length << 3;
+                case Method.Huffman4Bit:
+                case Method.Huffman8Bit:
+                    methodSize = (uint)stream.Length << 3;
                     using (var bw = new BinaryWriterX(new MemoryStream()))
                     {
                         bw.Write(methodSize);
                         stream.Position = 0;
                         stream.CopyTo(bw.BaseStream);
+                        bw.BaseStream.Position = 0;
+                        return new BinaryReaderX(bw.BaseStream).ReadBytes((int)bw.BaseStream.Length);
+                    }
+                case Method.LZSS:
+                    methodSize = (uint)(stream.Length << 3 | 0x1);
+                    using (var bw = new BinaryWriterX(new MemoryStream()))
+                    {
+                        bw.Write(methodSize);
+                        stream.Position = 0;
+                        var comp = LZ10.Compress(stream);
+                        for (int i = 0; i < 4; i++) comp.ToList().RemoveAt(0);
+                        bw.Write(comp);
+                        bw.BaseStream.Position = 0;
+                        return new BinaryReaderX(bw.BaseStream).ReadBytes((int)bw.BaseStream.Length);
+                    }
+                case Method.RLE:
+                    methodSize = (uint)(stream.Length << 3 | 0x4);
+                    using (var bw = new BinaryWriterX(new MemoryStream()))
+                    {
+                        bw.Write(methodSize);
+                        stream.Position = 0;
+                        var comp = RLE.Compress(stream);
+                        bw.Write(comp);
                         bw.BaseStream.Position = 0;
                         return new BinaryReaderX(bw.BaseStream).ReadBytes((int)bw.BaseStream.Length);
                     }
