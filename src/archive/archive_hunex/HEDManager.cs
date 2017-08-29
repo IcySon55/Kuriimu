c16c583a34ae4d2f1ee7e906fbafd90486c3199f
Sn0wCrack <snowcrack123@gmail.com>
2017-08-29 19:21:51 +1000

    Added HuneX Load
    Add support for loading from the HuneX engine's few archive formats (hed and mrg).

---
+++ src/archive/archive_hunex/HEDManager.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Kuriimu.Kontract;
+using Kuriimu.IO;
+using System.Collections.Generic;
+using System.IO;
+using System.Drawing;
+
+namespace archive_hunex
+{
+    public class HEDManager : IArchiveManager
+    {
+        private HED _hed = null;
+
+        #region Properties
+
+        // Information
+        public string Name => "HuneX HED";
+
+        public string Description => "HuneX Engine HED Archive Format";
+        public string Extension => "*.hed";
+        public string About => "This is the HuneX HED archive manager for Karameru.";
+
+        // Feature Support
+        public bool ArchiveHasExtendedProperties => false;
+
+        public bool CanAddFiles => false;
+        public bool CanRenameFiles => false;
+        public bool CanReplaceFiles => false;
+        public bool CanDeleteFiles => false;
+        public bool CanSave => false;
+
+        public FileInfo FileInfo { get; set; }
+
+        #endregion
+
+        public bool Identify(string filename)
+        {
+            // There is no garunteed way to tell if a file is a
+            var mrgFileName = Path.Combine(Path.GetDirectoryName(filename), Path.GetFileNameWithoutExtension(filename) + ".mrg");
+            if (Path.GetExtension(filename) != ".hed") return false;
+            if (!File.Exists(filename) || !File.Exists(mrgFileName)) return false;
+            return true;
+        }
+
+        public void Load(string filename)
+        {
+            // Both MRG and HED files must exist for HED extraction
+            // NAM file is used to store the file names in order and is optional
+            // CPK file is used for file protection in an unknown way and is optional
+
+            FileInfo = new FileInfo(filename);
+            var baseTerm = Path.GetFileNameWithoutExtension(filename);
+            var namFileName = Path.Combine(Path.GetDirectoryName(filename), baseTerm + ".nam");
+            var mrgFileName = Path.Combine(Path.GetDirectoryName(filename), baseTerm + ".mrg");
+            var cpkFileName = Path.Combine(Path.GetDirectoryName(filename), baseTerm + ".cpk");
+
+            Stream namStream = null;
+            Stream mrgStream = null;
+            Stream cpkStream = null;
+
+            if (File.Exists(namFileName))
+                namStream = File.OpenRead(namFileName);
+
+            if (File.Exists(mrgFileName))
+                mrgStream = File.OpenRead(mrgFileName);
+
+            if (File.Exists(cpkFileName))
+                cpkStream = File.OpenRead(cpkFileName);
+
+            if (FileInfo.Exists)
+            {
+                _hed = new HED(FileInfo.OpenRead(), namStream, mrgStream, cpkStream, baseTerm);
+            }
+        }
+
+        public void Save(string filename = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ArchiveFileInfo> Files => _hed.Files;
+
+        public void Unload()
+        {
+            _hed?.Close();
+        }
+
+        public bool AddFile(ArchiveFileInfo afi) => false;
+
+        public bool DeleteFile(ArchiveFileInfo afi) => false;
+
+        public bool ShowProperties(Icon icon) => false;
+    }
+}
---
+++ src/archive/archive_hunex/HEDSupport.cs
@@ -0,0 +1,199 @@
+﻿using Kuriimu.IO;
+using Kuriimu.Kontract;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace archive_hunex
+{
+    public class HEDFileEntry
+    {
+        public string Name;
+        public string Suffix;
+        public int Offset;
+        public long Size;
+
+        public HEDFileEntry(BinaryReaderX blob, string name, string suffix)
+        {
+            Name = name;
+            Suffix = suffix;
+
+            if (blob.BaseStream.Length == 8)
+            {
+                var offsetLow = blob.ReadUInt16();
+                var offsetHigh = blob.ReadUInt16();
+                var sizeSect = blob.ReadUInt16();
+                var sizeLow = blob.ReadUInt16();
+
+                Offset = 0x800 * (((offsetHigh & 0xF000) << 4) | offsetLow);
+                Size = 0x800 * sizeSect;
+
+                if (sizeLow != 0)
+                    Size = (0x800 * (sizeSect - 1) & 0xFFFF0000) | sizeLow;
+            }
+            else if (blob.BaseStream.Length == 4)
+            {
+                var offsetLow = blob.ReadUInt16();
+                // Contains 4 extra offset bits, as well as 12 bits for the Sect Size
+                var offsetSizeHigh = blob.ReadUInt16();
+
+                Offset = 0x800 * (offsetLow | ((offsetSizeHigh & 0xF000) << 4));
+                Size = 0x800 * (offsetSizeHigh & 0x0FFF);
+            }
+            else
+            {
+                throw new Exception("HED Entry is of invalid size.");
+            }
+        }
+    }
+
+    public class HEDFileInfo : ArchiveFileInfo
+    {
+        public HEDFileEntry Entry;
+    }
+
+    public class NAM
+    {
+        private BinaryReaderX br = null;
+
+        // NOTE: This uses CP-1252 mainly due to certain NAM files containing
+        // malformed strings that seemingly don't fit any code page, the usage of
+        // CP-1252 is mostly for handling these errors silently, as most text should
+        // conform to Shift-JIS otherwise.
+        private const int codePage = 1252;
+
+        public string BaseTerm;
+        public uint Length;
+        public uint Total;
+        public Dictionary<int, uint> Index;
+        public long Size;
+
+        public bool Indexed = false;
+
+        public NAM(Stream namStream, string baseTerm)
+        {
+            Size = namStream.Length;
+            BaseTerm = baseTerm;
+
+            br = new BinaryReaderX(namStream, true);
+
+            getInfo();
+        }
+
+        private void getInfo()
+        {
+            checkIndex();
+            if (!Indexed)
+            {
+                checkNamLength();
+                return;
+            }
+            checkNamTotal();
+            makeNamIndex();
+        }
+
+        private void checkIndex()
+        {
+            byte[] bstr = br.ReadBytes(0x7);
+            string str = Encoding.GetEncoding(codePage).GetString(bstr);
+
+            if (str == "MRG.NAM")
+            {
+                Indexed = true;
+                Index = new Dictionary<int, uint>();
+            }
+        }
+
+        private void checkNamLength()
+        {
+            Length = 0x20;
+
+            if (BaseTerm.Contains("voice"))
+            {
+                Length = 0x08;
+            }
+        }
+
+        private void checkNamTotal()
+        {
+            br.BaseStream.Position = 0x10;
+            Total = br.ReadUInt32();
+        }
+
+        private void makeNamIndex()
+        {
+            br.BaseStream.Position = 0x20;
+            for (int i = 0; i < Total; ++i)
+            {
+                Index[i] = br.ReadUInt32();
+            }
+
+            int index = Convert.ToInt32(Total);
+            uint value = Convert.ToUInt32(Size);
+
+            Index[index] = value;
+        }
+
+        private string readString(byte[] bstr)
+        {
+            // Trim trailing null bytes.
+            byte[] trimmed = trimArray(bstr);
+
+            return Encoding.GetEncoding(codePage).GetString(trimmed);
+        }
+
+        private byte[] getNameWithIndex(int index)
+        {
+            int len = Convert.ToInt32(Index[index + 1] - Index[index] - 4);
+            br.BaseStream.Position = Index[index];
+
+            uint count = br.ReadUInt32();
+            if (index == count)
+            {
+                return br.ReadBytes(len);
+            }
+            else
+            {
+                return new byte[] { 0x0 };
+            }
+        }
+
+        private byte[] trimArray(byte[] arr)
+        {
+            int len = arr.Length - 1;
+            while (arr[len] == 0)
+                --len;
+
+            byte[] output = new byte[len + 1];
+            Array.Copy(arr, output, len + 1);
+
+            return output;
+        }
+
+        public string GetName(int index)
+        {
+            byte[] name;
+
+            if (Indexed)
+            {
+                name = getNameWithIndex(index);
+            }
+            else
+            {
+                br.BaseStream.Position = Length * index;
+                name = br.ReadBytes(Convert.ToInt32(Length));
+            }
+
+            return readString(name);
+        }
+
+        public void Close()
+        {
+            br?.Dispose();
+            br = null;
+        }
+    }
+}
