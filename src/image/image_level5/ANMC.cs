b8316de67fd6df18aa9ed65938663c24959cb3c9
Rhainer Zufall <alberteinstein1980@gmx.de>
2017-09-05 15:59:49 +0200

    ANMC plugin base structure added;

---
+++ src/image/image_level5/ANMC.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+using Kuriimu.IO;
+
+namespace image_xi.ANMC
+{
+    class ANMC
+    {
+        public static Bitmap Load(Stream input)
+        {
+            using (var br = new BinaryReaderX(input))
+            {
+                //Header
+                var header = br.ReadStruct<Header>();
+
+                //Tables
+                var tmp = br.ReadStruct<Entry>();
+                br.BaseStream.Position -= 8;
+                var tables = br.ReadMultiple<Entry>((tmp.offset - 0x14) / 8);
+
+                //First table contains as much elements as images exist in the appropriate archive
+                //Files table
+                var fileInfo = br.ReadMultiple<FileMeta>(tables[0].entryCount);
+
+                //seems to part the files in sub parts for better grouping
+                //Subparts
+                var subParts = br.ReadMultiple<SubPart>(tables[1].entryCount);
+
+                //Unknown table; Size == 0 until now
+
+                //unknown table
+                var unk1 = br.ReadMultiple<ulong>(tables[3].entryCount);
+
+                //infoMeta1 - Position ingame?
+                var inforMeta1 = br.ReadMultiple<InfoMeta1>(tables[4].entryCount);
+
+                //nameHashes
+                var hashes = br.ReadMultiple<uint>(tables[5].entryCount);
+
+                //InfoMeta2
+
+
+                return null;
+            }
+        }
+
+        public static void Save(Stream input)
+        {
+            using (var bw = new BinaryWriterX(input))
+            {
+
+            }
+        }
+    }
+}
---
+++ src/image/image_level5/AnmcSupport.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using Kuriimu.Kontract;
+using Kuriimu.IO;
+
+namespace image_xi.ANMC
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct Header
+    {
+        public Magic magic;
+        public uint const1;
+        public ushort tmpOff;
+        public ushort unk2;
+        public uint unk3;
+        public uint unk4;
+
+        public uint stringOffset => (uint)tmpOff << 2;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct Entry
+    {
+        public ushort offTmp;
+        public ushort entryCount;
+        public ushort unk1;
+        public ushort entryLength;
+
+        public ushort offset => (ushort)(offTmp << 2);
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct FileMeta
+    {
+        public uint nameHash;
+        public uint nameOffset;
+        public uint unk1;
+        public uint unk2;
+        public uint unk3;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct SubPart
+    {
+        public uint nameHash;
+        public uint nameOffset;
+        public uint unk1;
+        public uint const1;
+        public uint refHash;    //Hash of filename; e.g. FileMeta
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x33)]
+        public float[] Floats;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct InfoMeta1
+    {
+        public uint subPartHash;    //Hash of name of subPart
+        public uint nameOffset;
+        public uint unk1;
+        public uint subPartHash2;   //again
+        public uint unkHash;    //Hash of name in previous yet unkown table; e.g. tables[3]
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0xa)]
+        public float[] Floats;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct InfoMeta2
+    {
+        public uint hash;
+        public ushort nameOffset;
+        public ushort unk1;
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x6)]
+        public float[] Floats;
+    }
+}
