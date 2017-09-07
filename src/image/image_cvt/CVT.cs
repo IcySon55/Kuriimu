14a582421fe4147efe1a167fc74f59372ef3cdb0
djmatheusito <30787327+djmatheusito@users.noreply.github.com>
2017-09-06 21:03:49 -0300

    Added CVT Load/Save

---
+++ src/image/image_cvt/CVT.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using Cetera.Image;
+using Kuriimu.Kontract;
+using Kuriimu.IO;
+
+namespace image_cvt
+{
+   public class CVT
+    {
+        public Header header;
+
+        public List<Bitmap> bmps = new List<Bitmap>();
+        
+        public CVT(Stream input)
+        {            
+            using (var br = new BinaryReaderX(input))
+            {
+                header = br.ReadStruct<Header>();
+
+                var settings = new ImageSettings
+                {
+                    Width = header.width,
+                    Height = header.height,
+                    Format = ImageSettings.ConvertFormat(Format.ETC1A4),
+                    PadToPowerOf2 = false
+                };
+
+                bmps.Add(Common.Load(br.ReadBytes((int)br.BaseStream.Length - 0x50), settings));
+            }
+        }
+
+        public void Save(string filename)
+        {
+            using (BinaryWriterX bw = new BinaryWriterX(File.Create(filename)))
+            {
+                             
+                bw.WriteStruct(header);
+                
+                var settings = new ImageSettings
+                {
+                    Width = header.width,
+                    Height = header.height,
+                    Format = ImageSettings.ConvertFormat(header.format)
+                };
+                
+                bw.Write(Common.Save(bmps[0], settings));
+            }
+        }
+   }
+}
---
+++ src/image/image_cvt/CvtAdapter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.IO;
+using Kuriimu.Kontract;
+using Kuriimu.IO;
+
+namespace image_cvt
+{
+    public sealed class CvtAdpter : IImageAdapter
+    {
+        private CVT _cvt = null;
+        private List<BitmapInfo> _bitmaps;
+
+        #region Properties
+
+        public string Name => "CVT";
+        public string Description => "Chase inVestigation Texture";
+        public string Extension => "*.cvt";
+        public string About => "This is the CVT image adapter for Kukkii.";
+
+        // Feature Support
+        public bool FileHasExtendedProperties => false;
+        public bool CanSave => true;
+
+        public FileInfo FileInfo { get; set; }
+
+        #endregion
+
+        public bool Identify(string filename)
+        {
+            using (var br = new BinaryReaderX(File.OpenRead(filename)))
+            {
+                if (br.BaseStream.Length < 2) return false;
+                return br.ReadString(2) == "n";
+            }
+        }
+
+        public void Load(string filename)
+        {
+            FileInfo = new FileInfo(filename);
+
+            if (FileInfo.Exists)
+            {
+                _cvt = new CVT(FileInfo.OpenRead());
+
+                _bitmaps = _cvt.bmps.Select(o => new BitmapInfo { Bitmap = o }).ToList();
+            }
+        }
+
+        public void Save(string filename = "")
+        {
+            if(filename.Trim() != string.Empty)
+                FileInfo = new FileInfo(filename);
+
+            _cvt.bmps = _bitmaps.Select(o => o.Bitmap).ToList();
+            _cvt.Save(FileInfo.FullName);
+        }
+
+        // Bitmaps
+        public IList<BitmapInfo> Bitmaps => _bitmaps;
+
+        public bool ShowProperties(Icon icon) => false;
+
+    }
+}
