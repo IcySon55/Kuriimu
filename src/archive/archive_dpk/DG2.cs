aca07c31f7df49a85cb7f5f96c97d6ba86382d66
Rhainer Zufall <alberteinstein1980@gmx.de>
2017-09-07 13:46:13 +0200

    Added Drakengard 2 DPK Load and Save;

---
+++ src/archive/archive_dpk/DG2.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Kuriimu.Kontract;
+using Kuriimu.IO;
+
+namespace archive_dpk.DG2
+{
+    public class DG2
+    {
+        public List<DG2FileInfo> Files = new List<DG2FileInfo>();
+        private Stream _stream = null;
+
+        public Header header;
+        public List<Entry> entries;
+
+        public DG2(Stream input)
+        {
+            _stream = input;
+            using (var br = new BinaryReaderX(input, true))
+            {
+                //Header
+                header = br.ReadStruct<Header>();
+
+                //Entries
+                br.BaseStream.Position = header.headerSize;
+                entries = br.ReadMultiple<Entry>(header.fileCount);
+
+                //Files
+                var count = 0;
+                foreach (var entry in entries)
+                    Files.Add(new DG2FileInfo
+                    {
+                        State = ArchiveFileState.Archived,
+                        FileName = $"{count++:0000}.bin",
+                        FileData = new SubStream(br.BaseStream, entry.offset, entry.fileSize),
+                        entry = entry
+                    });
+            }
+        }
+
+        public void Save(Stream output)
+        {
+            using (var bw = new BinaryWriterX(output))
+            {
+                int dataOffset = header.dataOffset;
+
+                //Files
+                var files = Files.OrderBy(x => x.entry.offset);
+                int offset = dataOffset;
+                foreach (var file in files)
+                    offset = file.Write(bw.BaseStream, offset);
+
+                //Entry
+                bw.BaseStream.Position = header.headerSize;
+                foreach (var file in Files)
+                    bw.WriteStruct(file.entry);
+
+                //Header
+                bw.BaseStream.Position = 0;
+                bw.WriteStruct(header);
+            }
+        }
+
+        public void Close()
+        {
+            _stream?.Dispose();
+            foreach (var afi in Files)
+                if (afi.State != ArchiveFileState.Archived)
+                    afi.FileData?.Dispose();
+            _stream = null;
+        }
+    }
+}
---
+++ src/archive/archive_dpk/Dg2Manager.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kuriimu.Kontract;
+using System.Drawing;
+using Kuriimu.IO;
+using System.IO;
+
+namespace archive_dpk.DG2
+{
+    public class Dg2Manager : IArchiveManager
+    {
+        private DG2 _dpk = null;
+
+        #region Properties
+
+        // Information
+        public string Name => "DPK";
+        public string Description => "Drakengard PacKage";
+        public string Extension => "*.bin";
+        public string About => "This is the DPK archive manager for Karameru.";
+
+        // Feature Support
+        public bool ArchiveHasExtendedProperties => false;
+        public bool CanAddFiles => false;
+        public bool CanRenameFiles => false;
+        public bool CanReplaceFiles => true;
+        public bool CanDeleteFiles => false;
+        public bool CanSave => true;
+
+        public FileInfo FileInfo { get; set; }
+
+        #endregion
+
+        public bool Identify(string filename)
+        {
+            using (var br = new BinaryReaderX(File.OpenRead(filename)))
+                return br.ReadString(4) == "dpk";
+        }
+
+        public void Load(string filename)
+        {
+            FileInfo = new FileInfo(filename);
+
+            if (FileInfo.Exists)
+                _dpk = new DG2(FileInfo.OpenRead());
+        }
+
+        public void Save(string filename = "")
+        {
+            if (!string.IsNullOrEmpty(filename))
+                FileInfo = new FileInfo(filename);
+
+            // Save As...
+            if (!string.IsNullOrEmpty(filename))
+            {
+                _dpk.Save(FileInfo.Create());
+                _dpk.Close();
+            }
+            else
+            {
+                // Create the temp file
+                _dpk.Save(File.Create(FileInfo.FullName + ".tmp"));
+                _dpk.Close();
+                // Delete the original
+                FileInfo.Delete();
+                // Rename the temporary file
+                File.Move(FileInfo.FullName + ".tmp", FileInfo.FullName);
+            }
+
+            // Reload the new file to make sure everything is in order
+            Load(FileInfo.FullName);
+        }
+
+        public void Unload()
+        {
+            _dpk?.Close();
+        }
+
+        // Files
+        public IEnumerable<ArchiveFileInfo> Files => _dpk.Files;
+
+        public bool AddFile(ArchiveFileInfo afi) => false;
+
+        public bool DeleteFile(ArchiveFileInfo afi) => false;
+
+        // Features
+        public bool ShowProperties(Icon icon) => false;
+    }
+}
