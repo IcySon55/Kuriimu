a2b4c30e43285cadc8e26f37559df3ca9da07af0
IcySon55 <icyson55@gmail.com>
2017-09-10 03:07:02 -0400

    Replaced tabs.

--- get_git_info.ps1
+++ get_git_info.ps1
@@ -2,35 +2,35 @@
 
 Try
 {
-	$gitUser       = git config user.name;
-	$gitEmail      = git config user.email;
-	$gitStateUser  = git --no-pager log -1 --pretty=format:"%an";
-	$gitStateEmail = git --no-pager log -1 --pretty=format:"%ae";
-	$gitHashShort  = git rev-parse --short HEAD;
-	$gitHashLong   = git rev-parse HEAD;
-	$gitLastTag    = git describe --tags --abbrev=0;
-	$gitOwnerRepo  = [regex]::Match((git config --get remote.origin.url), "(?:github.com\/)(.+?)\/([^.\n]*)");
-	$gitOwner      = $gitOwnerRepo.Groups[1].Value;
-	$gitRepo       = $gitOwnerRepo.Groups[2].Value;
-	$gitBranch     = git name-rev --name-only HEAD;
-	$gitCount      = git rev-list HEAD --count;
-	$compileTime   = [DateTime]::UtcNow.ToString('u').Replace('Z','UTC');
+    $gitUser       = git config user.name;
+    $gitEmail      = git config user.email;
+    $gitStateUser  = git --no-pager log -1 --pretty=format:"%an";
+    $gitStateEmail = git --no-pager log -1 --pretty=format:"%ae";
+    $gitHashShort  = git rev-parse --short HEAD;
+    $gitHashLong   = git rev-parse HEAD;
+    $gitLastTag    = git describe --tags --abbrev=0;
+    $gitOwnerRepo  = [regex]::Match((git config --get remote.origin.url), "(?:github.com\/)(.+?)\/([^.\n]*)");
+    $gitOwner      = $gitOwnerRepo.Groups[1].Value;
+    $gitRepo       = $gitOwnerRepo.Groups[2].Value;
+    $gitBranch     = git name-rev --name-only HEAD;
+    $gitCount      = git rev-list HEAD --count;
+    $compileTime   = [DateTime]::UtcNow.ToString('u').Replace('Z','UTC');
 }
 Catch
 {
-	$gitUser       = "unknown";
-	$gitEmail      = "unknown@example.com";
-	$gitStateUser  = "unknown";
-	$gitStateEmail = "unknown@example.com";
-	$gitHashShort  = "unknown";
-	$gitHashLong   = "unknown";
-	$gitLastTag    = "0.1.0";
-	$gitOwner      = "IcySon55";
-	$gitOwnerRepo  = "Kuriimu";
-	$gitRepo       = "Kuriimu";
-	$gitBranch     = "master";
-	$gitCount      = 0;
-	$compileTime   = "unknown"
+    $gitUser       = "unknown";
+    $gitEmail      = "unknown@example.com";
+    $gitStateUser  = "unknown";
+    $gitStateEmail = "unknown@example.com";
+    $gitHashShort  = "unknown";
+    $gitHashLong   = "unknown";
+    $gitLastTag    = "0.1.0";
+    $gitOwner      = "IcySon55";
+    $gitOwnerRepo  = "Kuriimu";
+    $gitRepo       = "Kuriimu";
+    $gitBranch     = "master";
+    $gitCount      = 0;
+    $compileTime   = "unknown"
 }
 
 $projectPath = $args[0].Replace("""","");
