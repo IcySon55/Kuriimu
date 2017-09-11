981e204343ce7bab4de6d03927eb0921b7846620
Rhainer Zufall <alberteinstein1980@gmx.de>
2017-09-11 15:59:34 +0200

    Started IOBJ plugin;

---
+++ src/image/image_iobj/IOBJ.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using Cetera.Image;
+using Kuriimu.IO;
+
+namespace image_iobj
+{
+    public class IOBJ
+    {
+        public List<Bitmap> bmps = new List<Bitmap>();
+
+        List<byte[]> table1Entries = new List<byte[]>();
+
+        public IOBJ(Stream input)
+        {
+            using (var br = new BinaryReaderX(input))
+            {
+                //Header
+                var header = br.ReadStruct<Header>();
+
+                //Table1
+                br.BaseStream.Position = header.table1Offset;
+                var t1entryCount = br.ReadInt32();
+                var offsets = br.ReadMultiple<int>(t1entryCount);
+                for (int i = 0; i < offsets.Count; i++)
+                {
+                    br.BaseStream.Position = header.table1Offset + offsets[i];
+                    table1Entries.Add(br.ReadBytes((i + 1 < offsets.Count - 1) ? offsets[i + 1] - offsets[i] : header.PtgtOffset - offsets[i]));
+                }
+
+                //PTGT
+                br.BaseStream.Position = header.PtgtOffset;
+            }
+        }
+
+        public void Save(string filename)
+        {
+            using (var bw = new BinaryWriterX(File.Create(filename)))
+            {
+
+            }
+        }
+    }
+}
---
+++ src/image/image_iobj/IobjSupport.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+using Kuriimu.Kontract;
+
+namespace image_iobj
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public class Header
+    {
+        public Magic magic;
+        public int table1Offset;
+        public int PtgtOffset;
+        public int table2Offset;
+        public int image1Offset;
+        public int image2Offset;
+    }
+}
