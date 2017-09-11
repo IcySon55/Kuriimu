201a8806e8a3d101976badd99cf49c4fb8e76596
onepiecefreak3 <alberteinstein1980@gmx.de>
2017-09-11 21:49:16 +0200

    Added IOBJ Load and Save;

--- src/image/image_iobj/IOBJ.cs
+++ src/image/image_iobj/IOBJ.cs
@@ -3,6 +3,8 @@ using System.Drawing;
 using System.IO;
 using Cetera.Image;
 using Kuriimu.IO;
+using System.Text;
+using System.Linq;
 
 namespace image_iobj
 {
@@ -11,6 +13,10 @@ namespace image_iobj
         public List<Bitmap> bmps = new List<Bitmap>();
 
         List<byte[]> table1Entries = new List<byte[]>();
+        PTGTHeader ptgtHeader;
+        byte[] ptgtEntries;
+        //List<PTGTEntry> ptgtEntries = new List<PTGTEntry>();
+        List<byte[]> imgMetaInf = new List<byte[]>();
 
         public IOBJ(Stream input)
         {
@@ -18,6 +24,8 @@ namespace image_iobj
             {
                 //Header
                 var header = br.ReadStruct<Header>();
+                var imgCount = br.ReadInt32();
+                var imgOffsets = br.ReadMultiple<int>(imgCount);
 
                 //Table1
                 br.BaseStream.Position = header.table1Offset;
@@ -26,11 +34,48 @@ namespace image_iobj
                 for (int i = 0; i < offsets.Count; i++)
                 {
                     br.BaseStream.Position = header.table1Offset + offsets[i];
-                    table1Entries.Add(br.ReadBytes((i + 1 < offsets.Count - 1) ? offsets[i + 1] - offsets[i] : header.PtgtOffset - offsets[i]));
+                    table1Entries.Add(br.ReadBytes((i + 1 < offsets.Count) ? offsets[i + 1] - offsets[i] : header.PtgtOffset - (offsets[i] + header.table1Offset)));
                 }
 
                 //PTGT
                 br.BaseStream.Position = header.PtgtOffset;
+                ptgtHeader = br.ReadStruct<PTGTHeader>();
+                //var entryCount = br.ReadInt32();
+                //var ptgtEntriesL = br.ReadMultiple<PTGTOffEntry>(entryCount);
+                ptgtEntries = br.ReadBytes(imgOffsets[0] - (header.PtgtOffset + 8));
+                /*foreach (var entry in ptgtEntriesL)
+                {
+                    br.BaseStream.Position = header.PtgtOffset + entry.offset + 0xc;
+                    ptgtEntries.Add(new PTGTEntry
+                    {
+                        offsetEntry = entry,
+                        floats = br.ReadMultiple<float>(0x10).ToArray()
+                    });
+                }*/
+
+                //Images
+                for (int i = 0; i < imgCount; i++)
+                {
+                    br.BaseStream.Position = imgOffsets[i];
+                    var dataSize = br.ReadInt32();
+                    var width = br.ReadInt32();
+                    var height = br.ReadInt32();
+                    br.BaseStream.Position += 0x20;
+                    var format = (Format)br.ReadInt32();
+                    br.BaseStream.Position = imgOffsets[i];
+
+                    imgMetaInf.Add(br.ReadBytes(0x80));
+
+                    var settings = new ImageSettings
+                    {
+                        Width = width,
+                        Height = height,
+                        Orientation = Orientation.XFlip,
+                        Format = ImageSettings.ConvertFormat(format)
+                    };
+
+                    bmps.Add(Common.Load(br.ReadBytes(dataSize), settings));
+                }
             }
         }
 
@@ -38,7 +83,76 @@ namespace image_iobj
         {
             using (var bw = new BinaryWriterX(File.Create(filename)))
             {
+                var table1Offset = 0x10 + bmps.Count * 0x4;
+                var ptgtTableOffset = table1Offset + 0x4 + table1Entries.Count * 0x4 + table1Entries.SelectMany(x => x.SelectMany(b => new[] { b })).Count();
+                var imgDataSize = (ptgtTableOffset + 0x8 + ptgtEntries.Length + 0x7f) & ~0x7f;//ptgtEntries.Count * 0x8 + ptgtEntries.Count * 0xe + 0x7f) & ~0x7f;
 
+                //Header
+                bw.Write(Encoding.ASCII.GetBytes("IOBJ"));
+                bw.Write(table1Offset);
+                bw.Write(ptgtTableOffset);
+                bw.Write(bmps.Count);
+
+                //Table1
+                bw.BaseStream.Position = table1Offset;
+                bw.Write(table1Entries.Count);
+                var offset = 0x4 + table1Entries.Count * 0x4;
+                foreach (var entry in table1Entries)
+                {
+                    bw.Write(offset);
+                    offset += entry.Length;
+                }
+                foreach (var entry in table1Entries)
+                    bw.Write(entry);
+
+                //PTGT
+                bw.WriteStruct(ptgtHeader);
+                bw.Write(ptgtEntries);
+                /*bw.Write(ptgtEntries.Count);
+                foreach (var entry in ptgtEntries)
+                    bw.WriteStruct(entry.offsetEntry);
+                foreach (var entry in ptgtEntries)
+                    foreach (var floatEntry in entry.floats)
+                        bw.Write(floatEntry);
+                bw.WriteAlignment(0x80);*/
+
+                //Images
+                var imgOffsets = new List<int>();
+                var count = 0;
+                foreach (var bmp in bmps)
+                {
+                    imgOffsets.Add((int)bw.BaseStream.Position);
+
+                    Format format;
+                    using (var br = new BinaryReaderX(new MemoryStream(imgMetaInf[count])))
+                    {
+                        br.BaseStream.Position = 0x2c;
+                        format = (Format)br.ReadInt32();
+                    }
+
+                    var settings = new ImageSettings
+                    {
+                        Width = bmp.Width,
+                        Height = bmp.Height,
+                        Orientation = Orientation.XFlip,
+                        Format = ImageSettings.ConvertFormat(format)
+                    };
+
+                    var pic = Common.Save(bmp, settings);
+
+                    bw.Write(imgMetaInf[count++]);
+                    bw.BaseStream.Position -= 0x80;
+                    bw.Write(pic.Length);
+                    bw.BaseStream.Position += 0x7c;
+
+                    bw.Write(pic);
+                    bw.WriteAlignment(0x80);
+                }
+
+                //imgOffsets in Header
+                bw.BaseStream.Position = 0x10;
+                foreach (var imgOff in imgOffsets)
+                    bw.Write(imgOff);
             }
         }
     }
--- src/image/image_iobj/IobjSupport.cs
+++ src/image/image_iobj/IobjSupport.cs
@@ -3,14 +3,38 @@ using Kuriimu.Kontract;
 
 namespace image_iobj
 {
+    public enum Format : int
+    {
+        RGBA4444 = 1,
+        ETC1A4 = 0x20
+    }
+
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
     public class Header
     {
         public Magic magic;
         public int table1Offset;
         public int PtgtOffset;
-        public int table2Offset;
-        public int image1Offset;
-        public int image2Offset;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public class PTGTHeader
+    {
+        public Magic magic;
+        public int unk1;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public class PTGTOffEntry
+    {
+        public int offset;
+        public short unk1;
+        public short unk2;
+    }
+
+    public class PTGTEntry
+    {
+        public PTGTOffEntry offsetEntry;
+        public float[] floats;
     }
 }
