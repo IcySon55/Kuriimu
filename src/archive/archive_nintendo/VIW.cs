07f25189498570a934f746fba642e5f3eb2bc595
IcySon55 <icyson55@gmail.com>
2017-09-10 21:15:39 -0400

    Added an early INF/VIW plugin to the archive_nintendo project.

---
+++ src/archive/archive_nintendo/VIW.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Kuriimu.IO;
+using Kuriimu.Kontract;
+
+namespace archive_nintendo.VIW
+{
+    class VIW
+    {
+        public List<ArchiveFileInfo> Files = new List<ArchiveFileInfo>();
+        private Stream _infStream;
+        private Stream _viwStream;
+        private Stream _stream;
+
+        private InfHeader Header;
+        private List<InfEntry> Offsets;
+        private List<InfMetaEntry> MetaEntries;
+        private List<ViwEntry> Names;
+
+        public VIW(Stream infInput, Stream viwInput, Stream input)
+        {
+            _infStream = infInput;
+            _viwStream = viwInput;
+            _stream = input;
+            using (var br = new BinaryReaderX(infInput, true))
+            {
+                uint index = 0;
+
+                Header = br.ReadStruct<InfHeader>();
+                Offsets = br.ReadMultiple<InfEntry>(Header.FileCount);
+
+                if (br.BaseStream.Length > Header.Table1Offset)
+                    MetaEntries = br.ReadMultiple<InfMetaEntry>(Header.NameCount);
+            }
+
+            using (var br = new BinaryReaderX(viwInput, true))
+            {
+                Names = br.ReadMultiple<ViwEntry>(Header.NameCount);
+            }
+
+            using (var br = new BinaryReaderX(input, true))
+            {
+                Files.AddRange(Offsets.Select(o => new ArchiveFileInfo
+                {
+                    FileName = Names.FirstOrDefault(n => n.ID == MetaEntries[Offsets.IndexOf(o)].Unk1)?.Name,
+                    FileData = new SubStream(input, o.Offset, o.CompressedSize),
+                    State = ArchiveFileState.Archived
+                }));
+            }
+        }
+
+        public void Save(Stream output)
+        {
+            using (var bw = new BinaryWriterX(output))
+            {
+
+                foreach (var info in Files)
+                {
+                }
+
+                foreach (var info in Files)
+                    info.FileData.CopyTo(bw.BaseStream);
+            }
+        }
+
+        public void Close()
+        {
+            _infStream?.Dispose();
+            _viwStream?.Dispose();
+            _stream?.Dispose();
+            foreach (var afi in Files)
+                if (afi.State != ArchiveFileState.Archived)
+                    afi.FileData?.Dispose();
+            _infStream = null;
+            _viwStream = null;
+            _stream = null;
+        }
+    }
+}
---
+++ src/archive/archive_nintendo/ViwSupport.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.InteropServices;
+
+namespace archive_nintendo.VIW
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public class InfHeader
+    {
+        public int FileCount;
+        public int NameCount;
+        public int Table0Offset;
+        public int Table1Offset;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public class InfEntry
+    {
+        public int Offset;
+        public int CompressedSize;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public class InfMetaEntry
+    {
+        public short Unk1;
+        public short Unk2;
+        public short Unk3;
+        public short Unk4;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public class ViwEntry
+    {
+        public int ID;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x14)]
+        public string Name;
+    }
+}
