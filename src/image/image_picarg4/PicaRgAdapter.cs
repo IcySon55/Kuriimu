3681da99a2fb0c42c68b866875c7f704527dc535
onepiecefreak3 <alberteinstein1980@gmx.de>
2017-09-11 22:28:21 +0200

    Added PicaRg4 Load and Save;

---
+++ src/image/image_picarg4/PicaRgAdapter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using Kuriimu.IO;
+using Kuriimu.Kontract;
+
+namespace image_picarg4
+{
+    public sealed class PicaRgAdapter : IImageAdapter
+    {
+        private PICARG _picaRg = null;
+        private List<BitmapInfo> _bitmaps;
+
+        #region Properties
+
+        public string Name => "PicaRg4";
+        public string Description => "Pica image 4";
+        public string Extension => "*.lzb";
+        public string About => "This is the PicaRg4 image adapter for Kukkii.";
+
+        // Feature Support
+        public bool FileHasExtendedProperties => false;
+        public bool CanSave => true;
+
+        public FileInfo FileInfo { get; set; }
+
+        #endregion
+
+        public bool Identify(string filename)
+        {
+            using (var br = new BinaryReaderX(File.OpenRead(filename)))
+            {
+                return br.ReadString(7) == "picaRg4";
+            }
+        }
+
+        public void Load(string filename)
+        {
+            FileInfo = new FileInfo(filename);
+
+            if (FileInfo.Exists)
+            {
+                _picaRg = new PICARG(FileInfo.OpenRead());
+
+                _bitmaps = _picaRg.bmps.Select(o => new BitmapInfo { Bitmap = o }).ToList();
+            }
+        }
+
+        public void Save(string filename = "")
+        {
+            if (filename.Trim() != string.Empty)
+                FileInfo = new FileInfo(filename);
+
+            _picaRg.bmps = _bitmaps.Select(o => o.Bitmap).ToList();
+            _picaRg.Save(FileInfo.FullName);
+        }
+
+        // Bitmaps
+        public IList<BitmapInfo> Bitmaps => _bitmaps;
+
+        public bool ShowProperties(Icon icon) => false;
+    }
+}
---
+++ src/image/image_picarg4/PicaRgSupport.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace image_picarg4
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public class Header
+    {
+        public byte unk1;
+        public short unk2;
+        public short unk3;
+        public short width;
+        public short height;
+    }
+}
