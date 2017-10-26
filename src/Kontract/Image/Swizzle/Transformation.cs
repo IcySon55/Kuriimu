af7f60f0328c3aff31d8ff037a40e00b0b07fed8
onepiecefreak3 <alberteinstein1980@gmx.de>
2017-10-26 15:13:49 +0200

    Added Transformation swizzle Load; Containing Rotation and Transpose;

---
+++ src/Kontract/Image/Swizzle/Transformation.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using Kontract.Interface;
+
+namespace Kontract.Image.Swizzle
+{
+    class Rotate : IImageSwizzle
+    {
+        int degree;
+
+        /// <summary>
+        /// Swizzles points to accomplish a rotated image
+        /// Valid degree are: 0, 90, 180, 270
+        /// Other values will be clamped to be one of those 4
+        /// </summary>
+        /// 
+        /// <param name="degree">
+        /// Value containing the degree by which the image should be rotated
+        /// </param>
+        public Rotate(int degree)
+        {
+            this.degree = degree % 360 / 90 * 90;
+        }
+
+        public Point Load(Point point, int width, int height)
+        {
+            switch (degree)
+            {
+                case 90:
+                    return new Point(
+                        (height - 1) - point.Y,
+                        point.X);
+                case 180:
+                    return new Point(
+                        (width - 1) - point.X,
+                        (height - 1) - point.Y);
+                case 270:
+                    return new Point(
+                        point.Y,
+                        (height - 1) - point.X);
+                default:
+                    return point;
+            }
+        }
+
+        public Point Save(int pointCount, int tileSize, int width, int height)
+        {
+            return new Point(0, 0);
+        }
+    }
+
+    class Transpose : IImageSwizzle
+    {
+        public Point Load(Point point, int width, int height)
+        {
+            return new Point(point.Y, point.X);
+        }
+
+        public Point Save(int pointCount, int tileSize, int width, int height)
+        {
+            return new Point(0, 0);
+        }
+    }
+}
