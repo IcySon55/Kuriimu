c9aab5e0bdedbf046609a58aa30c932cda78b9e0
onepiecefreak3 <alberteinstein1980@gmx.de>
2017-11-06 21:38:21 +0100

    Added some imageTests;

--- src/KuriimuTests/ImageTest.cs
+++ src/KuriimuTests/ImageTest.cs
@@ -1,82 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Threading.Tasks;
-using Kontract.Image;
-using Kontract.Image.Support;
-using Kontract.Image.Format;
-using Kontract.Image.Swizzle;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
-using System.Drawing;
+using Kontract.Interface;
 
 namespace KuriimuTests
 {
     [TestClass]
     public class ImageTest
     {
-        private TestContext testContextInstance;
-        public TestContext TestContext
+        private const string sample_file_path = "../../../sample_files/image_files";
+        private const string tmp_path = "tmp";
+
+        public static void Test<T>(string file) where T : IImageAdapter, new()
         {
-            get { return testContextInstance; }
-            set { testContextInstance = value; }
+            Directory.CreateDirectory(Path.Combine(sample_file_path, tmp_path));
+            var path1 = Path.Combine(sample_file_path, file);
+            var path2 = Path.Combine(sample_file_path, tmp_path, "test-" + Path.GetFileName(file));
+
+            var mgr = new T();
+            mgr.Load(path1);
+            mgr.Save(path2);
+            FileAssert.AreEqual(path1, path2);
+
+            // Delete if successful
+            File.Delete(path2);
         }
+
+        //BNR - Order of paletted colors couldn't be reconstructed, but they work after saving
+        //CTX - unknown build errors (giving an error that namespace is missing, but VS and rebuilding manually doesn't give any build errors. Also Kukkii can handle CTX properly)
+        //CTXB - Only ETC images available. Not testable through our ETC-Encoder
+        //CVT - Only ETC images available. Not testable through our ETC-Encoder
 
         [TestMethod]
-        public void XYZsRGBConversion()
-        {
-            var r = 1;
-            var g = 0;
-            var b = 0;
-
-            var xyz = sRGB.RGBToXYZ(new sRGB.RGBComponent { R = r, G = g, B = b });
-            var color = sRGB.XYZToRGB(xyz);
-
-            Assert.IsTrue(r == color.R && g == color.G && b == color.B);
-        }
+        public void AIFTest() => Test<image_aif.AifAdapter>("com_bg_b1.aif");
 
         [TestMethod]
-        public void RGBXYZConversion()
-        {
-            var r = 1;
-            var g = 0;
-            var b = 0;
-
-            var xyz = sRGB.RGBToXYZ(new sRGB.RGBComponent { R = r, G = g, B = b });
-            var color = sRGB.XYZToRGB(xyz);
-
-            Assert.IsTrue(r == color.R && g == color.G && b == color.B);
-        }
-
-        [TestMethod]
-        public void RGBA8888_ZOrder()
-        {
-            var settings = new ImageSettings
-            {
-                Width = 4,
-                Height = 4,
-                Format = new RGBA(8, 8, 8, 8),
-                Swizzle = new CTRSwizzle(4, 4)
-            };
-            var tex = new byte[] {
-                255,0xff,0xff,0xff,
-                255,0xff,0xff,0xff,
-                255,0xff,0xff,0xff,
-                255,0xff,0xff,0xff,
-                255,0x30,0x30,0x30,
-                255,0x30,0x30,0x30,
-                255,0x30,0x30,0x30,
-                255,0x30,0x30,0x30,
-                255,0x80,0x80,0x80,
-                255,0x80,0x80,0x80,
-                255,0x80,0x80,0x80,
-                255,0x80,0x80,0x80,
-                255,0x00,0x00,0x00,
-                255,0x00,0x00,0x00,
-                255,0x00,0x00,0x00,
-                255,0x00,0x00,0x00,};
-            var bmp = Kontract.Image.Image.Load(tex, settings);
-        }
+        public void COMPTest() => Test<image_comp.CompAdapter>("mz_Name_000.comp");
     }
 }
