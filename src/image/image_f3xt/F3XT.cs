72ac4d63b97f71ef2227edb9c9ab36d10bc9ae1d
onepiecefreak3 <alberteinstein1980@gmx.de>
2017-11-07 01:22:33 +0100

    Revised F3XT;

--- src/image/image_f3xt/F3XT.cs
+++ src/image/image_f3xt/F3XT.cs
@@ -1,9 +1,8 @@
 using System.Drawing;
 using System.IO;
-using System.Runtime.InteropServices;
-using Cetera.Image;
-using Kontract.Compression;
-using Kontract;
+//using Cetera.Image;
+using Kontract.Image;
+using Kontract.Image.Swizzle;
 using Kontract.IO;
 
 namespace image_f3xt
@@ -14,40 +13,24 @@ namespace image_f3xt
         public ImageSettings settings;
         public Header header;
 
-        [StructLayout(LayoutKind.Sequential, Pack = 1)]
-        public struct Header
-        {
-            Magic magic;
-            uint texEntries;
-            uint flags;
-            uint unk1;
-            public ushort width;
-            public ushort height;
-            public uint dataStart;
-
-            public Format format => (Format)((int)flags & 0xFF);
-        }
-
         public F3XT(Stream input)
         {
             using (BinaryReaderX br = new BinaryReaderX(input))
             {
-                //map the file
+                //Header
                 header = br.ReadStruct<Header>();
 
                 settings = new ImageSettings
                 {
                     Width = header.width,
                     Height = header.height,
-                    Format = ImageSettings.ConvertFormat(header.format),
-                    PadToPowerOf2 = false
+                    Format = Support.Format[header.format],
+                    Swizzle = new CTRSwizzle(header.width, header.height)
                 };
 
                 br.BaseStream.Position = header.dataStart;
 
-                //throw new Exception((stream.BaseStream.Length - stream.BaseStream.Position).ToString());
-
-                Image = Common.Load(br.ReadBytes((int)(br.BaseStream.Length - br.BaseStream.Position)), settings);
+                Image = Kontract.Image.Image.Load(br.ReadBytes((int)(br.BaseStream.Length - br.BaseStream.Position)), settings);
             }
         }
 
@@ -55,14 +38,19 @@ namespace image_f3xt
         {
             using (BinaryWriterX br = new BinaryWriterX(input))
             {
-                ImageSettings modSettings = settings;
-                modSettings.Width = Image.Width;
-                modSettings.Height = Image.Height;
+                settings = new ImageSettings
+                {
+                    Width = Image.Width,
+                    Height = Image.Height,
+                    Format = Support.Format[header.format],
+                    Swizzle = new CTRSwizzle(Image.Width, Image.Height)
+                };
+                byte[] data = Kontract.Image.Image.Save(Image, settings);
 
-                byte[] data = Common.Save(Image, modSettings);
+                header.width = (ushort)Image.Width;
+                header.height = (ushort)Image.Height;
+                br.WriteStruct(header);
 
-                header.width = (ushort)Image.Width; header.height = (ushort)Image.Height;
-                br.WriteStruct<Header>(header);
                 br.BaseStream.Position = header.dataStart;
                 br.Write(data);
             }
