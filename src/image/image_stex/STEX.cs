5b3d6b8fa2f1b691b9f6266edbd8555cd1136a82
onepiecefreak3 <alberteinstein1980@gmx.de>
2017-11-12 14:26:53 +0100

    Revised STEX to use new image library;

--- src/image/image_stex/STEX.cs
+++ src/image/image_stex/STEX.cs
@@ -1,7 +1,7 @@
 ﻿using System.Drawing;
 using System.IO;
-using System.Runtime.InteropServices;
-using Cetera.Image;
+using Kontract.Image;
+using Kontract.Image.Swizzle;
 using Kontract;
 using Kontract.IO;
 
@@ -12,65 +12,9 @@ namespace image_stex
 {
     public class STEX
     {
-        public enum DataTypes : uint
-        {
-            Byte = 0x1400,
-            UByte = 0x1401,
-            Short = 0x1402,
-            UShort = 0x1403,
-            Int = 0x1404,
-            UInt = 0x1405,
-            Float = 0x1406,
-            UnsignedByte44DMP = 0x6760,
-            Unsigned4BitsDMP = 0x6761,
-            UnsignedShort4444 = 0x8033,
-            UnsignedShort5551 = 0x8034,
-            UnsignedShort565 = 0x8363
-        };
-
-        public enum Format : uint
-        {
-            RGBA8888 = 0x6752,
-            RGB888 = 0x6754,
-            A8 = 0x6756,
-            L8 = 0x6757,
-            LA88 = 0x6758,
-            ETC1 = 0x675A,
-            ETC1A4 = 0x675B
-        };
-
-        [StructLayout(LayoutKind.Sequential, Pack = 1)]
-        public struct Header
-        {
-            public Magic magic;
-            public uint zero0;
-            public uint const0; //0xde1
-            public int width;
-            public int height;
-            public DataTypes type;
-            public Format imageFormat;
-            public int dataSize;
-
-        }
-
-        public class TexInfo
-        {
-            public TexInfo(Stream input)
-            {
-                using (BinaryReaderX br = new BinaryReaderX(input, true))
-                {
-                    offset = br.ReadInt32();
-                    unk1 = br.ReadUInt32();
-                    name = br.ReadCStringA();
-                }
-            }
-            public int offset = 0x20;
-            public uint unk1 = 0xFFFFFFFF;
-            public string name = "";
-        }
-
         public Header header;
         public TexInfo texInfo;
+
         public Bitmap bmp;
         public ImageSettings settings;
 
@@ -85,13 +29,8 @@ namespace image_stex
                 {
                     Width = header.width,
                     Height = header.height,
-                    Format = (header.type == DataTypes.UnsignedShort565)
-                        ? ImageSettings.ConvertFormat(Cetera.Image.Format.RGB565)
-                        : (header.type == DataTypes.UnsignedShort5551)
-                            ? ImageSettings.ConvertFormat(Cetera.Image.Format.RGBA5551)
-                            : (header.type == DataTypes.UnsignedShort4444)
-                                ? ImageSettings.ConvertFormat(Cetera.Image.Format.RGBA4444)
-                                : ImageSettings.ConvertFormat(header.imageFormat),
+                    Format = Support.Format[(uint)((header.type << 16) | header.imageFormat)],
+                    Swizzle = new CTRSwizzle(header.width, header.height)
                 };
                 br.BaseStream.Position = texInfo.offset;
                 bmp = Common.Load(br.ReadBytes(header.dataSize), settings);
