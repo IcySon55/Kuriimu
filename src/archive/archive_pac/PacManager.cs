7b0f9fcfda16e137ee5369708a80272f4647a28a
onepiecefreak3 <alberteinstein1980@gmx.de>
2017-11-24 15:39:27 +0100

    Added PAC Load/Save;

---
+++ src/archive/archive_pac/PacManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using Kontract.Interface;
+using Kontract.IO;
+
+namespace archive_pac
+{
+    public class PacManager : IArchiveManager
+    {
+        private PAC _pac = null;
+
+        #region Properties
+
+        // Information
+        public string Name => "PAC";
+        public string Description => "PACkage";
+        public string Extension => "*.pac";
+        public string About => "This is the PAC archive manager for Karameru.";
+
+        // Feature Support
+        public bool FileHasExtendedProperties => false;
+        public bool CanAddFiles => false;
+        public bool CanRenameFiles => false;
+        public bool CanReplaceFiles => false;
+        public bool CanDeleteFiles => false;
+        public bool CanSave => true;
+
+        public FileInfo FileInfo { get; set; }
+
+        #endregion
+
+        public bool Identify(string filename)
+        {
+            using (var br = new BinaryReaderX(File.OpenRead(filename)))
+            {
+                if (br.BaseStream.Length < 4) return false;
+                return br.ReadString(4) == "DGKP";
+            }
+        }
+
+        public void Load(string filename)
+        {
+            FileInfo = new FileInfo(filename);
+
+            if (FileInfo.Exists)
+                _pac = new PAC(File.Open(filename, FileMode.Open, FileAccess.ReadWrite));
+        }
+
+        public void Save(string filename = "")
+        {
+            if (!string.IsNullOrEmpty(filename))
+                FileInfo = new FileInfo(filename);
+
+            // Save As...
+            if (!string.IsNullOrEmpty(filename))
+            {
+                _pac.Save(FileInfo.Create());
+                _pac.Close();
+            }
+            else
+            {
+                // Create the temp file
+                _pac.Save(File.Create(FileInfo.FullName + ".tmp"));
+                _pac.Close();
+                // Delete the original
+                FileInfo.Delete();
+                // Rename the temporary file
+                File.Move(FileInfo.FullName + ".tmp", FileInfo.FullName);
+            }
+
+            // Reload the new file to make sure everything is in order
+            Load(FileInfo.FullName);
+        }
+
+        public void Unload()
+        {
+            _pac.Close();
+        }
+
+        // Files
+        public IEnumerable<ArchiveFileInfo> Files => _pac.Files;
+
+        public bool AddFile(ArchiveFileInfo afi) => false;
+
+        public bool DeleteFile(ArchiveFileInfo afi) => false;
+
+        // Features
+        public bool ShowProperties(Icon icon) => false;
+    }
+}
