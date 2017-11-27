9f88f62648d36a619bf090d4eecd94b4f6fb84d7
onepiecefreak3 <alberteinstein1980@gmx.de>
2017-11-27 22:50:13 +0100

    Added ANAG archive plugin (currently stub); Added text plugin for Shingeki no Kyoujin (Load/Save);

---
+++ src/text/text_SnK3DS/SnK.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Kontract.IO;
+using Kontract;
+
+namespace text_SnK3DS
+{
+    public class SnK
+    {
+        public List<Label> Labels = new List<Label>();
+
+        private List<byte[]> controls = new List<byte[]>();
+
+        public SnK(string filename)
+        {
+            using (BinaryReaderX br = new BinaryReaderX(File.OpenRead(filename)))
+            {
+                var bytes = br.ReadAllBytes().ToList();
+                var entryCount = 1;
+                while (bytes.Count() > 0)
+                {
+                    var startOffset = (int)br.BaseStream.Position;
+
+                    var index = bytes.FindIndex(b => b == 0x04);
+
+                    if (index != -1)
+                    {
+                        //check for false positives
+                        br.BaseStream.Position = startOffset + index + 1;
+                        var tmpSize = br.ReadInt16();
+                        if (tmpSize >= br.BaseStream.Length && tmpSize + br.BaseStream.Position >= br.BaseStream.Length)
+                        {
+                            var indexList = bytes.Select((b, i) => (b == 0x04) ? i : -1).Where(b => b != -1).ToList();
+                            var count = 1;
+                            while ((tmpSize >= br.BaseStream.Length && tmpSize + br.BaseStream.Position >= br.BaseStream.Length) && count < indexList.Count())
+                            {
+                                br.BaseStream.Position = startOffset + indexList[count++] + 1;
+                                tmpSize = br.ReadInt16();
+                            }
+
+                            if (count < indexList.Count)
+                            {
+                                index = indexList[count - 1];
+                            }
+                            else
+                            {
+                                controls.Add(bytes.ToArray());
+                                break;
+                            }
+                        }
+
+                        br.BaseStream.Position = startOffset;
+
+                        controls.Add(br.ReadBytes(index));
+                        br.BaseStream.Position++;
+
+                        var stringSize = br.ReadInt16();
+                        Labels.Add(new Label
+                        {
+                            Name = "Entry " + entryCount,
+                            TextID = entryCount++,
+                            Text = br.ReadCStringSJIS()
+                        });
+
+                        bytes.RemoveRange(0, (int)br.BaseStream.Position - startOffset);
+                    }
+                    else
+                    {
+                        controls.Add(bytes.ToArray());
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void Save(string filename)
+        {
+            using (BinaryWriterX bw = new BinaryWriterX(File.Create(filename)))
+            {
+                for (int i = 0; i < Labels.Count(); i++)
+                {
+                    bw.Write(controls[i]);
+                    bw.Write((byte)4);
+
+                    var text = Encoding.GetEncoding("SJIS").GetBytes(Labels[i].Text);
+                    bw.Write((short)(text.Count() + 1));
+                    bw.Write(text);
+                    bw.Write((byte)0);
+                }
+                if (controls.Count > Labels.Count())
+                {
+                    bw.Write(controls.Last());
+                }
+            }
+        }
+    }
+}
---
+++ src/text/text_SnK3DS/SnKAdapter.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using text_SnK3DS.Properties;
+using Kontract.Interface;
+using Kontract;
+using Kontract.IO;
+
+namespace text_SnK3DS
+{
+    public sealed class SnKAdapter : ITextAdapter
+    {
+        private FileInfo _fileInfo = null;
+        private SnK _snk = null;
+        private SnK _snkBackup = null;
+        private List<Entry> _entries = null;
+
+        #region Properties
+
+        // Information
+        public string Name => "SnK Text";
+        public string Description => "Text for Shingeki no Kyojin: Shichi kara no Dasshutsu 3DS";
+        public string Extension => "*.bin";
+        public string About => "This is the SnK text adapter for Kuriimu.";
+
+        // Feature Support
+        public bool FileHasExtendedProperties => false;
+        public bool CanSave => true;
+        public bool CanAddEntries => false;
+        public bool CanRenameEntries => false;
+        public bool CanDeleteEntries => false;
+        public bool CanSortEntries => true;
+        public bool EntriesHaveSubEntries => false;
+        public bool EntriesHaveUniqueNames => true;
+        public bool EntriesHaveExtendedProperties => false;
+
+        public FileInfo FileInfo
+        {
+            get
+            {
+                return _fileInfo;
+            }
+            set
+            {
+                _fileInfo = value;
+            }
+        }
+
+        public string LineEndings => "\n";
+
+        #endregion
+
+        public bool Identify(string filename)
+        {
+            using (BinaryReaderX br = new BinaryReaderX(File.OpenRead(filename)))
+            {
+                if (br.BaseStream.Length == 0) return false;
+
+                var bytes = br.ReadAllBytes();
+                var index = bytes.ToList().FindIndex(b => b == 0x04);
+                if (index == -1) return false;
+                if (index + 3 >= br.BaseStream.Length) return false;
+
+                br.BaseStream.Position = index + 1;
+                var size = br.ReadUInt16();
+                if (br.BaseStream.Position + size + 1 >= br.BaseStream.Length) return false;
+                br.BaseStream.Position += size - 1;
+
+                var check1 = br.ReadByte();
+                var check2 = br.ReadByte();
+
+                return (check1 == 0 && check2 == 0x1e);
+            }
+        }
+
+        //Load file, make backup if needed
+        public LoadResult Load(string filename, bool autoBackup = false)
+        {
+            LoadResult result = LoadResult.Success;
+
+            _fileInfo = new FileInfo(filename);
+            _entries = null;
+
+            if (_fileInfo.Exists)
+            {
+                _snk = new SnK(_fileInfo.FullName);
+
+                string backupFilePath = _fileInfo.FullName + ".bak";
+                if (File.Exists(backupFilePath))
+                {
+                    _snkBackup = new SnK(backupFilePath);
+                }
+                else if (autoBackup || MessageBox.Show("Would you like to create a backup of " + _fileInfo.Name + "?\r\nA backup allows the Original text box to display the source text before edits were made.", "Create Backup", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    File.Copy(_fileInfo.FullName, backupFilePath);
+                    _snkBackup = new SnK(backupFilePath);
+                }
+                else
+                {
+                    _snkBackup = null;
+                }
+            }
+            else
+                result = LoadResult.FileNotFound;
+
+            return result;
+        }
+
+        public SaveResult Save(string filename = "")
+        {
+            SaveResult result = SaveResult.Success;
+
+            if (filename.Trim() != string.Empty)
+                _fileInfo = new FileInfo(filename);
+
+            try
+            {
+                _snk.Save(_fileInfo.FullName);
+            }
+            catch (Exception)
+            {
+                result = SaveResult.Failure;
+            }
+
+            return result;
+        }
+
+        // Entries
+        public IEnumerable<TextEntry> Entries
+        {
+            get
+            {
+                if (_entries == null)
+                {
+                    _entries = new List<Entry>();
+
+                    foreach (Label label in _snk.Labels)
+                    {
+                        if (_snkBackup == null)
+                        {
+                            Entry entry = new Entry(label);
+                            _entries.Add(entry);
+                        }
+                        else
+                        {
+                            Entry entry = new Entry(label, _snkBackup.Labels.FirstOrDefault(o => o.TextID == label.TextID));
+                            _entries.Add(entry);
+                        }
+                    }
+                }
+
+                if (SortEntries)
+                    return _entries.OrderBy(e => e.Name).ThenBy(e => e.EditedLabel.TextID);
+
+                return _entries;
+            }
+        }
+
+        public IEnumerable<string> NameList => Entries?.Select(o => o.Name);
+
+        public string NameFilter => @".*";
+
+        public int NameMaxLength => 0;
+
+        // Features
+        public bool ShowProperties(Icon icon) => false;
+
+        public TextEntry NewEntry() => new Entry();
+
+        public bool AddEntry(TextEntry entry) => false;
+
+        public bool RenameEntry(TextEntry entry, string newName) => false;
+
+        public bool DeleteEntry(TextEntry entry) => false;
+
+        public bool ShowEntryProperties(TextEntry entry, Icon icon) => false;
+
+        // Settings
+        public bool SortEntries
+        {
+            get { return Settings.Default.SortEntries; }
+            set
+            {
+                Settings.Default.SortEntries = value;
+                Settings.Default.Save();
+            }
+        }
+    }
+}
