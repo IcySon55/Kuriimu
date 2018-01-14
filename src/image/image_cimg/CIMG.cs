09bfa931a51bfc9d777300a4c040a68a9c74d073
onepiecefreak <alberteinstein1980@gmx.de>
2018-01-15 00:49:11 +0100

    CIMG: Support for image mode 0; Add alignments;

--- src/image/image_cimg/CIMG.cs
+++ src/image/image_cimg/CIMG.cs
@@ -19,7 +19,8 @@ namespace image_cimg
         public ImageSettings settings;
         IImageFormat paletteFormat = null;
 
-        string magic;
+        bool _compressed = false;
+
         Header header;
 
         const int unkByteSize1 = 0x8;
@@ -32,10 +33,7 @@ namespace image_cimg
 
         public CIMG(Stream input)
         {
-            var decomp = Nintendo.Decompress(input);
-            input.Dispose();
-
-            using (var br = new BinaryReaderX(new MemoryStream(decomp)))
+            using (var br = new BinaryReaderX(GetStreamToUse(input)))
             {
                 //Header
                 header = br.ReadStruct<Header>();
@@ -53,20 +51,21 @@ namespace image_cimg
                 br.BaseStream.Position = header.tileDataOffset;
                 var tileData = br.ReadMultiple<short>(header.tileEntryCount);
 
-                //Get Imagedata
-                br.BaseStream.Position = header.imageDataOffset;
-                var imgData = br.ReadBytes(header.imageTileCount * imgDataEntry);
-
-                //Deflate imgData
-                imgData = Inflate(imgData.ToList(), tileData);
-
+                //Get and Deflate imgData
+                byte[] imgData = null;
                 switch (header.imgFormat)
                 {
+                    case 0:
+                        paletteFormat = new Palette(pal, new RGBA(5, 5, 5, 0, ByteOrder.LittleEndian, true), 4);
+                        imgData = Inflate(new SubStream(br.BaseStream, header.imageDataOffset, header.imageTileCount * (imgDataEntry / 2)), tileData, imgDataEntry / 2);
+                        break;
                     case 1:
                         paletteFormat = new Palette(pal, new RGBA(5, 5, 5, 0, ByteOrder.LittleEndian, true));
+                        imgData = Inflate(new SubStream(br.BaseStream, header.imageDataOffset, header.imageTileCount * imgDataEntry), tileData, imgDataEntry);
                         break;
                     case 2:
                         paletteFormat = new AI(3, 5, pal, new RGBA(5, 5, 5, 0, ByteOrder.LittleEndian, true));
+                        imgData = Inflate(new SubStream(br.BaseStream, header.imageDataOffset, header.imageTileCount * imgDataEntry), tileData, imgDataEntry);
                         break;
                     default:
                         throw new NotImplementedException($"Image mode {header.imgFormat} not supported.");
@@ -82,12 +81,40 @@ namespace image_cimg
             }
         }
 
-        byte[] Inflate(List<byte> imgData, List<short> tileData)
+        Stream GetStreamToUse(Stream input)
+        {
+            using (var br = new BinaryReaderX(input, true))
+            {
+                var magic = br.ReadString(4);
+                br.BaseStream.Position = 0;
+                if (magic == "LIMG")
+                {
+                    return input;
+                }
+                else
+                {
+                    var decomp = Nintendo.Decompress(br.BaseStream);
+                    br.Dispose();
+
+                    _compressed = true;
+                    return new MemoryStream(decomp);
+                }
+            }
+        }
+
+        byte[] Inflate(Stream imgData, List<short> tileData, int tileSize)
         {
             List<byte> result = new List<byte>();
 
-            foreach (var tileID in tileData)
-                result.AddRange(imgData.GetRange(tileID * imgDataEntry, imgDataEntry));
+            using (var br = new BinaryReaderX(imgData, true))
+                foreach (var tileID in tileData)
+                    if (tileID >= 0)
+                    {
+                        br.BaseStream.Position = tileID * tileSize;
+                        result.AddRange(br.ReadBytes(tileSize));
+                    }
+                    else
+                        result.AddRange(new byte[tileSize]);
 
             return result.ToArray();
         }
@@ -104,25 +131,28 @@ namespace image_cimg
                 settings.Swizzle = new NitroSwizzle(bmps[0].Width, bmps[0].Height);
                 var newImgData = Common.Save(bmps[0], settings);
 
-                //Get Palette
+                //Get Palette and deflate imgData
                 byte[] pal = null;
+                List<byte[]> tiles = null;
+                List<short> tileIndeces = null;
                 switch (header.imgFormat)
                 {
+                    case 0:
+                        pal = (paletteFormat as Palette).paletteBytes;
+                        (tiles, tileIndeces) = Deflate(newImgData, imgDataEntry / 2);
+                        break;
                     case 1:
                         pal = (paletteFormat as Palette).paletteBytes;
+                        (tiles, tileIndeces) = Deflate(newImgData, imgDataEntry);
                         break;
                     case 2:
                         pal = (paletteFormat as AI).paletteBytes;
+                        (tiles, tileIndeces) = Deflate(newImgData, imgDataEntry);
                         break;
                     default:
                         throw new NotImplementedException($"Image mode {header.imgFormat} not supported.");
                 }
                 short colorCount = (short)(pal.Length / 2);
-
-                //Deflate image data
-                List<byte[]> tiles;
-                List<short> tileIndeces;
-                (tiles, tileIndeces) = Deflate(newImgData);
 
                 //Update Header
                 header.tileEntryCount = (short)tileIndeces.Count();
@@ -138,26 +168,31 @@ namespace image_cimg
                 while (colorCount++ % 0x10 != 0) bw.Write((ushort)0x8000);
                 foreach (var unk1 in unkChunk1) bw.Write(unk1);
                 foreach (var unk2 in unkChunk2) bw.Write(unk2);
+                bw.WriteAlignment(4);
                 foreach (var index in tileIndeces) bw.Write(index);
+                bw.WriteAlignment(4);
                 foreach (var tile in tiles) bw.Write(tile);
 
                 //Compress data with LZ10
                 bw.BaseStream.Position = 0;
-                File.WriteAllBytes(filename, Nintendo.Compress(ms, Nintendo.Method.LZ10));
+                if (_compressed)
+                    File.WriteAllBytes(filename, Nintendo.Compress(ms, Nintendo.Method.LZ10));
+                else
+                    File.WriteAllBytes(filename, ms.ToArray());
             }
         }
 
-        (List<byte[]>, List<short>) Deflate(byte[] newImgData)
+        (List<byte[]>, List<short>) Deflate(byte[] newImgData, int tileSize)
         {
             var existingTiles = new List<byte[]>();
             var tileIndeces = new List<short>();
 
             short tileIndex = 0;
-            var tileCount = newImgData.Length / imgDataEntry;
+            var tileCount = newImgData.Length / tileSize;
             using (var imgBr = new BinaryReaderX(new MemoryStream(newImgData)))
                 for (int i = 0; i < tileCount; i++)
                 {
-                    var tile = imgBr.ReadBytes(imgDataEntry);
+                    var tile = imgBr.ReadBytes(tileSize);
                     if (existingTiles.Find(et => et.Equals(tile)) == null)
                     {
                         existingTiles.Add(tile);
