1bf892ba1b34326081a060e19b2d895244ccd0fe
onepiecefreak <alberteinstein1980@gmx.de>
2018-01-15 01:15:16 +0100

    Added LPC2 Load/Save;

---
+++ src/archive/archive_level5/Lpc2Manager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using Kontract.Interface;
+using Kontract.IO;
+
+namespace archive_level5.LPC2
+{
+    public class Lpc2Manager : IArchiveManager
+    {
+        private LPC2 _lpc2 = null;
+
+        #region Properties
+
+        // Information
+        public string Name => "LPC2";
+        public string Description => "Level 5 LPC2 Archive";
+        public string Extension => "*.cani";
+        public string About => "This is the LPC2 archive manager for Karameru.";
+
+        // Feature Support
+        public bool FileHasExtendedProperties => false;
+        public bool CanAddFiles => false;
+        public bool CanRenameFiles => false;
+        public bool CanReplaceFiles => true;
+        public bool CanDeleteFiles => false;
+        public bool CanSave => true;
+
+        public FileInfo FileInfo { get; set; }
+
+        #endregion
+
+        public bool Identify(string filename)
+        {
+            using (var br = new BinaryReaderX(File.OpenRead(filename)))
+            {
+                br.BaseStream.Position = 5;
+                return br.ReadString(4) == "LPC2";
+            }
+        }
+
+        public void Load(string filename)
+        {
+            FileInfo = new FileInfo(filename);
+
+            if (FileInfo.Exists)
+                _lpc2 = new LPC2(FileInfo.OpenRead());
+        }
+
+        public void Save(string filename = "")
+        {
+            if (!string.IsNullOrEmpty(filename))
+                FileInfo = new FileInfo(filename);
+
+            // Save As...
+            if (!string.IsNullOrEmpty(filename))
+            {
+                _lpc2.Save(FileInfo.FullName);
+                _lpc2.Close();
+            }
+            else
+            {
+                // Create the temp file
+                _lpc2.Save(FileInfo.FullName + ".tmp");
+                _lpc2.Close();
+                // Delete the original
+                FileInfo.Delete();
+                // Rename the temporary file
+                File.Move(FileInfo.FullName + ".tmp", FileInfo.FullName);
+            }
+
+            // Reload the new file to make sure everything is in order
+            Load(FileInfo.FullName);
+        }
+
+        public void Unload()
+        {
+            _lpc2?.Close();
+        }
+
+        // Files
+        public IEnumerable<ArchiveFileInfo> Files => _lpc2.Files;
+
+        public bool AddFile(ArchiveFileInfo afi) => false;
+
+        public bool DeleteFile(ArchiveFileInfo afi) => false;
+
+        // Features
+        public bool ShowProperties(Icon icon) => false;
+    }
+}
---
+++ src/archive/archive_level5/Lpc2Support.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using Kontract;
+using Kontract.Interface;
+using System.IO;
+using Kontract.IO;
+
+namespace archive_level5.LPC2
+{
+    public class LPC2FileInfo : ArchiveFileInfo
+    {
+        public FileEntry entry;
+
+        public (int newNameOff, int newFileOff) Write(Stream input, int nameOffset, int fileOffset)
+        {
+            entry.fileSize = (int)FileSize;
+            entry.nameOffset = nameOffset;
+            entry.fileOffset = fileOffset;
+
+            using (var bw = new BinaryWriterX(input, true))
+            {
+                FileData.CopyTo(bw.BaseStream);
+                if (bw.BaseStream.Position % 4 != 0)
+                    bw.WriteAlignment(4);
+                else
+                    bw.WritePadding(4);
+            }
+
+            return (nameOffset + Encoding.ASCII.GetByteCount(FileName) + 1,
+                ((fileOffset + FileSize) % 4 != 0) ?
+                    (int)((fileOffset + FileSize + 3) & ~3) :
+                    (int)(fileOffset + FileSize + 4));
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public class Header
+    {
+        public Magic magic;
+        public int fileCount;
+        public int headerSize;
+        public int fileSize;
+
+        public int fileEntryOffset;
+        public int nameOffset;
+        public int dataOffset;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public class FileEntry
+    {
+        public int nameOffset; //relative
+        public int fileOffset; //relative
+        public int fileSize;
+    }
+}
