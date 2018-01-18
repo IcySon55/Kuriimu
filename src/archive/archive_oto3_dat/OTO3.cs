6d7fcea8a02d2ccd6ca4f685de9864bb121f148d
onepiecefreak <alberteinstein1980@gmx.de>
2018-01-19 00:35:42 +0100

    Added Load/Save for Osawari Tantei 3 dat archives;

---
+++ src/archive/archive_oto3_dat/OTO3.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Kontract.Interface;
+using Kontract.IO;
+
+namespace archive_oto3_dat
+{
+    public class OTO3
+    {
+        public List<OTO3FileInfo> Files = new List<OTO3FileInfo>();
+        private Stream _stream = null;
+
+        Header header;
+        List<Entry> entries = new List<Entry>();
+
+        public OTO3(Stream input)
+        {
+            _stream = input;
+            using (var br = new BinaryReaderX(input, true))
+            {
+                //Header
+                header = br.ReadStruct<Header>();
+
+                //Entries
+                br.BaseStream.Position = 0x10;
+                for (int i = 0; i < header.entryCount; i++)
+                    entries.Add(new Entry(br.BaseStream, header.nameBufSize));
+
+                //Files
+                foreach (var entry in entries)
+                    Files.Add(new OTO3FileInfo
+                    {
+                        State = ArchiveFileState.Archived,
+                        FileName = entry.fileName,
+                        FileData = new SubStream(br.BaseStream, entry.fileOffset, entry.fileSize),
+                        entry = entry
+                    });
+            }
+        }
+
+        public void Save(Stream output)
+        {
+            using (var bw = new BinaryWriterX(output))
+            {
+                var dataOffset = (0x10 + Files.Count * (0x8 + header.nameBufSize) + 0xf) & ~0xf;
+
+                //Write FileData
+                bw.BaseStream.Position = dataOffset;
+                foreach (var file in Files)
+                    file.Write(bw.BaseStream);
+
+                //Write Entries
+                bw.BaseStream.Position = 0x10;
+                foreach (var file in Files)
+                    file.entry.Write(bw.BaseStream, header.nameBufSize);
+
+                //Write Header
+                bw.BaseStream.Position = 0;
+                header.entryCount = Files.Count;
+                bw.WriteStruct(header);
+            }
+        }
+
+        public void Close()
+        {
+            _stream?.Dispose();
+            foreach (var afi in Files)
+                if (afi.State != ArchiveFileState.Archived)
+                    afi.FileData?.Dispose();
+            _stream = null;
+        }
+    }
+}
---
+++ src/archive/archive_oto3_dat/Oto3Manager.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using Kontract.Interface;
+using Kontract.IO;
+
+namespace archive_oto3_dat
+{
+    public class Oto3Manager : IArchiveManager
+    {
+        private OTO3 _oto3 = null;
+
+        #region Properties
+
+        // Information
+        public string Name => "OTO3 DAT";
+        public string Description => "Osawari Tantei 3 Archive";
+        public string Extension => "*.dat";
+        public string About => "This is the Osawari Tantei 3 archive manager for Karameru.";
+
+        // Feature Support
+        public bool FileHasExtendedProperties => false;
+        public bool CanAddFiles => false;
+        public bool CanRenameFiles => false;
+        public bool CanReplaceFiles => true;
+        public bool CanDeleteFiles => false;
+        public bool CanSave => true;
+
+        public FileInfo FileInfo { get; set; }
+
+        #endregion
+
+        public bool Identify(string filename)
+        {
+            using (var br = new BinaryReaderX(File.OpenRead(filename)))
+            {
+                try
+                {
+                    var count = br.ReadInt32();
+                    var nameBuf = br.ReadInt32();
+
+                    var lastEntryOff = 0x10 + (0x8 + nameBuf) * (count - 1);
+                    br.BaseStream.Position = lastEntryOff;
+
+                    var off = br.ReadInt32();
+                    var size = br.ReadInt32();
+
+                    return br.BaseStream.Length == off + size;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+
+        public void Load(string filename)
+        {
+            FileInfo = new FileInfo(filename);
+
+            if (FileInfo.Exists)
+                _oto3 = new OTO3(FileInfo.OpenRead());
+        }
+
+        public void Save(string filename = "")
+        {
+            if (!string.IsNullOrEmpty(filename))
+                FileInfo = new FileInfo(filename);
+
+            // Save As...
+            if (!string.IsNullOrEmpty(filename))
+            {
+                _oto3.Save(FileInfo.Create());
+                _oto3.Close();
+            }
+            else
+            {
+                // Create the temp file
+                _oto3.Save(File.Create(FileInfo.FullName + ".tmp"));
+                _oto3.Close();
+                // Delete the original
+                FileInfo.Delete();
+                // Rename the temporary file
+                File.Move(FileInfo.FullName + ".tmp", FileInfo.FullName);
+            }
+
+            // Reload the new file to make sure everything is in order
+            Load(FileInfo.FullName);
+        }
+
+        public void Unload()
+        {
+            _oto3?.Close();
+        }
+
+        // Files
+        public IEnumerable<ArchiveFileInfo> Files => _oto3.Files;
+
+        public bool AddFile(ArchiveFileInfo afi) => false;
+
+        public bool DeleteFile(ArchiveFileInfo afi) => false;
+
+        // Features
+        public bool ShowProperties(Icon icon) => false;
+    }
+}
