ebf96b99ca2520583a1d2283d1483bb9876c362a
IcySon55 <icyson55@gmail.com>
2018-03-17 01:30:44 -0400

    Fixed an imports bug in MMB.

--- src/archive/archive_nintendo/MmbManager.cs
+++ src/archive/archive_nintendo/MmbManager.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
-using System.Windows.Forms;
 using Kontract.Interface;
-using Kontract.IO;
 
 namespace archive_nintendo.MMB
 {
