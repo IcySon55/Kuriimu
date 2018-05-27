7738135d3d5801f0fdb34abd0ec5cb086851fbb0
onepiecefreak3 <alberteinstein1980@gmx.de>
2018-05-27 21:38:41 +0200

    Added PAC loading for Mario Party 10; #186

---
+++ src/archive/archive_pac_mario_party/PAC.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.IO;
+using Kontract.Interface;
+using Kontract.IO;
+
+namespace archive_pac_mario_party
+{
+    public sealed class PAC
+    {
+        public List<PacFileInfo> Files = new List<PacFileInfo>();
+        private Stream _stream = null;
+
+        Header header;
+        List<AssetEntry> assets;
+        List<Entry> entries;
+        byte[] strings;
+
+        const int assetSize = 0x30;
+
+        public PAC(Stream input)
+        {
+            _stream = input;
+            using (var br = new BinaryReaderX(input, true, ByteOrder.BigEndian))
+            {
+                header = br.ReadStruct<Header>();
+
+                br.BaseStream.Position = header.assetOffset;
+                assets = br.ReadMultiple<AssetEntry>(header.assetCount);
+
+                br.BaseStream.Position = header.entryOffset;
+                entries = br.ReadMultiple<Entry>(header.entryCount);
+
+                br.BaseStream.Position = header.stringOffset;
+                strings = br.ReadBytes(header.dataOffset - header.stringOffset);
+
+                CheckCounts();
+
+                using (var sr = new BinaryReaderX(new MemoryStream(strings)))
+                    foreach (var asset in assets)
+                    {
+                        sr.BaseStream.Position = asset.stringOffset - header.stringOffset;
+                        var assetName = sr.ReadCStringA();
+                        for (int i = (asset.entryOffset - header.entryOffset) / assetSize; i < ((asset.entryOffset - header.entryOffset) / assetSize) +asset.count; i++)
+                        {
+                            sr.BaseStream.Position = entries[i].stringOffset - header.stringOffset;
+                            var entryName = sr.ReadCStringA();
+                            Files.Add(new PacFileInfo
+                            {
+                                State = ArchiveFileState.Archived,
+                                FileName = Path.Combine(assetName, entryName),
+                                FileData = new SubStream(br.BaseStream, entries[i].dataOffset, entries[i].compSize),
+                                entry = entries[i]
+                            });
+                        }
+                    }
+
+                br.BaseStream.Position = header.dataOffset;
+            }
+        }
+
+        private void CheckCounts()
+        {
+            if (assets.Count != header.assetCount)
+                throw new Exception("Asset count doesn't match.");
+
+            if (entries.Count != header.entryCount)
+                throw new Exception("Entry count doesn't match.");
+
+            var tmp = entries.Select(e => e.stringOffset).ToList();
+            tmp.AddRange(entries.Select(e => e.extOffset));
+            tmp.AddRange(assets.Select(a => a.stringOffset));
+            tmp = tmp.Distinct().ToList();
+            if (tmp.Count != header.stringCount)
+                throw new Exception("String count doesn't match.");
+
+            if (entries.Select(e => e.dataOffset).Distinct().Count() != header.fileDataCount)
+                throw new Exception("Filedata count doesn't match.");
+        }
+
+        public void Save(Stream input)
+        {
+            using (var bw = new BinaryWriterX(input))
+            {
+
+            }
+        }
+
+        public void Close()
+        {
+            _stream?.Dispose();
+            foreach (var afi in Files)
+                if (afi.State != ArchiveFileState.Archived)
+                    afi.FileData?.Dispose();
+            _stream = null;
+        }
+    }
+}
---
+++ src/archive/archive_pac_mario_party/PacSupport.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Kontract;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kontract.Interface;
+using Kontract.Compression;
+using System.IO;
+
+namespace archive_pac_mario_party
+{
+    public class PacFileInfo : ArchiveFileInfo
+    {
+        public Entry entry;
+
+        public override Stream FileData
+        {
+            get => new MemoryStream(ZLib.Decompress(base.FileData));
+        }
+
+        public override long? FileSize => (State == ArchiveFileState.Archived) ? entry.decompSize : base.FileData.Length;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public class Header
+    {
+        public Magic magic = "PAC\0";
+        public int unk1;
+        public int unk2;
+        public int dataOffset;
+
+        public Magic magic2;
+        public int assetCount;
+        public int entryCount;
+        public int stringCount;
+        public int fileDataCount;
+        public long zero0;
+        public long zero1;
+        public int assetOffset;
+        public int entryOffset;
+        public int stringOffset;
+        public int fileDataOffset;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public class AssetEntry
+    {
+        public int stringOffset;
+        public uint unk1;
+        public int count;
+        public int entryOffset;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public class Entry
+    {
+        public int stringOffset;
+        public uint unk1;
+        public int extOffset;
+        public int const1;
+
+        public int dataOffset;
+        public int decompSize;
+        public int compSize;
+        public int compSize2;
+
+        public long zero1;
+        public int unk2;
+        public int zero2;
+    }
+}
