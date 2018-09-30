fdb509c4c3ad677c237d9dddf98865fa203acfae
onepiecefreak3 <alberteinstein1980@gmx.de>
2018-09-30 20:57:06 +0200

    Refactored some Switch encryption stuff;

--- src/Kontract/Encryption/Switch.cs
+++ src/Kontract/Encryption/Switch.cs
@@ -7,6 +7,8 @@ using System.IO;
 using Kontract.IO;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
 using Kontract.Encryption.AES;
 using Kontract.Hash;
 
@@ -14,87 +16,68 @@ namespace Kontract.Encryption
 {
     public class Switch
     {
+        private const long _mediaUnit = 0x200;
+
         public class Keyset
         {
-            private static byte[] ncaHeader_sha256 = new byte[] { 0x8e, 0x03, 0xde, 0x24, 0x81, 0x8d, 0x96, 0xce, 0x4f, 0x2a, 0x09, 0xb4, 0x3a, 0xf9, 0x79, 0xe6, 0x79, 0x97, 0x4f, 0x75, 0x70, 0x71, 0x3a, 0x61, 0xee, 0xd8, 0xb3, 0x14, 0x86, 0x4a, 0x11, 0xd5 };
+            private Dictionary<string, byte[]> _keyMaterial;
 
-            public byte[] ncaHeaderKey = new byte[0x20];
+            public byte[] this[string i]
+            {
+                get
+                {
+                    if (!_keyMaterial.ContainsKey(i))
+                        throw new KeyNotFoundException(i);
+                    return _keyMaterial[i];
+                }
+                set
+                {
+                    if (!_keyMaterial.ContainsKey(i))
+                        _keyMaterial.Add(i, value);
+                    else
+                        _keyMaterial[i] = value;
+                }
+            }
 
-            byte[][] masterkeys = new byte[3][];
-            byte[] titlekekSource;
+            //private static byte[] ncaHeader_sha256 = new byte[] { 0x8e, 0x03, 0xde, 0x24, 0x81, 0x8d, 0x96, 0xce, 0x4f, 0x2a, 0x09, 0xb4, 0x3a, 0xf9, 0x79, 0xe6, 0x79, 0x97, 0x4f, 0x75, 0x70, 0x71, 0x3a, 0x61, 0xee, 0xd8, 0xb3, 0x14, 0x86, 0x4a, 0x11, 0xd5 };
 
-            byte[] aesKekGenSource;
-            byte[] aesKeyGenSource;
+            //public byte[] ncaHeaderKey = new byte[0x20];
 
-            byte[] kakAppSource;
-            byte[] kakOceanSource;
-            byte[] kakSystemSource;
+            //byte[][] masterkeys = new byte[3][];
+            //byte[] titlekekSource;
 
-            public byte[][][] keyAreaKeys = new byte[3][][];
-            public byte[][] titleKeks = new byte[3][];
+            //byte[] aesKekGenSource;
+            //byte[] aesKeyGenSource;
+
+            //byte[] kakAppSource;
+            //byte[] kakOceanSource;
+            //byte[] kakSystemSource;
 
             public Keyset()
             {
-                if (File.Exists("bin\\switch_keys.dat"))
+                if (!File.Exists("bin\\switch_keys.dat"))
+                    throw new FileNotFoundException("Couldn't find switch_keys.dat");
+
+                _keyMaterial = File.ReadAllLines("bin\\switch_keys.dat")
+                    .Select(l => l.Replace(" ", "").Replace("\t", ""))
+                    .Where(l => !l.StartsWith(";") && !String.IsNullOrEmpty(l) && Regex.IsMatch(l.Split('=').Skip(1).First(), "^[a-fA-F0-9]+$"))
+                    .ToDictionary(
+                        l => l.Split('=').First(),
+                        l => l.Split('=').Skip(1).First().Hexlify()
+                        );
+
+                var masterKeys = _keyMaterial.Where(k => Regex.IsMatch(k.Key, "master_key_[\\d]{2}")).Select(m => Convert.ToInt32(Regex.Match(m.Key, "[\\d]{2}").Value));
+                foreach (var i in masterKeys)
                 {
-                    //use keyfile, if found
-                    var lines = File.ReadAllLines("bin\\switch_keys.dat");
-                    foreach (var line in lines)
-                    {
-                        if (line != String.Empty)
-                        {
-                            var key_desc = line.Replace(" ", "");
-                            var name = key_desc.Split(new[] { ':', '=' })[0];
-                            var key = key_desc.Split(new[] { ':', '=' })[1];
+                    if (!_keyMaterial.ContainsKey($"key_area_key_application_{i:00}"))
+                        this[$"key_area_key_application_{i:00}"] = GenerateKek(this["key_area_key_application_source"], this[$"master_key_{i:00}"], this["aes_kek_generation_source"], this["aes_key_generation_source"]);
+                    if (!_keyMaterial.ContainsKey($"key_area_key_ocean_{i:00}"))
+                        this[$"key_area_key_ocean_{i:00}"] = GenerateKek(this["key_area_key_ocean_source"], this[$"master_key_{i:00}"], this["aes_kek_generation_source"], this["aes_key_generation_source"]);
+                    if (!_keyMaterial.ContainsKey($"key_area_key_system_{i:00}"))
+                        this[$"key_area_key_system_{i:00}"] = GenerateKek(this["key_area_key_system_source"], this[$"master_key_{i:00}"], this["aes_kek_generation_source"], this["aes_key_generation_source"]);
 
-                            if (name.Contains("master_key"))
-                                masterkeys[Convert.ToInt32(name.Split(new string[] { "master_key_" }, StringSplitOptions.None)[1])] = key.Hexlify(16);
-                            if (name == "header_key")
-                                ncaHeaderKey = key.Hexlify(32);
-                            if (name == "title_kek_source")
-                                titlekekSource = key.Hexlify(16);
-                            if (name == "aes_kek_generation_source")
-                                aesKekGenSource = key.Hexlify(16);
-                            if (name == "aes_key_generation_source")
-                                aesKeyGenSource = key.Hexlify(16);
-                            if (name == "key_area_key_application_source")
-                                kakAppSource = key.Hexlify(16);
-                            if (name == "key_area_key_ocean_source")
-                                kakOceanSource = key.Hexlify(16);
-                            if (name == "key_area_key_system_source")
-                                kakSystemSource = key.Hexlify(16);
-                        }
-                    }
-                }
-                else
-                {
-                    //else ask for every key
-                    ncaHeaderKey = InputBox.Show($"Input NCA Header Key", "Decrypt XCI").Hexlify(32);
-                    if (!SHA256.Create(ncaHeaderKey).SequenceEqual(ncaHeader_sha256))
-                        throw new InvalidDataException("The given NCA Header Key is wrong.");
-
-                    masterkeys[0] = InputBox.Show($"Input Master Key #00", "Decrypt XCI").Hexlify(16);
-                    masterkeys[1] = InputBox.Show($"Input Master Key #01", "Decrypt XCI").Hexlify(16);
-                    masterkeys[2] = InputBox.Show($"Input Master Key #02", "Decrypt XCI").Hexlify(16);
-
-                    titlekekSource = InputBox.Show($"Input Title Kek Source", "Decrypt XCI").Hexlify(16);
-
-                    aesKekGenSource = InputBox.Show($"Input AES Kek Generation Source", "Decrypt XCI").Hexlify(16);
-                    aesKeyGenSource = InputBox.Show($"Input AES Key Generation Source", "Decrypt XCI").Hexlify(16);
-
-                    kakAppSource = InputBox.Show($"Input Key Area Key Application Source", "Decrypt XCI").Hexlify(16);
-                    kakOceanSource = InputBox.Show($"Input Key Area Key Ocean Source", "Decrypt XCI").Hexlify(16);
-                    kakSystemSource = InputBox.Show($"Input Key Area Key System Source", "Decrypt XCI").Hexlify(16);
-                }
-
-                for (int i = 0; i < masterkeys.Length; i++)
-                {
-                    keyAreaKeys[i] = new byte[3][];
-                    keyAreaKeys[i][0] = GenerateKek(kakAppSource, masterkeys[i], aesKekGenSource, aesKeyGenSource);
-                    keyAreaKeys[i][1] = GenerateKek(kakOceanSource, masterkeys[i], aesKekGenSource, aesKeyGenSource);
-                    keyAreaKeys[i][2] = GenerateKek(kakSystemSource, masterkeys[i], aesKekGenSource, aesKeyGenSource);
-
-                    titleKeks[i] = Decryption.ECB128(titlekekSource, masterkeys[i]);
+                    if (!_keyMaterial.ContainsKey($"titlekek_{i:00}"))
+                        this[$"titlekek_{i:00}"] = Decryption.ECB128(this["titlekek_source"], this[$"master_key_{i:00}"]);
                 }
             }
 
@@ -116,14 +99,12 @@ namespace Kontract.Encryption
 
         private static byte[] xci_sha256 = new byte[] { 0x2e, 0x36, 0xcc, 0x55, 0x15, 0x7a, 0x35, 0x10, 0x90, 0xa7, 0x3e, 0x7a, 0xe7, 0x7c, 0xf5, 0x81, 0xf6, 0x9b, 0x0b, 0x6e, 0x48, 0xfb, 0x06, 0x6c, 0x98, 0x48, 0x79, 0xa6, 0xed, 0x7d, 0x2e, 0x96 };
 
-        public static void DecryptXCI(Stream input, byte[] xci_header_key)
+        public static void DecryptXCI(Stream input)
         {
             if (!input.CanWrite)
                 throw new Exception("File not writeable. Is it open somewhere?");
 
-            if (!SHA256.Create(xci_header_key).SequenceEqual(xci_sha256))
-                throw new InvalidDataException("The given XCI Header Key is wrong.");
-
+            var keyset = new Keyset();
             using (var bw = new BinaryWriterX(input, true))
             using (var br = new BinaryReaderX(input, true))
             {
@@ -135,7 +116,7 @@ namespace Kontract.Encryption
                 var encrypted_xci_header = br.ReadBytes(0x70);
 
                 bw.BaseStream.Position = 0x190;
-                bw.Write(Decryption.CBC128(encrypted_xci_header, xci_header_key, iv));
+                bw.Write(Decryption.CBC128(encrypted_xci_header, keyset["xci_header_key"], iv));
 
                 //Get HFS0 entries for NCA's
                 br.BaseStream.Position = 0x130;
@@ -148,7 +129,7 @@ namespace Kontract.Encryption
                 {
                     if (entry.name.Contains(".nca"))
                     {
-                        DecryptNCA(input, entry.entry.offset);
+                        DecryptNCA(input, entry.entry.offset, keyset);
                     }
                 }
             }
@@ -223,9 +204,9 @@ namespace Kontract.Encryption
         #endregion
 
         #region NCA Decryption
-        public static void DecryptNCA(Stream input, long offset)
+        public static void DecryptNCA(Stream input, long offset, Keyset keys = null)
         {
-            var keyset = new Keyset();
+            var keyset = keys ?? new Keyset();
 
             using (var bw = new BinaryWriterX(input, true))
             using (var br = new BinaryReaderX(input, true))
@@ -239,13 +220,13 @@ namespace Kontract.Encryption
                 var headerPart2 = new byte[0x800];
                 if (magic != "NCA2" && magic != "NCA3")
                 {
-                    headerPart = Decryption.XTS128(enc_header, keyset.ncaHeaderKey, 0x200, true);
+                    headerPart = Decryption.XTS128(enc_header, keyset["header_key"], 0x200, true);
                     magic = headerPart.GetElements(0x200, 4).Aggregate("", (o, b) => o + (char)b);
                     bw.BaseStream.Position = offset;
                     bw.Write(headerPart);
                     if (magic == "NCA3")
                     {
-                        headerPart2 = Decryption.XTS128(br.ReadBytes(0x800), keyset.ncaHeaderKey, 0x200, true, 2);
+                        headerPart2 = Decryption.XTS128(br.ReadBytes(0x800), keyset["header_key"], 0x200, true, 2);
                         bw.BaseStream.Position = offset + 0x400;
                         bw.Write(headerPart2);
                     }
@@ -253,7 +234,7 @@ namespace Kontract.Encryption
                     {
                         for (int i = 0; i < 4; i++)
                         {
-                            var buffer = Decryption.XTS128(br.ReadBytes(0x200), keyset.ncaHeaderKey, 0x200, true);
+                            var buffer = Decryption.XTS128(br.ReadBytes(0x200), keyset["header_key"], 0x200, true);
                             Array.Copy(buffer, 0, headerPart2, i * 0x200, 0x200);
                             bw.BaseStream.Position -= 0x200;
                             bw.Write(buffer);
@@ -261,7 +242,7 @@ namespace Kontract.Encryption
                     }
                     else
                     {
-                        throw new InvalidDataException("Invalid NCA Header! Are the keys correct?");
+                        throw new InvalidDataException("Invalid NCA Header! Is the header key correct?");
                     }
                 }
                 else
@@ -273,7 +254,7 @@ namespace Kontract.Encryption
 
                 //Get crypto_type for master_key
                 var cryptoType = (headerPart[0x220] > headerPart[0x206]) ? headerPart[0x220] : headerPart[0x206];
-                if (cryptoType == 1) cryptoType--;
+                if (cryptoType >= 1) cryptoType--;
 
                 //RightsID
                 bool hasRightsID = false;
@@ -298,13 +279,27 @@ namespace Kontract.Encryption
                     var keyIndex = headerPart[0x207];
                     if (keyIndex > 2)
                         throw new InvalidDataException($"NCA KeyIndex must be 0-2. Found KeyIndex: {keyIndex}");
-                    dec_key_area = Decryption.ECB128(headerPart.GetElements(0x300, 0x40), keyset.keyAreaKeys[cryptoType][keyIndex]);
+
+                    byte[] decKey = null;
+                    switch (keyIndex)
+                    {
+                        case 0:
+                            decKey = keyset[$"key_area_key_application_{cryptoType:00}"];
+                            break;
+                        case 1:
+                            decKey = keyset[$"key_area_key_ocean_{cryptoType:00}"];
+                            break;
+                        case 2:
+                            decKey = keyset[$"key_area_key_system_{cryptoType:00}"];
+                            break;
+                    }
+                    dec_key_area = Decryption.ECB128(headerPart.GetElements(0x300, 0x40), decKey);
                 }
                 else
                 {
                     //Decrypt title_key
                     var title_key = InputBox.Show("Input Titlekey:", "Decrypt NCA").Hexlify(16);
-                    dec_title_key = Decryption.ECB128(title_key, keyset.titleKeks[cryptoType]);
+                    dec_title_key = Decryption.ECB128(title_key, keyset[$"titlekek_{cryptoType:00}"]);
                 }
 
                 //Read out section crypto
@@ -341,7 +336,8 @@ namespace Kontract.Encryption
                                 case 3:
                                     //CTR
                                     enc_buffer = br.ReadBytes(sectionList[i].endMediaOffset * 0x200 - sectionList[i].mediaOffset * 0x200);
-                                    dec_buffer = Decryption.CTR128(enc_buffer, dec_key_area.GetElements(0x20, 0x10), GenerateCTR(i + 1, sectionList[i].mediaOffset * 0x200));
+                                    var section_ctr = headerPart2.GetElements(i * 0x200 + 0x140, 8).Reverse().ToArray();
+                                    dec_buffer = Decryption.CTR128(enc_buffer, dec_key_area.GetElements(0x20, 0x10), GenerateCTR(section_ctr, sectionList[i].mediaOffset * 0x200));
                                     bw.BaseStream.Position = offset + sectionList[i].mediaOffset * 0x200;
                                     bw.Write(dec_buffer);
                                     break;
@@ -359,14 +355,23 @@ namespace Kontract.Encryption
             }
         }
 
-        private static byte[] GenerateCTR(int section_id, long offset)
+        private static byte[] GenerateCTR(byte[] section_ctr, long offset)
+        {
+            int ctr = 0;
+            for (int i = 0; i < 4; i++)
+                ctr |= section_ctr[i] << ((3 - i) * 8);
+
+            return GenerateCTR(ctr, offset);
+        }
+
+        private static byte[] GenerateCTR(int section_ctr, long offset)
         {
             offset >>= 4;
             byte[] ctr = new byte[0x10];
             for (int i = 0; i < 4; i++)
             {
-                ctr[0x4 - i - 1] = (byte)(section_id & 0xFF);
-                section_id >>= 8;
+                ctr[0x4 - i - 1] = (byte)(section_ctr & 0xFF);
+                section_ctr >>= 8;
             }
             for (int i = 0; i < 8; i++)
             {
