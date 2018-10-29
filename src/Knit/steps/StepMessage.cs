70bbb64043128f2c473f9cf5d5a0309cd90da58d
IcySon55 <icyson55@gmail.com>
2018-10-29 09:26:45 -0400

    Added the new message step type to Knit.

---
+++ src/Knit/steps/StepMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Knit.steps
+{
+    public class StepMessage : Step
+    {
+        // Properties
+        [XmlAttribute("message")]
+        public string Message { get; set; } = string.Empty;
+
+        // Methods
+        public override async Task<StepResults> Perform(Dictionary<string, object> variableCache, IProgress<ProgressReport> progress)
+        {
+            return new StepResults { Message = Common.ProcessVariableTokens(Message, variableCache), Status = StepStatus.Success };
+        }
+    }
+}
