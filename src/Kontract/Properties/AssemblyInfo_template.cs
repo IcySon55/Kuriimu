64e861785496c6c7b38fa58086a7fa876ee8de0f
IcySon55 <icyson55@gmail.com>
2018-12-03 14:18:12 -0500

    Re-added the AssemblyInfo templates.

---
+++ src/Kontract/Properties/AssemblyInfo_template.cs
@@ -0,0 +1,41 @@
+﻿// Do not change this file, as it will be overwritten on build time
+// Edit AssemblyInfo_template.cs instead
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Kuriimu Contract")]
+[assembly: AssemblyDescription("Kuriimu Contract is the information interchange library between the applications and all plugins.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Fan Translators International")]
+[assembly: AssemblyProduct("Kuriimu Contract")]
+[assembly: AssemblyCopyright("Copyright © Fan Translators Int. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("92fb79a3-6d3f-4343-8ee9-fd18b8ba97cf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+// Use this when you apply git tags like "0.1.0" to automatically keep up with the version
+[assembly: AssemblyVersion("$gitLastTag$.$gitCount$")]
+[assembly: AssemblyFileVersion("$gitLastTag$.$gitCount$")]
+[assembly: AssemblyInformationalVersion("$gitLastTag$-$gitHashShort$")]
